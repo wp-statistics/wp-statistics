577d0a15f1bd7227be4dabf88056eec5cf9f0956
Mostafa Soufi <mostafa.soufi@hotmail.com>
2014-01-20 17:17:29 +0000

    update new version.

---
+++ includes/class/statistics.class.php
@@ -0,0 +1,203 @@
+<?php
+	class WP_Statistics {
+		
+		protected $db;
+		protected $tb_prefix;
+		
+		private $ip;
+		private $result;
+		private $agent;
+		
+		public $coefficient = 1;
+		
+		public function __construct() {
+		
+			global $wpdb, $table_prefix;
+			
+			$this->db = $wpdb;
+			$this->tb_prefix = $table_prefix;
+			$this->agent = $this->get_UserAgent();
+			if( get_option('wps_coefficient') ) {
+				$this->coefficient = get_option('wps_coefficient');
+			}
+
+		}
+		
+		public function Primary_Values() {
+		
+			$this->result = $this->db->query("SELECT * FROM {$this->tb_prefix}statistics_useronline");
+			
+			if( !$this->result ) {
+			
+				$this->db->insert(
+					$this->tb_prefix . "statistics_useronline",
+					array(
+						'ip'		=>	$this->get_IP(),
+						'timestamp'	=>	date('U'),
+						'date'		=>	$this->Current_Date(),
+						'referred'	=>	$this->get_Referred(),
+						'agent'		=>	$this->agent['browser'],
+						'platform'	=>	$this->agent['platform'],
+						'version'	=> 	$this->agent['version']
+					)
+				);
+			}
+			
+			$this->result = $this->db->query("SELECT * FROM {$this->tb_prefix}statistics_visit");
+			
+			if( !$this->result ) {
+			
+				$this->db->insert(
+					$this->tb_prefix . "statistics_visit",
+					array(
+						'last_visit'	=>	$this->Current_Date(),
+						'last_counter'	=>	$this->Current_date('Y-m-d'),
+						'visit'			=>	1
+					)
+				);
+			}
+			
+			$this->result = $this->db->query("SELECT * FROM {$this->tb_prefix}statistics_visitor");
+			
+			if( !$this->result ) {
+			
+				$this->db->insert(
+					$this->tb_prefix . "statistics_visitor",
+					array(
+						'last_counter'	=>	$this->Current_date('Y-m-d'),
+						'referred'		=>	$this->get_Referred(),
+						'agent'			=>	$this->agent['browser'],
+						'platform'		=>	$this->agent['platform'],
+						'version'		=> 	$this->agent['version'],
+						'ip'			=>	$this->get_IP(),
+						'location'		=>	'000'
+					)
+				);
+			}
+		}
+		
+		public function get_IP() {
+		
+			if (getenv('HTTP_CLIENT_IP')) {
+				$this->ip = getenv('HTTP_CLIENT_IP');
+			} elseif (getenv('HTTP_X_FORWARDED_FOR')) {
+				$this->ip = getenv('HTTP_X_FORWARDED_FOR');
+			} elseif (getenv('HTTP_X_FORWARDED')) {
+				$this->ip = getenv('HTTP_X_FORWARDED');
+			} elseif (getenv('HTTP_FORWARDED_FOR')) {
+				$this->ip = getenv('HTTP_FORWARDED_FOR');
+			} elseif (getenv('HTTP_FORWARDED')) {
+				$this->ip = getenv('HTTP_FORWARDED');
+			} else {
+				$this->ip = $_SERVER['REMOTE_ADDR'];
+			}
+			
+			return $this->ip;
+		}
+		
+		public function get_UserAgent() {
+		
+			$agent = parse_user_agent();
+
+			if( $agent['browser'] == null ) { $agent['browser'] = "Unknown"; }
+			if( $agent['platform'] == null ) { $agent['platform'] = "Unknown"; }
+			if( $agent['version'] == null ) { $agent['version'] = "Unknown"; }
+			
+			return $agent;
+		}
+		
+		public function get_Referred($default_referr = false) {
+		
+			if( $default_referr ) {
+				if( esc_sql(strip_tags($_SERVER['HTTP_REFERER'])) ) {
+					return get_bloginfo('url');
+				} else {
+					return esc_sql(strip_tags($_SERVER['HTTP_REFERER']));
+				}
+			} else {
+				return esc_sql(strip_tags($_SERVER['HTTP_REFERER']));
+			}
+		}
+		
+		public function Current_Date($format = 'Y-m-d H:i:s', $strtotime = null) {
+		
+			if( $strtotime ) {
+				return date($format, strtotime("{$strtotime} day") ) ;
+			} else {
+				return date($format) ;
+			}
+		}
+		
+		public function Check_Search_Engines ($search_engine_name, $search_engine = null) {
+		
+			if( strstr($search_engine, $search_engine_name) ) {
+				return 1;
+			}
+		}
+		
+		public function Search_Engine_Info($url = false) {
+
+			if(!$url) {
+				$url = isset($_SERVER['HTTP_REFERER']) ? $this->get_Referred() : false;
+			}
+			
+			if($url == false) {
+				return false;
+			}
+
+			$parts = parse_url($url);
+
+			$search_engines = wp_statistics_searchengine_list();
+
+			foreach( $search_engines as $key => $value ) {
+				$search_regex = wp_statistics_searchengine_regex($key);
+
+				preg_match( '/' . $search_regex . '/', $parts['host'], $matches);
+				
+				if( isset($matches[1]) )
+					{
+					return $value;
+					}
+			}
+
+			return array('name' => 'Unknown', 'tag' => '', 'sqlpattern' => '', 'regexpattern' => '', 'querykey' => 'q', 'image' => 'unknown.png' );
+		}
+		
+		public function Search_Engine_QueryString($url = false) {
+		
+			if(!$url) {
+				$url = isset($_SERVER['HTTP_REFERER']) ? $this->get_Referred() : false;
+			}
+			
+			if($url == false) {
+				return false;
+			}
+
+			$parts = parse_url($url);
+			
+			if( array_key_exists('query',$parts) ) { parse_str($parts['query'], $query); } else { $query = array(); }
+
+			$search_engines = wp_statistics_searchengine_list();
+
+			foreach( $search_engines as $key => $value ) {
+				$search_regex = wp_statistics_searchengine_regex($key);
+
+				preg_match( '/' . $search_regex . '/', $parts['host'], $matches);
+				
+				if( isset($matches[1]) )
+					{
+					if( array_key_exists($search_engines[$key]['querykey'], $query) ) {
+						$words = strip_tags($query[$search_engines[$key]['querykey']]);
+					}
+					else {
+						$words = '';
+					}
+				
+					if( $words == '' ) { $words = 'No search query found!'; }
+					return $words;
+					}
+			}
+
+			return '';
+		}
+	}
---
+++ includes/optimization/optimization-geoip.php
@@ -0,0 +1,390 @@
+<script type="text/javascript">
+	jQuery(document).ready(function() {
+		jQuery("#purge-data-submit").click(function(){
+		
+			var action = jQuery('#purge-data').val();
+			
+			if(action == 0)
+				return false;
+				
+			var agree = confirm('<?php _e('Are you sure?', 'wp_statistics'); ?>');
+
+			if(!agree)
+				return false;
+				
+			var data = new Array();
+			data['purge-days'] = action;
+			
+			
+			jQuery("#purge-data-submit").attr("disabled", "disabled");
+			jQuery("#purge-data-result").html("<img src='<?php echo plugins_url('wp-statistics'); ?>/images/loading.gif'/>");
+			
+			jQuery.post("<?php echo parse_url(plugins_url('purge-data.php', __FILE__), PHP_URL_PATH ); ?>", {purge_days:data['purge-days']})
+				.done(function(result){
+				jQuery("#purge-data-result").html(result);
+				jQuery("#purge-data-submit").removeAttr("disabled");
+			});
+		});
+
+		jQuery("#empty-table-submit").click(function(){
+		
+			var action = jQuery('#empty-table').val();
+			
+			if(action == 0)
+				return false;
+				
+			var agree = confirm('<?php _e('Are you sure?', 'wp_statistics'); ?>');
+
+			if(!agree)
+				return false;
+				
+			var data = new Array();
+			data['table-name'] = jQuery("#empty-table").val();
+			
+			
+			jQuery("#empty-table-submit").attr("disabled", "disabled");
+			jQuery("#empty-result").html("<img src='<?php echo plugins_url('wp-statistics'); ?>/images/loading.gif'/>");
+			
+			jQuery.post("<?php echo parse_url(plugins_url('empty.php', __FILE__), PHP_URL_PATH ); ?>", {table_name:data['table-name']})
+				.done(function(result){
+				jQuery("#empty-result").html(result);
+				jQuery("#empty-table-submit").removeAttr("disabled");
+			});
+		});
+
+		jQuery("#delete-agents-submit").click(function(){
+		
+			var action = jQuery('#delete-agent').val();
+			
+			if(action == 0)
+				return false;
+				
+			var agree = confirm('<?php _e('Are you sure?', 'wp_statistics'); ?>');
+
+			if(!agree)
+				return false;
+				
+			var data = new Array();
+			data['agent-name'] = jQuery("#delete-agent").val();
+			
+			
+			jQuery("#delete-agents-submit").attr("disabled", "disabled");
+			jQuery("#delete-agents-result").html("<img src='<?php echo plugins_url('wp-statistics'); ?>/images/loading.gif'/>");
+	
+			jQuery.post("<?php echo parse_url(plugins_url('delete-agents.php', __FILE__), PHP_URL_PATH ); ?>", {agent_name:data['agent-name']})
+				.done(function(result){
+					jQuery("#delete-agents-result").html(result);
+					jQuery("#delete-agents-submit").removeAttr("disabled");
+					aid = data['agent-name'].replace(/[^a-zA-Z]/g, "");
+					jQuery("#agent-" + aid + "-id").remove();
+			});
+		});		
+
+		jQuery("#delete-platforms-submit").click(function(){
+		
+			var action = jQuery('#delete-platform').val();
+			
+			if(action == 0)
+				return false;
+				
+			var agree = confirm('<?php _e('Are you sure?', 'wp_statistics'); ?>');
+
+			if(!agree)
+				return false;
+				
+			var data = new Array();
+			data['platform-name'] = jQuery("#delete-platform").val();
+			
+			
+			jQuery("#delete-platforms-submit").attr("disabled", "disabled");
+			jQuery("#delete-platforms-result").html("<img src='<?php echo plugins_url('wp-statistics'); ?>/images/loading.gif'/>");
+	
+			jQuery.post("<?php echo parse_url(plugins_url('delete-platforms.php', __FILE__), PHP_URL_PATH ); ?>", {platform_name:data['platform-name']})
+				.done(function(result){
+				jQuery("#delete-platforms-result").html(result);
+				jQuery("#delete-platforms-submit").removeAttr("disabled");
+				pid = data['platform-name'].replace(/[^a-zA-Z]/g, "");
+				jQuery("#platform-" + pid + "-id").remove();
+			});
+		});		
+
+	});
+</script>
+<?php
+	if( array_key_exists( 'populate', $_GET ) ) {
+		if( $_GET['populate'] == 1 ) {
+			require_once( plugin_dir_path( __FILE__ ) . '../functions/geoip-populate.php' );
+			echo wp_statistics_populate_geoip_info();
+		}
+	}
+?>
+<a name="top"></a>
+<div class="wrap">
+    <?php screen_icon('options-general'); ?>
+    <h2><?php echo get_admin_page_title(); ?></h2>
+	<br>
+	<a href="#resources"><?php _e('Resources', 'wp_statistics'); ?></a> | <a href="#versioninfo"><?php _e('Version Info', 'wp_statistics'); ?></a> | <a href="#clientinfo"><?php _e('Client Info', 'wp_statistics'); ?></a> | <a href="#export"><?php _e('Export', 'wp_statistics'); ?></a> | <a href="#empty"><?php _e('Data', 'wp_statistics'); ?></a> | <a href="#deleteuseragenttypes"><?php _e('Delete User Agent Types', 'wp_statistics'); ?></a> | <a href="#geoipoptions"><?php _e('GeoIP Options', 'wp_statistics'); ?></a>
+	
+	<form method="post" action="<?php echo plugins_url('export.php', __FILE__); ?>">
+	<table class="form-table">
+		<tbody>
+			<?php settings_fields('wps_settings'); ?>
+			<tr valign="top">
+				<th scope="row" colspan="2"><a name="resources" href="#top" style='text-decoration: none;'><h3><?php _e('Resources', 'wp_statistics'); ?></h3></a></th>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">
+					<?php _e('Memory usage in PHP', 'wp_statistics'); ?>:
+				</th>
+				
+				<td>
+					<strong><?php echo number_format(memory_get_usage()); ?></strong> <?php _e('Byte', 'wp_statistics'); ?>
+					<p class="description"><?php _e('Memory usage in PHP', 'wp_statistics'); ?></p>
+				</td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">
+					<?php echo sprintf(__('Number of rows in the <code>%sstatistics_useronline</code> table', 'wp_statistics'), $table_prefix); ?>:
+				</th>
+				
+				<td>
+					<strong><?php echo $result['useronline']; ?></strong> <?php _e('Row', 'wp_statistics'); ?>
+					<p class="description"><?php _e('Number of rows', 'wp_statistics'); ?></p>
+				</td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">
+					<?php echo sprintf(__('Number of rows in the <code>%sstatistics_visit</code> table', 'wp_statistics'), $table_prefix); ?>:
+				</th>
+				
+				<td>
+					<strong><?php echo $result['visit']; ?></strong> <?php _e('Row', 'wp_statistics'); ?>
+					<p class="description"><?php _e('Number of rows', 'wp_statistics'); ?></p>
+				</td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">
+					<?php echo sprintf(__('Number of rows in the <code>%sstatistics_visitor</code> table', 'wp_statistics'), $table_prefix); ?>:
+				</th>
+				
+				<td>
+					<strong><?php echo $result['visitor']; ?></strong> <?php _e('Row', 'wp_statistics'); ?>
+					<p class="description"><?php _e('Number of rows', 'wp_statistics'); ?></p>
+				</td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row" colspan="2"><a name="versioninfo" href="#top" style='text-decoration: none;'><h3><?php _e('Version Info', 'wp_statistics'); ?></h3></a></th>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">
+					<?php _e('WP Statistics Version', 'wp_statistics'); ?>:
+				</th>
+				
+				<td>
+					<strong><?php echo WP_STATISTICS_VERSION; ?></strong>
+					<p class="description"><?php _e('The WP Statistics version you are running.', 'wp_statistics'); ?></p>
+				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row">
+					<?php _e('PHP Version', 'wp_statistics'); ?>:
+				</th>
+				
+				<td>
+					<strong><?php echo phpversion(); ?></strong>
+					<p class="description"><?php _e('The PHP version you are running.', 'wp_statistics'); ?></p>
+				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row">
+					<?php _e('jQuery Version', 'wp_statistics'); ?>:
+				</th>
+				
+				<td>
+					<strong><script type="text/javascript">document.write(jQuery().jquery);</script></strong>
+					<p class="description"><?php _e('The jQuery version you are running.', 'wp_statistics'); ?></p>
+				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row" colspan="2"><a name="clientinfo" href="#top" style='text-decoration: none;'><h3><?php _e('Client Info', 'wp_statistics'); ?></h3></a></th>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">
+					<?php _e('Client IP', 'wp_statistics'); ?>:
+				</th>
+				
+				<td>
+					<strong><?php $wpstats = new WP_Statistics(); echo $wpstats->get_IP(); ?></strong>
+					<p class="description"><?php _e('The client IP address.', 'wp_statistics'); ?></p>
+				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row">
+					<?php _e('User Agent', 'wp_statistics'); ?>:
+				</th>
+				
+				<td>
+					<strong><?php echo $_SERVER['HTTP_USER_AGENT']; ?></strong>
+					<p class="description"><?php _e('The client user agent string.', 'wp_statistics'); ?></p>
+				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row" colspan="2"><a name="export" href="#top" style='text-decoration: none;'><h3><?php _e('Export', 'wp_statistics'); ?></h3></a></th>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">
+					<label for="table-to-export"><?php _e('Export from', 'wp_statistics'); ?>:</label>
+				</th>
+				
+				<td>
+					<select id="table-to-export" name="table-to-export">
+						<option value="0"><?php _e('Please select.', 'wp_statistics'); ?></option>
+						<option value="useronline"><?php echo $table_prefix . 'statistics_useronline'; ?></option>
+						<option value="visit"><?php echo $table_prefix . 'statistics_visit'; ?></option>
+						<option value="visitor"><?php echo $table_prefix . 'statistics_visitor'; ?></option>
+					</select>
+					<p class="description"><?php _e('Select the table for the output file.', 'wp_statistics'); ?></p>
+				</td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">
+					<label for="export-file-type"><?php _e('Export To', 'wp_statistics'); ?>:</label>
+				</th>
+				
+				<td>
+					<select id="export-file-type" name="export-file-type">
+						<option value="0"><?php _e('Please select.', 'wp_statistics'); ?></option>
+						<option value="excel">Excel</option>
+						<option value="xml">XML</option>
+						<option value="csv">CSV</option>
+						<option value="tsv">TSV</option>
+					</select>
+					<p class="description"><?php _e('Select the output file type.', 'wp_statistics'); ?></p>
+					<?php submit_button(__('Start Now!', 'wp_statistics'), 'primary', 'export-file-submit'); ?>
+				</td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row" colspan="2"><a name="empty" href="#top" style='text-decoration: none;'><h3><?php _e('Data', 'wp_statistics'); ?></h3></a></th>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">
+					<label for="empty-table"><?php _e('Empty Table', 'wp_statistics'); ?>:</label>
+				</th>
+				
+				<td>
+					<select id="empty-table" name="empty-table">
+						<option value="0"><?php _e('Please select.', 'wp_statistics'); ?></option>
+						<option value="useronline"><?php echo $table_prefix . 'statistics_useronline'; ?></option>
+						<option value="visit"><?php echo $table_prefix . 'statistics_visit'; ?></option>
+						<option value="visitor"><?php echo $table_prefix . 'statistics_visitor'; ?></option>
+						<option value="all"><?php echo __('All','wp_statistics'); ?></option>
+					</select>
+					<p class="description"><?php _e('All data table will be lost.', 'wp_statistics'); ?></p>
+					<input id="empty-table-submit" class="button button-primary" type="submit" value="<?php _e('Clear now!', 'wp_statistics'); ?>" name="empty-table-submit" Onclick="return false;"/>
+					
+					<span id="empty-result"></span>
+				</td>
+			</tr>
+			
+			<tr>
+				<th scope="row">
+					<label for="purge-data"><?php _e('Purge records older than', 'wp_statistics'); ?>:</label>
+				</th>
+
+				<td>
+					<input type="text" class="small-text code" id="purge-data" name="wps_purge_data" value="365"/>
+					<label for="purge-data"><?php _e('days', 'wp_statistics'); ?></label>
+					<p class="description"><?php _e('Deleted user statistics data older than the selected number of days.  Minimum value is 30 days.', 'wp_statistics'); ?></p>
+					<input id="purge-data-submit" class="button button-primary" type="submit" value="<?php _e('Purge now!', 'wp_statistics'); ?>" name="purge-data-submit" Onclick="return false;"/>
+
+					<span id="purge-data-result"></span>
+				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row" colspan="2"><a name="deleteuseragenttypes" href="#top" style='text-decoration: none;'><h3><?php _e('Delete User Agent Types', 'wp_statistics'); ?></h3></a></th>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">
+					<label for="delete-agent"><?php _e('Delete Agents', 'wp_statistics'); ?>:</label>
+				</th>
+				
+				<td>
+					<select id="delete-agent" name="delete-agent">
+						<option value="0"><?php _e('Please select.', 'wp_statistics'); ?></option>
+						<?php
+							$agents = wp_statistics_ua_list();
+							
+							foreach($agents as $agent) {
+								$aid = preg_replace( "/[^a-zA-Z]/", "", $agent );
+								echo "<option value='$agent' id='agent-" . $aid . "-id'>" . __($agent, 'wp_statistics') . "</option>";
+							}
+						?>
+					</select>
+					<p class="description"><?php _e('All visitor data will be lost for this agent type.', 'wp_statistics'); ?></p>
+					<input id="delete-agents-submit" class="button button-primary" type="submit" value="<?php _e('Delete now!', 'wp_statistics'); ?>" name="delete-agents-submit" Onclick="return false;">
+					
+					<span id="delete-agents-result"></span>
+				</td>
+			</tr>
+
+			<tr valign="top">
+				<th scope="row">
+					<label for="delete-platform"><?php _e('Delete Platforms', 'wp_statistics'); ?>:</label>
+				</th>
+				
+				<td>
+					<select id="delete-platform" name="delete-platform">
+						<option value="0"><?php _e('Please select.', 'wp_statistics'); ?></option>
+						<?php
+							$platforms = wp_statistics_platform_list();
+							
+							foreach($platforms as $platform) {
+								$pid = preg_replace( "/[^a-zA-Z]/", "", $platform );
+								echo "<option value='$platform' id='platform-" . $pid . "-id'>" . __($platform, 'wp_statistics') . "</option>";
+							}
+						?>
+					</select>
+					<p class="description"><?php _e('All visitor data will be lost for this platform type.', 'wp_statistics'); ?></p>
+					<input id="delete-platforms-submit" class="button button-primary" type="submit" value="<?php _e('Delete now!', 'wp_statistics'); ?>" name="delete-platforms-submit" Onclick="return false;">
+					
+					<span id="delete-platforms-result"></span>
+				</td>
+			</tr>
+
+
+			<tr valign="top">
+				<th scope="row" colspan="2"><a name="geoipoptions" href="#top" style='text-decoration: none;'><h3><?php _e('GeoIP Options', 'wp_statistics'); ?></h3></a></th>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">
+					<label for="populate-submit"><?php _e('Countries', 'wp_statistics'); ?>:</label>
+				</th>
+				
+				<td>
+					<input id="populate-submit" class="button button-primary" type="button" value="<?php _e('Update Now!', 'wp_statistics'); ?>" name="populate-submit" onclick="location.href=document.URL+'&populate=1'">
+					<p class="description"><?php _e('Get updates for the location and the countries, this may take a while', 'wp_statistics'); ?></p>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	</form>
+</div>
