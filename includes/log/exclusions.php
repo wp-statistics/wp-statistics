a1cb16355bcdd1cfce28229f0dd0daaa153297a7
Mostafa Soufi <mostafa.soufi@hotmail.com>
2014-03-02 07:48:48 +0000

    Update new version.

---
+++ includes/log/exclusions.php
@@ -0,0 +1,145 @@
+<script type="text/javascript">
+	jQuery(document).ready(function(){
+		postboxes.add_postbox_toggles(pagenow);
+	});
+</script>
+<?php
+	if( get_option( 'wps_record_exclusions' ) != 1 ) {
+		echo "<div class='updated settings-error'><p><strong>" . __('Attention: Exclusion are not currently set to be recorded, the results below may not reflect current statistics!', 'wp_statistics') . "</strong></p></div>";
+	}
+
+	$daysToDisplay = 20; if( array_key_exists('hitdays',$_GET) ) { if( $_GET['hitdays'] > 0 ) { $daysToDisplay = $_GET['hitdays']; } }
+	
+	$excluded_reasons = array('Robot','IP Match','Self Referral','Login Page','Admin Page','User Role');
+	$excluded_results = array();
+	$excluded_total = 0;
+	
+	foreach( $excluded_reasons as $reason ) {
+		for( $i=$daysToDisplay; $i>=0; $i--) {
+			$thisdate = date('Y-m-d', strtotime('-'.$i." day") );
+			$thisreason = strtolower( $reason );
+			$query = "SELECT count FROM {$wpdb->prefix}statistics_exclusions WHERE reason = '{$thisreason}' AND date = '{$thisdate}'";
+			$excluded_results[$reason][$i] = $wpdb->get_var( $query );
+			if( $excluded_results[$reason][$i] < 1 ) { $excluded_results[$reason][$i] = 0; }
+			$excluded_total += $excluded_results[$reason][$i];
+		}
+	}
+?>
+<div class="wrap">
+	<?php screen_icon('options-general'); ?>
+	<h2><?php _e('Hit Statistics', 'wp_statistics'); ?></h2>
+
+	<ul class="subsubsub">
+		<li class="all"><a <?php if($daysToDisplay == 10) { echo 'class="current"'; } ?>href="?page=wps_exclusions_menu&hitdays=10"><?php _e('10 Days', 'wp_statistics'); ?></a></li>
+		| <li class="all"><a <?php if($daysToDisplay == 20) { echo 'class="current"'; } ?>href="?page=wps_exclusions_menu&hitdays=20"><?php _e('20 Days', 'wp_statistics'); ?></a></li>
+		| <li class="all"><a <?php if($daysToDisplay == 30) { echo 'class="current"'; } ?>href="?page=wps_exclusions_menu&hitdays=30"><?php _e('30 Days', 'wp_statistics'); ?></a></li>
+		| <li class="all"><a <?php if($daysToDisplay == 60) { echo 'class="current"'; } ?>href="?page=wps_exclusions_menu&hitdays=60"><?php _e('2 Months', 'wp_statistics'); ?></a></li>
+		| <li class="all"><a <?php if($daysToDisplay == 90) { echo 'class="current"'; } ?>href="?page=wps_exclusions_menu&hitdays=90"><?php _e('3 Months', 'wp_statistics'); ?></a></li>
+		| <li class="all"><a <?php if($daysToDisplay == 180) { echo 'class="current"'; } ?>href="?page=wps_exclusions_menu&hitdays=180"><?php _e('6 Months', 'wp_statistics'); ?></a></li>
+		| <li class="all"><a <?php if($daysToDisplay == 270) { echo 'class="current"'; } ?>href="?page=wps_exclusions_menu&hitdays=270"><?php _e('9 Months', 'wp_statistics'); ?></a></li>
+		| <li class="all"><a <?php if($daysToDisplay == 365) { echo 'class="current"'; } ?>href="?page=wps_exclusions_menu&hitdays=365"><?php _e('1 Year', 'wp_statistics'); ?></a></li>
+	</ul>
+
+	<br><br>
+	<h3><?php echo sprintf(__('Total Exclusions: %s', 'wp_statistics'), $excluded_total); ?></h3>
+	
+	<div class="postbox-container" style="width: 100%; float: left; margin-right:20px">
+		<div class="metabox-holder">
+			<div class="meta-box-sortables">
+				<div class="postbox">
+					<div class="handlediv" title="<?php _e('Click to toggle', 'wp_statistics'); ?>"><br /></div>
+					<h3 class="hndle"><span><?php _e('Exclusions Statistical Chart', 'wp_statistics'); ?></span></h3>
+					<div class="inside">
+						<script type="text/javascript">
+						var visit_chart;
+						jQuery(document).ready(function() {
+							visit_chart = new Highcharts.Chart({
+								chart: {
+									renderTo: 'exclusion-stats',
+									type: '<?php echo get_option('wps_chart_type'); ?>',
+									backgroundColor: '#FFFFFF',
+									height: '600'
+								},
+								credits: {
+									enabled: false
+								},
+								title: {
+									text: '<?php echo __('Excluded hits chart in the last', 'wp_statistics') . ' ' . $daysToDisplay . ' ' . __('days', 'wp_statistics'); ?>',
+									style: {
+										fontSize: '12px',
+										fontFamily: 'Tahoma',
+										fontWeight: 'bold'
+									}
+								},
+								xAxis: {
+									type: 'datetime',
+									labels: {
+										rotation: -45,
+										step: <?php echo round($daysToDisplay/20);?>
+										},
+									categories: [
+									<?php
+										for( $i=$daysToDisplay; $i>=0; $i--) {
+											echo '"'.$wpstats->Current_Date_i18n('Y-m-d', '-'.$i).'"';
+											if( $i > 0 ) { echo ", "; }
+										}
+									?>]
+								},
+								yAxis: {
+									min: 0,
+									title: {
+										text: '<?php _e('Number of excluded hits', 'wp_statistics'); ?>',
+										style: {
+											fontSize: '12px',
+											fontFamily: 'Tahoma'
+										}
+									}
+								},
+								<?php if( is_rtl() ) { ?>
+								legend: {
+									rtl: true,
+									itemStyle: {
+											fontSize: '11px',
+											fontFamily: 'Tahoma'
+										}
+								},
+								<?php } ?>
+								tooltip: {
+									crosshairs: true,
+									shared: true,
+									style: {
+										fontSize: '12px',
+										fontFamily: 'Tahoma'
+									},
+									useHTML: true
+								},
+								series: [
+									<?php
+									
+									foreach( $excluded_reasons as $reason ) {
+									
+										echo "{\n";
+										echo "name: '" . __($reason, 'wp_statistics') . "',\n";
+										echo "data: [";
+
+										for( $i=$daysToDisplay; $i>=0; $i--) {
+											echo $excluded_results[$reason][$i];
+											if( $i > 0 ) { echo ", "; }
+										}
+										echo "]\n";
+										echo "								},\n";
+									}
+									?>
+								]
+							});
+						});
+						</script>
+						
+						<div id="exclusion-stats"></div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ includes/log/last-search.php
@@ -0,0 +1,128 @@
+<script type="text/javascript">
+	jQuery(document).ready(function(){
+		jQuery('.show-map').click(function(){
+			alert('<?php _e('To be added soon', 'wp_statistics'); ?>');
+		});
+
+	postboxes.add_postbox_toggles(pagenow);
+	});
+</script>
+<?php
+	$search_engines = wp_statistics_searchengine_list();
+	
+	$search_result['All'] = wp_statistics_searchword('all','total');
+
+	foreach( $search_engines as $key => $se ) {
+		$search_result[$key] = wp_statistics_searchword($key,'total');
+	}
+	
+	if( array_key_exists('referred',$_GET) ) {
+		if( $_GET['referred'] != '' ) {
+			$referred = $_GET['referred'];
+		}
+		else {
+			$referred = 'All';
+		}
+	}
+	else {
+		$referred = 'All';
+	}
+	
+	$total = $search_result[$referred];
+?>
+<div class="wrap">
+	<?php screen_icon('options-general'); ?>
+	<h2><?php _e('Latest search words', 'wp_statistics'); ?></h2>
+	<ul class="subsubsub">
+<?php
+		$search_result_count = count( $search_result );
+		$i = 0;
+		$separator = '|';
+		
+		foreach( $search_result as $key => $value ) {
+			$i++;
+			
+			if( $i == $search_result_count ) { $separator = ''; }
+			
+			if( $key == 'All' )
+				{
+				$tag = '';
+				$name = 'All';
+				}
+			else
+				{
+				$tag = $search_engines[$key]['tag'];
+				$name = $search_engines[$key]['name'];
+				}
+			
+			echo "<li><a href='?page=wps_words_menu&referred={$tag}'>" . __($name, 'wp_statistics') . " <span class='count'>({$value})</span></a>{$separator}</li>";
+		}
+?>
+	</ul>
+	<div class="postbox-container" id="last-log">
+		<div class="metabox-holder">
+			<div class="meta-box-sortables">
+				<div class="postbox">
+					<div class="handlediv" title="<?php _e('Click to toggle', 'wp_statistics'); ?>"><br /></div>
+					<h3 class="hndle"><span><?php _e('Latest search words', 'wp_statistics'); ?></span></h3>
+					<div class="inside">
+						<div class='log-latest'>
+							<?php
+								if( $total > 0 ) {
+									$wpstats = new WP_Statistics();
+									
+									// Instantiate pagination object with appropriate arguments
+									$pagesPerSection = 10;
+									$options = array(25, "All");
+									$stylePageOff = "pageOff";
+									$stylePageOn = "pageOn";
+									$styleErrors = "paginationErrors";
+									$styleSelect = "paginationSelect";
+
+									$Pagination = new Pagination($total, $pagesPerSection, $options, false, $stylePageOff, $stylePageOn, $styleErrors, $styleSelect);
+									
+									$start = $Pagination->getEntryStart();
+									$end = $Pagination->getEntryEnd();
+
+									// Retrieve MySQL data
+									if( $referred && $referred != '') {
+										$search_query = wp_statistics_searchword_query($referred);
+									} else {
+										$search_query = wp_statistics_searchword_query('all');
+									}
+									
+									$result = $wpdb->get_results("SELECT * FROM `{$table_prefix}statistics_visitor` WHERE {$search_query} ORDER BY `{$table_prefix}statistics_visitor`.`ID` DESC  LIMIT {$start}, {$end}");
+									
+									foreach($result as $items) {
+									
+										if( !$wpstats->Search_Engine_QueryString($items->referred) ) continue;
+										
+										echo "<div class='log-item'>";
+											echo "<div class='log-referred'>".substr($wpstats->Search_Engine_QueryString($items->referred), 0, 100)."</div>";
+											echo "<div class='log-ip'>{$items->last_counter} - <a href='http://www.geoiptool.com/en/?IP={$items->ip}' target='_blank'>{$items->ip}</a></div>";
+											echo "<div class='clear'></div>";
+											echo "<a class='show-map'><img src='".plugins_url('wp-statistics/assets/images/map.png')."' class='log-tools' title='".__('Map', 'wp_statistics')."'/></a>";
+											
+											$this_search_engine = $wpstats->Search_Engine_Info($items->referred);
+											echo "<a href='?page=wps_words_menu&referred={$this_search_engine['tag']}'><img src='".plugins_url('wp-statistics/assets/images/' . $this_search_engine['image'])."' class='log-tools' title='".__($this_search_engine['name'], 'wp_statistics')."'/></a>";
+											
+											echo "<a href='?page=wp-statistics/wp-statistics.php&type=last-all-visitor&agent={$items->agent}'><img src='".plugins_url('wp-statistics/assets/images/').$items->agent.".png' class='log-tools' title='{$items->agent}'/></a>";
+											echo "<div class='log-url'><a href='{$items->referred}'><img src='".plugins_url('wp-statistics/assets/images/link.png')."' title='{$items->referred}'/> ".substr($items->referred, 0, 100)."[...]</a></div>";
+										echo "</div>";
+									}
+								}
+
+								echo "</div>";
+							?>
+					</div>
+				</div>
+				
+				<div class="pagination-log">
+					<?php if( $total > 0 ) { echo $Pagination->display(); ?>
+					<p id="result-log"><?php echo ' ' . __('Page', 'wp_statistics') . ' ' . $Pagination->getCurrentPage() . ' ' . __('From', 'wp_statistics') . ' ' . $Pagination->getTotalPages(); ?></p>
+					<?php }?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
