d97d342f61c954d5423ce1c7d4faab13e932560e
Greg Ross <greg@toolstack.com>
2014-07-31 12:38:43 +0000

    - Removed objectToArray() function from functions.php as a) it is no longer used anywhere in the plugin and b) causes a conflict with the "amr users" plugin.

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -707,28 +707,6 @@
 		}
 	}
 	
-	// This function coverts a PHP object an an array.
-	function objectToArray($d) {
-		if (is_object($d)) {
-			// Gets the properties of the given object
-			// with get_object_vars function
-			$d = get_object_vars($d);
-		}
- 
-		if (is_array($d)) {
-			/*
-			* Return array converted to object
-			* Using __FUNCTION__ (Magic constant)
-			* for recursive call
-			*/
-			return array_map(__FUNCTION__, $d);
-		}
-		else {
-			// Return array
-			return $d;
-		}
-	}
-	
 	// This function gets a countries map coordinates (latitude/longitude).
 	function wp_statistics_get_gmap_coordinate($country, $coordinate) {
 	
