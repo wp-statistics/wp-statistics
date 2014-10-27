40f7c5d7b1a6e875e3bf4730af3590f446915d72
Greg Ross <greg@toolstack.com>
2014-10-27 22:42:02 +0000

    - Added code to remove the user settings during removal.

--- wps-uninstall.php
+++ wps-uninstall.php
@@ -7,6 +7,9 @@
 		delete_option('wp_statistics_db_version');
 		delete_option('wp_statistics_plugin_version');
 		
+		// Delete the user options.
+		$wpdb->query("DELETE FROM {$wp_prefix}usermeta WHERE meta_key LIKE 'wp_statistics%'");
+		
 		// Drop the tables
 		$wpdb->query("DROP TABLE IF EXISTS {$wp_prefix}statistics_useronline, {$wp_prefix}statistics_visit, {$wp_prefix}statistics_visitor, {$wp_prefix}statistics_exclusions, {$wp_prefix}statistics_pages, {$wp_prefix}statistics_historical");
 
