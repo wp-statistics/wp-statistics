2ee1d3e71f2465902ec297a4fcd8e94f4c19544d
Greg Ross <greg@toolstack.com>
2015-08-21 11:54:02 +0000

    - Updated purging code to include the search table.

--- includes/functions/purge.php
+++ includes/functions/purge.php
@@ -52,6 +52,17 @@
 				$result_string .= '<br>' . sprintf(__('No records found to purge from %s!', 'wp_statistics'), '<code>' . $table_name . '</code>' ); 
 			}
 
+			// Purge the search data.
+			$table_name = $wpdb->prefix . 'statistics_search';
+
+			$result = $wpdb->query('DELETE FROM ' . $table_name . ' WHERE `last_counter` < \'' . $date_string . '\'');
+			
+			if($result) {
+				$result_string .= '<br>' . sprintf(__('%s data older than %s days purged successfully.', 'wp_statistics'), '<code>' . $table_name . '</code>', '<code>' . $purge_days . '</code>');
+			} else {
+				$result_string .= '<br>' . sprintf(__('No records found to purge from %s!', 'wp_statistics'), '<code>' . $table_name . '</code>' ); 
+			}
+
 			// Purge the pages data, this is more complex as we want to save the historical data per page.
 			$table_name = $wpdb->prefix . 'statistics_pages';
 			$historical = 0;
