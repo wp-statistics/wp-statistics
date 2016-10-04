b274e5f57e1d80bdf828632b5c9f348949ff4616
Greg Ross <greg@toolstack.com>
2016-10-04 18:37:57 +0000

    - Fix display of top sites list when the display port is very small.

--- includes/log/widgets/pages.php
+++ includes/log/widgets/pages.php
@@ -1,24 +1,24 @@
 <?php
-	function wp_statistics_generate_pages_postbox_content($total, $uris) {
+	function wp_statistics_generate_pages_postbox_content( $total, $uris ) {
 	
-		echo "<div class='log-latest'>";
+		echo '<div class="log-latest">';
 		
 		$i = 0;
 		$site_url = site_url();
 		
-		foreach($uris as $uri) {
+		foreach( $uris as $uri ) {
 			$i++;
-			echo "<div class='log-item'>";
+			echo '<div class="log-item">';
 
-			if( empty($uri[3]) ) { $uri[3] = '[' . __('No page title found', 'wp_statistics') . ']'; }
+			if( empty( $uri[3] ) ) { $uri[3] = '[' . __( 'No page title found', 'wp_statistics' ) . ']'; }
 			
-			echo "<div class='log-page-title'>{$i} - {$uri[3]}</div>";
-			echo "<div class='right-div'>".__('Visits', 'wp_statistics').": <a href='?page=" . WP_STATISTICS_PAGES_PAGE . "&page-uri={$uri[0]}'>" . number_format_i18n($uri[1]) . "</a></div>";
-			echo "<div class='left-div'><a dir='ltr' href='{$site_url}{$uri[0]}'>".htmlentities(urldecode($uri[0]),ENT_QUOTES)."</a></div>";
-			echo "</div>";
+			echo "<div class=\"log-page-title\">{$i} - {$uri[3]}</div>";
+			echo '<div class="right-div">' . __( 'Visits', 'wp_statistics' ) . ': <a href="?page=' . WP_STATISTICS_PAGES_PAGE . '&page-uri=' . $uri[0] . '">' . number_format_i18n( $uri[1] ) . '</a></div>';
+			echo '<div><a href="' . $site_url . $uri[0] . '">' . htmlentities( urldecode( $uri[0] ), ENT_QUOTES ) . '</a></div>';
+			echo '</div>';
 			
 			if( $i > 9 ) { break; }
 		}
 		
-		echo "</div>";
+		echo '</div>';
 	}
