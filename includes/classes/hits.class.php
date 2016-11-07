bf3813235d6a13c87aa7b6e45baf61cb8637ab4b
Greg Ross <greg@toolstack.com>
2016-11-07 18:29:48 +0000

    - Add additional error checking on new IP code.

--- includes/classes/hits.class.php
+++ includes/classes/hits.class.php
@@ -51,14 +51,29 @@ class Hits extends WP_Statistics {
 		}
 
 		// Create a IP Tools instance from the current IP address for use later.
-		$ip = new IP( $this->ip );
+		// Fall back to the localhost if it can't be parsed.
+		try {
+			$ip = new IP( $this->ip );
+		}
+		catch( Exception $e ) {
+			$ip = new IP( '127.0.0.1' );
+		}
 
 		// Let's check to see if our subnet matches a private IP address range, if so go ahead and set the location information now.
 		if( $this->get_option( 'private_country_code' ) != '000' && $this->get_option( 'private_country_code' ) != '' ) {
 			$private_subnets = array( '10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16', '127.0.0.1/24', 'fc00::/7' );
 
 			foreach( $private_subnets as $psub ) {
-				if( Range::parse( $psub )->contains( $ip ) ) {
+				$contains_ip = false;
+
+				try {
+					$contains_ip = Range::parse( $psub )->contains( $ip );
+				}
+				catch( Exception $e ) {
+					$contains_ip = false;
+				}
+
+				if( $contains_ip ) {
 					$this->location = $this->get_option( 'private_country_code' );
 					break;
 				}
@@ -156,7 +171,16 @@ class Hits extends WP_Statistics {
 
 				// The shortest ip address is 1.1.1.1, anything less must be a malformed entry.
 				if( strlen( $subnet ) > 6 ) {
-					if( Range::parse( $subnet )->contains( $ip ) ) {
+					$range_prased = false;
+
+					try {
+						$range_prased = Range::parse( $subnet )->contains( $ip );
+					}
+					catch( Exception $e ) {
+						$range_parased = false;
+					}
+
+					if( $range_prased ) {
 						$this->exclusion_match = TRUE;
 						$this->exclusion_reason = 'ip match';
 
