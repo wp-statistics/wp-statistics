394c997a36a2210a6d6ae5806647d4cab9e0e5d4
Mostafa Soufi <mostafa.soufi@hotmail.com>
2017-09-28 11:47:49 +0330

    Complete newsletter postbox

--- includes/templates/postbox.php
+++ includes/templates/postbox.php
@@ -4,7 +4,7 @@
             <h2 class="hndle"><span>Subscribe to Newsletter</span></h2>
             <div class="inside">
                 <form action="//veronalabs.us7.list-manage.com/subscribe/post?u=628dc1468c7c7db0ba42f0137&amp;id=ea320ea877" method="post" name="mc-embedded-subscribe-form" target="_blank" novalidate>
-                    <p></p>
+                    <p><?php _e( 'Subscribe to our mailing list for get any news of the WP SMS plugin', 'wp-statistics' ); ?></p>
                     <input name="EMAIL" type="email" class="ltr" value="<?php bloginfo( 'admin_email' ); ?>">
                     <input type="hidden" name="b_628dc1468c7c7db0ba42f0137_ea320ea877" tabindex="-1" value="">
                     <input type="submit" value="<?php _e( 'Subscribe', 'wp-statistics' ); ?>" name="subscribe" class="button">
