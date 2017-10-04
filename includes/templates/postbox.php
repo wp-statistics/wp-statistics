ab25cd6593a9838f0a7693e888b5045ea78191ab
pedro-mendonca <ped.gaspar@gmail.com>
2017-10-04 10:39:45 +0100

    Add missing gettext function

--- includes/templates/postbox.php
+++ includes/templates/postbox.php
@@ -1,7 +1,7 @@
 <div id="postbox-container-1" class="postbox-container">
     <div class="meta-box-sortables">
         <div id="wps-plugins-support" class="postbox">
-            <h2 class="hndle"><span>Subscribe to Newsletter</span></h2>
+            <h2 class="hndle"><span><?php _e( 'Subscribe to Newsletter', 'wp-statistics' ); ?></span></h2>
             <div class="inside">
                 <form action="//veronalabs.us7.list-manage.com/subscribe/post?u=628dc1468c7c7db0ba42f0137&amp;id=ea320ea877" method="post" name="mc-embedded-subscribe-form" target="_blank" novalidate>
                     <p><?php _e( 'Subscribe to our mailing list for get any news of the WP-Statistics', 'wp-statistics' ); ?></p>
