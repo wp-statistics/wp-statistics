b32a7dd0fbb6461d48fe4938189563bb4c35de8d
Mostafa Soufi <mostafa.soufi@hotmail.com>
2017-12-02 20:00:42 +0330

    Rollback instance ip

--- includes/classes/class-wp-statistics-hits.php
+++ includes/classes/class-wp-statistics-hits.php
@@ -46,11 +46,11 @@ class WP_Statistics_Hits {
 		// Create a IP Tools instance from the current IP address for use later.
 		// Fall back to the localhost if it can't be parsed.
 		try {
-			$ip = (string)IP::parse($WP_Statistics->ip);
+			$ip = new IP($WP_Statistics->ip);
 		} catch ( Exception $e ) {
-			$ip = (string)IP::parse('127.0.0.1');
+			$ip = new IP('127.0.0.1');
 		}
-
+		
 		// Let's check to see if our subnet matches a private IP address range, if so go ahead and set the location information now.
 		if ( $WP_Statistics->get_option('private_country_code') != '000' &&
 		     $WP_Statistics->get_option('private_country_code') != ''
