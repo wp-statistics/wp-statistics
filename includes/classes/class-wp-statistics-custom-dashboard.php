a1a14c8b9c56485aa23da9359dfc0debddfc75e0
Mostafa Soufi <mostafa.soufi@hotmail.com>
2017-12-11 00:09:46 +0330

    Added custom dashboard class

---
+++ includes/classes/class-wp-statistics-custom-dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Class WP_Statistics_Custom_Dashboard
+ */
+class WP_Statistics_Custom_Dashboard {
+	/**
+	 * WP_Statistics_Custom_Dashboard constructor.
+	 */
+	public function __construct() {
+	}
+}
