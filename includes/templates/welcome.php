efb5df1e44b30688acb5fb4c4ffb0ad88454aff8
Mostafa Soufi <mostafa.soufi@hotmail.com>
2017-12-30 20:29:46 +0330

    Add i18n functions to welcome page

--- includes/templates/welcome.php
+++ includes/templates/welcome.php
@@ -1,19 +1,14 @@
 <div class="wrap about-wrap full-width-layout">
     <div class="wp-statistics-welcome">
         <h1><?php printf( __( 'Welcome to WP-Statistics&nbsp;%s', 'wp-statistics' ), WP_Statistics::$reg['version'] ); ?></h1>
-        <div class="notice notice-success is-dismissible"><p>Real-Time stats available!
-                <a href="admin.php?page=wp_statistics_realtime_stats">Click here</a> to show it.</p>
-            <button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span>
-            </button>
-        </div>
 
         <p class="about-text"><?php _e( 'Thank you for updating to the latest version!', 'wp-statistics' ); ?></p>
         <div class="wp-badge"><?php printf( __( 'Version %s', 'wp-statistics' ), WP_Statistics::$reg['version'] ); ?></div>
 
         <h2 class="nav-tab-wrapper wp-clearfix">
-            <a href="#" class="nav-tab nav-tab-active" data-tab="whats-news">What’s New</a>
-            <a href="#" class="nav-tab" data-tab="credit">Credits</a>
-            <a href="#" class="nav-tab" data-tab="changelog">Changelog</a>
+            <a href="#" class="nav-tab nav-tab-active" data-tab="whats-news"><?php _e( 'What’s New', 'wp-statistics' ); ?></a>
+            <a href="#" class="nav-tab" data-tab="credit"><?php _e( 'Credits', 'wp-statistics' ); ?></a>
+            <a href="#" class="nav-tab" data-tab="changelog"><?php _e( 'Changelog', 'wp-statistics' ); ?></a>
         </h2>
 
         <div data-content="whats-news" class="tab-content current">
@@ -22,42 +17,36 @@
 
         <div data-content="credit" class="tab-content">
             <div class="about-wrap-content">
-                <p class="about-description">WP-Statistics is created by some peoples and is one of
-                    <a target="_blank" href="http://veronalabs.com">VeronaLabs.com</a> projects.</p>
-                <h3 class="wp-people-group">Project Leaders</h3>
+                <p class="about-description"><?php echo sprintf( __( 'WP-Statistics is created by some peoples and is one of <a target="_blank" href="%s">VeronaLabs.com</a> projects.', 'wp-statistics' ), 'http://veronalabs.com' ); ?></p>
+                <h3 class="wp-people-group"><?php _e( 'Project Leaders', 'wp-statistics' ); ?></h3>
                 <ul class="wp-people-group ">
                     <li class="wp-person">
-                        <a href="https://profiles.wordpress.org/mostafas1990/" class="web"><?php echo get_avatar( 'mst404@gmail.com', 62, '', '', array( 'class' => 'gravatar' ) ); ?>
-                            Mostafa Soufi</a>
-                        <span class="title">Original Author</span>
+                        <a href="https://profiles.wordpress.org/mostafas1990/" class="web"><?php echo get_avatar( 'mst404@gmail.com', 62, '', '', array( 'class' => 'gravatar' ) ); ?><?php _e( 'Mostafa Soufi', 'wp-statistics' ); ?></a>
+                        <span class="title"><?php _e( 'Original Author', 'wp-statistics' ); ?></span>
                     </li>
                 </ul>
-                <h3 class="wp-people-group">Other Contributors</h3>
+                <h3 class="wp-people-group"><?php _e( 'Other Contributors', 'wp-statistics' ); ?></h3>
                 <ul class="wp-people-group">
                     <li class="wp-person">
-                        <a href="https://profiles.wordpress.org/gregross/" class="web"><?php echo get_avatar( 'greg@toolstack.com', 62, '', '', array( 'class' => 'gravatar' ) ); ?>
-                            Greg Ross</a>
-                        <span class="title">Core contributor</span>
+                        <a href="https://profiles.wordpress.org/gregross/" class="web"><?php echo get_avatar( 'greg@toolstack.com', 62, '', '', array( 'class' => 'gravatar' ) ); ?><?php _e( 'Greg Ross', 'wp-statistics' ); ?></a>
+                        <span class="title"><?php _e( 'Core contributor', 'wp-statistics' ); ?></span>
                     </li>
                     <li class="wp-person">
-                        <a href="https://profiles.wordpress.org/dedidata/" class="web"><?php echo get_avatar( 'dedidata.com@gmail.com', 62, '', '', array( 'class' => 'gravatar' ) ); ?>
-                            Farhad Sakhaei</a>
-                        <span class="title">Core Contributor</span>
+                        <a href="https://profiles.wordpress.org/dedidata/" class="web"><?php echo get_avatar( 'dedidata.com@gmail.com', 62, '', '', array( 'class' => 'gravatar' ) ); ?><?php _e( 'Farhad Sakhaei', 'wp-statistics' ); ?></a>
+                        <span class="title"><?php _e( 'Core Contributor', 'wp-statistics' ); ?></span>
                     </li>
                     <li class="wp-person">
-                        <a href="https://profiles.wordpress.org/pedromendonca/" class="web"><?php echo get_avatar( 'ped.gaspar@gmail.com', 62, '', '', array( 'class' => 'gravatar' ) ); ?>
-                            Pedro Mendonça</a>
-                        <span class="title">Language Contributor</span>
+                        <a href="https://profiles.wordpress.org/pedromendonca/" class="web"><?php echo get_avatar( 'ped.gaspar@gmail.com', 62, '', '', array( 'class' => 'gravatar' ) ); ?><?php _e( 'Pedro Mendonça', 'wp-statistics' ); ?></a>
+                        <span class="title"><?php _e( 'Language Contributor', 'wp-statistics' ); ?></span>
                     </li>
                 </ul>
 
-                <p class="clear">WP-Statistics is being developed on GitHub, If you’re interested in contributing to
-                    plugin, Please look at <a target="_blank" href="https://github.com/wp-statistics/wp-statistics">Github
-                        page</a>.</p>
-
-                <h3 class="wp-people-group">External Libraries</h3>
-                <p class="wp-credits-list"><a href="http://www.maxmind.com/">MaxMind</a>,
-                    <a href="https://browscap.org/">Browscap</a>, <a href="http://www.chartjs.org/">Chart.js</a>.</p>
+                <p class="clear"><?php echo sprintf( __( 'WP-Statistics is being developed on GitHub, If you’re interested in contributing to plugin, Please look at <a target="_blank" href="%s">Github page</a>.', 'wp-statistics' ), 'https://github.com/wp-statistics/wp-statistics' ); ?></p>
+                <h3 class="wp-people-group"><?php _e( 'External Libraries', 'wp-statistics' ); ?></h3>
+                <p class="wp-credits-list">
+                    <a href="http://www.maxmind.com/"><?php _e( 'MaxMind', 'wp-statistics' ); ?></a>,
+                    <a href="https://browscap.org/">Browscap</a>,
+                    <a href="http://www.chartjs.org/"><?php _e( 'Chart.js', 'wp-statistics' ); ?></a>.</p>
             </div>
         </div>
 
@@ -68,7 +57,7 @@
         <hr>
 
         <div class="return-to-dashboard">
-            <a href="<?php echo admin_url( 'admin.php?page=wps_overview_page' ); ?>">Go to Stats → Overview</a>
+            <a href="<?php echo admin_url( 'admin.php?page=wps_overview_page' ); ?>"><?php _e( 'Go to Stats → Overview', 'wp-statistics' ); ?></a>
         </div>
     </div>
 </div>
