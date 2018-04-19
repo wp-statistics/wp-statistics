82a55f942e2e2010f44a6d873c548b013aa3ed4d
Mostafa Soufi <mostafa.soufi@hotmail.com>
2018-04-19 15:12:15 +0430

    Add allow_opt_out option for GDPR compliance

--- includes/settings/tabs/wps-privacy.php
+++ includes/settings/tabs/wps-privacy.php
@@ -1,25 +1,26 @@
 <?php
-if ( $wps_nonce_valid ) {
-	$wps_option_list = array(
-		'wps_store_ua',
-		'wps_hash_ips',
-		'wps_all_online',
-	);
+if ($wps_nonce_valid) {
+    $wps_option_list = array(
+        'wps_allow_opt_out',
+        'wps_hash_ips',
+        'wps_store_ua',
+        'wps_all_online',
+    );
 
-	// If the IP hash's are enabled, disable storing the complete user agent.
-	if ( array_key_exists( 'wps_hash_ips', $_POST ) ) {
-		$_POST['wps_store_ua'] = '';
-	}
+    // If the IP hash's are enabled, disable storing the complete user agent.
+    if (array_key_exists('wps_hash_ips', $_POST)) {
+        $_POST['wps_store_ua'] = '';
+    }
 
-	foreach ( $wps_option_list as $option ) {
-		if ( array_key_exists( $option, $_POST ) ) {
-			$value = $_POST[ $option ];
-		} else {
-			$value = '';
-		}
-		$new_option = str_replace( "wps_", "", $option );
-		$WP_Statistics->store_option( $new_option, $value );
-	}
+    foreach ($wps_option_list as $option) {
+        if (array_key_exists($option, $_POST)) {
+            $value = $_POST[$option];
+        } else {
+            $value = '';
+        }
+        $new_option = str_replace("wps_", "", $option);
+        $WP_Statistics->store_option($new_option, $value);
+    }
 }
 ?>
     <script type="text/javascript">
@@ -31,42 +32,57 @@ if ( $wps_nonce_valid ) {
     <table class="form-table">
         <tbody>
         <tr valign="top">
-            <th scope="row" colspan="2"><h3><?php _e( 'IP Addresses', 'wp-statistics' ); ?></h3></th>
+            <th scope="row" colspan="2"><h3><?php _e('Privacy and Data Protection', 'wp-statistics'); ?></h3></th>
         </tr>
 
         <tr valign="top">
             <th scope="row">
-                <label for="hash_ips"><?php _e( 'Hash IP Addresses', 'wp-statistics' ); ?>:</label>
+                <label for="allow_opt_out"><?php _e('Allow Opt-out', 'wp-statistics'); ?>:</label>
             </th>
 
             <td>
-                <input id="hash_ips" type="checkbox" value="1"
-                       name="wps_hash_ips" <?php echo $WP_Statistics->get_option( 'hash_ips' ) == true
-					? "checked='checked'" : ''; ?>>
-                <label for="hash_ips"><?php _e( 'Enable', 'wp-statistics' ); ?></label>
+                <input id="allow_opt_out" type="checkbox" value="1"
+                       name="wps_allow_opt_out" <?php echo $WP_Statistics->get_option('allow_opt_out') == true
+                    ? "checked='checked'" : ''; ?>>
+                <label for="allow_opt_out"><?php _e('Enable', 'wp-statistics'); ?></label>
 
-                <p class="description"><?php _e(
-						'This feature will not store IP addresses in the database but instead used a unique hash.  The "Store entire user agent string" setting will be disabled if this is selected.  You will not be able to recover the IP addresses in the future to recover location information if this is enabled.',
-						'wp-statistics'
-					); ?></p>
+                <p class="description"><?php echo sprintf(__('If you are in the Europe Union and want to comply with the <a href="%s">GDPR (General Data Protection Regulation)</a> rules, enable this option to show confirmation message for the visitors.', 'wp-statistics'), 'https://en.wikipedia.org/wiki/General_Data_Protection_Regulation'); ?></p>
             </td>
         </tr>
 
         <tr valign="top">
             <th scope="row">
-                <label for="store_ua"><?php _e( 'Store entire user agent string', 'wp-statistics' ); ?>:</label>
+                <label for="hash_ips"><?php _e('Hash IP Addresses', 'wp-statistics'); ?>:</label>
+            </th>
+
+            <td>
+                <input id="hash_ips" type="checkbox" value="1"
+                       name="wps_hash_ips" <?php echo $WP_Statistics->get_option('hash_ips') == true
+                    ? "checked='checked'" : ''; ?>>
+                <label for="hash_ips"><?php _e('Enable', 'wp-statistics'); ?></label>
+
+                <p class="description"><?php _e(
+                        'This feature will not store IP addresses in the database but instead used a unique hash.  The "Store entire user agent string" setting will be disabled if this is selected.  You will not be able to recover the IP addresses in the future to recover location information if this is enabled.',
+                        'wp-statistics'
+                    ); ?></p>
+            </td>
+        </tr>
+
+        <tr valign="top">
+            <th scope="row">
+                <label for="store_ua"><?php _e('Store entire user agent string', 'wp-statistics'); ?>:</label>
             </th>
 
             <td>
                 <input id="store_ua" type="checkbox" value="1"
-                       name="wps_store_ua" <?php echo $WP_Statistics->get_option( 'store_ua' ) == true
-					? "checked='checked'" : ''; ?>>
-                <label for="store_ua"><?php _e( 'Enable', 'wp-statistics' ); ?></label>
+                       name="wps_store_ua" <?php echo $WP_Statistics->get_option('store_ua') == true
+                    ? "checked='checked'" : ''; ?>>
+                <label for="store_ua"><?php _e('Enable', 'wp-statistics'); ?></label>
 
-                <p class="description"><?php _e( 'Only enabled for debugging', 'wp-statistics' ); ?></p>
+                <p class="description"><?php _e('Only enabled for debugging. (If the IP hash\'s are enabled, This option disabling automatically.)', 'wp-statistics'); ?></p>
             </td>
         </tr>
         </tbody>
     </table>
 
-<?php submit_button( __( 'Update', 'wp-statistics' ), 'primary', 'submit' );
+<?php submit_button(__('Update', 'wp-statistics'), 'primary', 'submit');
--- includes/settings/wps-settings.php
+++ includes/settings/wps-settings.php
@@ -1,119 +1,119 @@
 <?php
 $wps_nonce_valid = false;
 
-if ( array_key_exists( 'wp-statistics-nonce', $_POST ) ) {
-	if ( wp_verify_nonce( $_POST['wp-statistics-nonce'], 'update-options' ) ) {
-		$wps_nonce_valid = true;
-	}
+if (array_key_exists('wp-statistics-nonce', $_POST)) {
+    if (wp_verify_nonce($_POST['wp-statistics-nonce'], 'update-options')) {
+        $wps_nonce_valid = true;
+    }
 }
 
 $wps_admin = false;
 
-if ( current_user_can(
-	wp_statistics_validate_capability( $WP_Statistics->get_option( 'manage_capability', 'manage_options' ) )
-) ) {
-	$wps_admin = true;
+if (current_user_can(
+    wp_statistics_validate_capability($WP_Statistics->get_option('manage_capability', 'manage_options'))
+)) {
+    $wps_admin = true;
 }
 
-if ( $wps_admin === false ) {
-	$wps_admin = 0;
+if ($wps_admin === false) {
+    $wps_admin = 0;
 }
 ?>
     <div class="wrap wp-statistics-settings">
-        <h2><?php _e( 'Settings', 'wp-statistics' ); ?></h2>
+        <h2><?php _e('Settings', 'wp-statistics'); ?></h2>
 
         <div id="poststuff">
             <div id="post-body" class="metabox-holder columns-2">
                 <div class="wp-list-table widefat widefat">
                     <form id="wp-statistics-settings-form" method="post">
-						<?php wp_nonce_field( 'update-options', 'wp-statistics-nonce' ); ?>
+                        <?php wp_nonce_field('update-options', 'wp-statistics-nonce'); ?>
                         <div class="wp-statistics-container">
                             <ul class="tabs">
-								<?php if ( $wps_admin ) { ?>
+                                <?php if ($wps_admin) { ?>
                                     <li class="tab-link current" data-tab="general-settings"><?php _e(
-											'General',
-											'wp-statistics'
-										); ?></li>
+                                            'General',
+                                            'wp-statistics'
+                                        ); ?></li>
                                     <li class="tab-link" data-tab="privacy-settings"><?php _e(
-											'Privacy',
-											'wp-statistics'
-										); ?></li>
+                                            'Privacy',
+                                            'wp-statistics'
+                                        ); ?></li>
                                     <li class="tab-link" data-tab="notifications-settings"><?php _e(
-											'Notifications',
-											'wp-statistics'
-										); ?></li>
+                                            'Notifications',
+                                            'wp-statistics'
+                                        ); ?></li>
                                     <li class="tab-link" data-tab="overview-display-settings"><?php _e(
-											'Dashboard/Overview',
-											'wp-statistics'
-										); ?></li>
+                                            'Dashboard/Overview',
+                                            'wp-statistics'
+                                        ); ?></li>
                                     <li class="tab-link" data-tab="access-settings"><?php _e(
-											'Access Levels',
-											'wp-statistics'
-										); ?></li>
+                                            'Access Levels',
+                                            'wp-statistics'
+                                        ); ?></li>
                                     <li class="tab-link" data-tab="exclusions-settings"><?php _e(
-											'Exclusions',
-											'wp-statistics'
-										); ?></li>
+                                            'Exclusions',
+                                            'wp-statistics'
+                                        ); ?></li>
                                     <li class="tab-link" data-tab="externals-settings"><?php _e(
-											'Externals',
-											'wp-statistics'
-										); ?></li>
+                                            'Externals',
+                                            'wp-statistics'
+                                        ); ?></li>
                                     <li class="tab-link" data-tab="maintenance-settings"><?php _e(
-											'Maintenance',
-											'wp-statistics'
-										); ?></li>
+                                            'Maintenance',
+                                            'wp-statistics'
+                                        ); ?></li>
                                     <li class="tab-link" data-tab="removal-settings"><?php _e(
-											'Removal',
-											'wp-statistics'
-										); ?></li>
-								<?php } ?>
-                                <li class="tab-link" data-tab="about"><?php _e( 'About', 'wp-statistics' ); ?></li>
+                                            'Removal',
+                                            'wp-statistics'
+                                        ); ?></li>
+                                <?php } ?>
+                                <li class="tab-link" data-tab="about"><?php _e('About', 'wp-statistics'); ?></li>
                             </ul>
 
-							<?php if ( $wps_admin ) { ?>
+                            <?php if ($wps_admin) { ?>
                                 <div id="general-settings" class="tab-content current">
-									<?php include( WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-general.php' ); ?>
+                                    <?php include(WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-general.php'); ?>
                                 </div>
                                 <div id="privacy-settings" class="tab-content">
-									<?php include( WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-privacy.php' ); ?>
+                                    <?php include(WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-privacy.php'); ?>
                                 </div>
                                 <div id="notifications-settings" class="tab-content">
-									<?php include( WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-notifications.php' ); ?>
+                                    <?php include(WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-notifications.php'); ?>
                                 </div>
                                 <div id="overview-display-settings" class="tab-content">
-									<?php include( WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-overview-display.php' ); ?>
+                                    <?php include(WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-overview-display.php'); ?>
                                 </div>
                                 <div id="access-settings" class="tab-content">
-									<?php include( WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-access-level.php' ); ?>
+                                    <?php include(WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-access-level.php'); ?>
                                 </div>
                                 <div id="exclusions-settings" class="tab-content">
-									<?php include( WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-exclusions.php' ); ?>
+                                    <?php include(WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-exclusions.php'); ?>
                                 </div>
                                 <div id="externals-settings" class="tab-content">
-									<?php include( WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-externals.php' ); ?>
+                                    <?php include(WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-externals.php'); ?>
                                 </div>
                                 <div id="maintenance-settings" class="tab-content">
-									<?php include( WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-maintenance.php' ); ?>
+                                    <?php include(WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-maintenance.php'); ?>
                                 </div>
                                 <div id="removal-settings" class="tab-content">
-									<?php include( WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-removal.php' ); ?>
+                                    <?php include(WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-removal.php'); ?>
                                 </div>
-							<?php } ?>
+                            <?php } ?>
                             <div id="about" class="tab-content">
-								<?php include( WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-about.php' ); ?>
+                                <?php include(WP_Statistics::$reg['plugin-dir'] . 'includes/settings/tabs/wps-about.php'); ?>
                             </div>
                         </div><!-- container -->
                     </form>
                 </div>
-				<?php include WP_Statistics::$reg['plugin-dir'] . 'includes/templates/postbox.php'; ?>
+                <?php include WP_Statistics::$reg['plugin-dir'] . 'includes/templates/postbox.php'; ?>
             </div>
         </div>
     </div>
 
 <?php
-if ( $wps_nonce_valid ) {
-	if ( $wps_admin ) {
-		$WP_Statistics->save_options();
-	}
-	$WP_Statistics->save_user_options();
+if ($wps_nonce_valid) {
+    if ($wps_admin) {
+        $WP_Statistics->save_options();
+    }
+    $WP_Statistics->save_user_options();
 }
