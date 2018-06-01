3efdb0ff70be14e904e20ef757b6a38ed5797a25
Mostafa Soufi <mostafa.soufi@hotmail.com>
2018-06-01 17:15:43 +0430

    Added travod template

---
+++ includes/templates/suggestions/top-summary.php
@@ -0,0 +1,23 @@
+<div class="wp-statistics-travod">
+    <div class="header">
+        <div class="left">
+            <p><a href="<?php bloginfo('url'); ?>"><?php echo $this->getBaseURL(); ?></a>, Go Global With TRAVOD!</p>
+        </div>
+
+        <div class="right">
+            <p>Professional Translation Service by TRAVOD <a href="https://www.travod.com/" target="_blank"><img src="<?php echo plugins_url( 'wp-statistics/assets/images/TRAVOD-logo.png' ); ?>"></a> </p>
+        </div>
+    </div>
+
+    <div class="column">
+        column 1
+    </div>
+
+    <div class="column">
+        column 2
+    </div>
+
+    <div class="column">
+        column 2
+    </div>
+</div>
