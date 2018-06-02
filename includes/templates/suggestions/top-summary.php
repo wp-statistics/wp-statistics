e8e7a9736562625d30882f8c8c2330421263fa2c
Mostafa Soufi <mostafa.soufi@hotmail.com>
2018-06-02 18:36:31 +0430

    Complete the template content

--- includes/templates/suggestions/top-summary.php
+++ includes/templates/suggestions/top-summary.php
@@ -1,23 +1,74 @@
 <div class="wp-statistics-travod">
     <div class="header">
         <div class="left">
-            <p><a href="<?php bloginfo('url'); ?>"><?php echo $this->getBaseURL(); ?></a>, Go Global With TRAVOD!</p>
+            <p><a href="<?php bloginfo( 'url' ); ?>"><?php echo $this->getBaseURL(); ?></a>, Go Global With TRAVOD!</p>
         </div>
 
         <div class="right">
-            <p>Professional Translation Service by TRAVOD <a href="https://www.travod.com/" target="_blank"><img src="<?php echo plugins_url( 'wp-statistics/assets/images/TRAVOD-logo.png' ); ?>"></a> </p>
+            <p>Professional Translation Service by TRAVOD <a href="https://www.travod.com/" target="_blank"><img
+                            src="<?php echo plugins_url( 'wp-statistics/assets/images/TRAVOD-logo.png' ); ?>"></a></p>
         </div>
     </div>
 
     <div class="column">
-        column 1
+        <h2>Translate <?php echo $this->getBaseURL(); ?> into four languages and get:</h2>
+        <ul>
+            <li>International Sales Growth</li>
+            <li>Increase Web Traffic</li>
+            <li>Improve SEO</li>
+            <li>Greater Brand Awareness</li>
+            <li>Global Online
+                <Rea></Rea>
+                ch
+            </li>
+        </ul>
     </div>
 
     <div class="column">
-        column 2
+        <table width="100%">
+            <thead>
+            <tr>
+                <td>Language</td>
+                <td>Potential Traffic</td>
+                <td>Potential Leads</td>
+            </tr>
+            </thead>
+
+            <tbody>
+            <tr>
+                <td>German</td>
+                <td>120% (45,000 Visitors)</td>
+                <td>400% (45 Leads)</td>
+            </tr>
+
+            <tr>
+                <td>German</td>
+                <td>120% (45,000 Visitors)</td>
+                <td>400% (45 Leads)</td>
+            </tr>
+
+            <tr>
+                <td>German</td>
+                <td>120% (45,000 Visitors)</td>
+                <td>400% (45 Leads)</td>
+            </tr>
+
+            <tr>
+                <td>German</td>
+                <td>120% (45,000 Visitors)</td>
+                <td>400% (45 Leads)</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 
     <div class="column">
-        column 2
+        <h1>REQUEST A FREE QUITE</h1>
+        <p>To Global. Generate 4x more sales from untapped markets. Simply complete the form, and our Translation Advisory Team will be in touch with you soon.</p>
+        <form>
+            <input type="text" value=""/>
+            <input type="email" value=""/>
+            <input type="submit" value="GET A FREE ESTIMATE"/>
+        </form>
     </div>
 </div>
