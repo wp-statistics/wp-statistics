1d2d532ec1c17002e1d8be1bedee6f85b7510778
Mostafa Soufi <mostafa.soufi@hotmail.com>
2018-06-02 23:36:34 +0430

    Complete the potential method

--- includes/classes/class-wp-statistics-suggestions.php
+++ includes/classes/class-wp-statistics-suggestions.php
@@ -18,6 +18,8 @@ class WP_Statistics_Suggestions {
 	}
 
 	public function travod_widget() {
+		$base_url = $this->get_base_url( get_bloginfo( 'url' ) );
+
 		include( WP_Statistics::$reg['plugin-dir'] . "includes/templates/suggestions/top-summary.php" );
 	}
 
@@ -25,9 +27,7 @@ class WP_Statistics_Suggestions {
 		include( WP_Statistics::$reg['plugin-dir'] . "assets/js/travod.js" );
 	}
 
-	public function get_base_url() {
-		$url = get_bloginfo( 'url' );
-
+	public function get_base_url( $url ) {
 		if ( substr( $url, 0, 8 ) == 'https://' ) {
 			$url = substr( $url, 8 );
 		}
@@ -42,7 +42,7 @@ class WP_Statistics_Suggestions {
 			$url     = $explode['0'];
 		}
 
-		return ucfirst( $url );
+		return $url;
 	}
 
 	public function get_current_username() {
@@ -51,5 +51,236 @@ class WP_Statistics_Suggestions {
 		if ( isset( $user->data->display_name ) ) {
 			return $user->data->display_name;
 		}
+	}
+
+	private function get_domain_info( $domian_name ) {
+		$domains = array(
+			'google.me'     => array( 'country' => 'Montenegro', 'language' => 'Albanian' ),
+			'google.al'     => array( 'country' => 'Albania', 'language' => 'Albanian' ),
+			'google.com.et' => array( 'country' => 'Ethiopia', 'language' => 'Amharic' ),
+			'google.ae'     => array( 'country' => 'United Arab Emirates', 'language' => 'Arabic' ),
+			'google.co.ma'  => array( 'country' => 'Morocco', 'language' => 'Arabic' ),
+			'google.com.sa' => array( 'country' => 'Saudi Arabia', 'language' => 'Arabic' ),
+			'google.so'     => array( 'country' => 'Somalia', 'language' => 'Arabic' ),
+			'google.tn'     => array( 'country' => 'Tunisia', 'language' => 'Arabic' ),
+			'google.com.bh' => array( 'country' => 'Bahrain', 'language' => 'Arabic' ),
+			'google.dj'     => array( 'country' => 'Djibouti', 'language' => 'Arabic' ),
+			'google.dz'     => array( 'country' => 'Algeria', 'language' => 'Arabic' ),
+			'google.com.eg' => array( 'country' => 'Egypt', 'language' => 'Arabic' ),
+			'google.iq'     => array( 'country' => 'Iraq', 'language' => 'Arabic' ),
+			'google.jo'     => array( 'country' => 'Jordan', 'language' => 'Arabic' ),
+			'google.com.kw' => array( 'country' => 'Kuwait', 'language' => 'Arabic' ),
+			'google.com.lb' => array( 'country' => 'Lebanon', 'language' => 'Arabic' ),
+			'google.com.ly' => array( 'country' => 'Libya', 'language' => 'Arabic' ),
+			'google.com.om' => array( 'country' => 'Oman', 'language' => 'Arabic' ),
+			'google.ps'     => array( 'country' => 'Palestine', 'language' => 'Arabic' ),
+			'google.com.qa' => array( 'country' => 'Qatar', 'language' => 'Arabic' ),
+			'google.td'     => array( 'country' => 'Chad', 'language' => 'Arabic' ),
+			'google.am'     => array( 'country' => 'Armenia', 'language' => 'Armenian' ),
+			'google.az'     => array( 'country' => 'Azerbaijan', 'language' => 'Azerbaijani' ),
+			'google.by'     => array( 'country' => 'Belarus', 'language' => 'Belarusian' ),
+			'google.com.bd' => array( 'country' => 'Bangladesh', 'language' => 'Bengali' ),
+			'google.ba'     => array( 'country' => 'Bosnia and Herzegovina', 'language' => 'Bosnian' ),
+			'google.bg'     => array( 'country' => 'Bulgaria', 'language' => 'Bulgarian' ),
+			'google.mk'     => array( 'country' => 'Macedonia', 'language' => 'Bulgarian' ),
+			'google.com.mm' => array( 'country' => 'Myanmar', 'language' => 'Burmese' ),
+			'google.com.hk' => array( 'country' => 'Hong Kong', 'language' => 'Cantonese' ),
+			'google.ad'     => array( 'country' => 'Andorra', 'language' => 'Catalan' ),
+			'google.cat'    => array( 'country' => 'Catalan Countries', 'language' => 'Catalan' ),
+			'google.hr'     => array( 'country' => 'Croatia', 'language' => 'Croatian' ),
+			'google.cz'     => array( 'country' => 'Czech Republic', 'language' => 'Czech' ),
+			'google.dk'     => array( 'country' => 'Denmark', 'language' => 'Danish' ),
+			'google.mv'     => array( 'country' => 'Maldives', 'language' => 'Dhivehi' ),
+			'google.sr'     => array( 'country' => 'Suriname', 'language' => 'Dutch' ),
+			'google.be'     => array( 'country' => 'Belgium', 'language' => 'Dutch' ),
+			'google.nl'     => array( 'country' => 'Netherlands', 'language' => 'Dutch' ),
+			'google.bt'     => array( 'country' => 'Bhutan', 'language' => 'Dzongkha' ),
+			'google.com.ag' => array( 'country' => 'Antigua and Barbuda', 'language' => 'English' ),
+			'google.fm'     => array( 'country' => 'Federated States of Micronesia', 'language' => 'English' ),
+			'google.com.lc' => array( 'country' => 'Saint Lucia', 'language' => 'English' ),
+			'google.com.ng' => array( 'country' => 'Nigeria', 'language' => 'English' ),
+			'google.com.sb' => array( 'country' => 'Solomon Islands', 'language' => 'English' ),
+			'google.sc'     => array( 'country' => 'Seychelles', 'language' => 'English' ),
+			'google.com.sg' => array( 'country' => 'Singapore', 'language' => 'English' ),
+			'google.to'     => array( 'country' => 'Tonga', 'language' => 'English' ),
+			'google.tt'     => array( 'country' => 'Trinidad and Tobago', 'language' => 'English' ),
+			'google.co.ug'  => array( 'country' => 'Uganda', 'language' => 'English' ),
+			'google.co.uk'  => array( 'country' => 'United Kingdom', 'language' => 'English' ),
+			'google.com'    => array( 'country' => 'United States', 'language' => 'English' ),
+			'google.vu'     => array( 'country' => 'Vanuatu', 'language' => 'English' ),
+			'google.co.zm'  => array( 'country' => 'Zambia', 'language' => 'English' ),
+			'google.co.zw'  => array( 'country' => 'Zimbabwe', 'language' => 'English' ),
+			'google.com'    => array( 'country' => 'Worldwide', 'language' => 'English' ),
+			'google.ac'     => array( 'country' => 'Ascension Island', 'language' => 'English' ),
+			'google.com.ai' => array( 'country' => 'Anguilla', 'language' => 'English' ),
+			'google.as'     => array( 'country' => 'American Samoa', 'language' => 'English' ),
+			'google.com.au' => array( 'country' => 'Australia', 'language' => 'English' ),
+			'google.bs'     => array( 'country' => 'Bahamas', 'language' => 'English' ),
+			'google.co.bw'  => array( 'country' => 'Botswana', 'language' => 'English' ),
+			'google.com.bz' => array( 'country' => 'Belize', 'language' => 'English' ),
+			'google.cc'     => array( 'country' => 'Cocos (Keeling) Islands', 'language' => 'English' ),
+			'google.co.ck'  => array( 'country' => 'Cook Islands', 'language' => 'English' ),
+			'google.cm'     => array( 'country' => 'Cameroon', 'language' => 'English' ),
+			'google.dm'     => array( 'country' => 'Dominica', 'language' => 'English' ),
+			'google.com.fj' => array( 'country' => 'Fiji', 'language' => 'English' ),
+			'google.com.gh' => array( 'country' => 'Ghana', 'language' => 'English' ),
+			'google.com.gi' => array( 'country' => 'Gibraltar', 'language' => 'English' ),
+			'google.gm'     => array( 'country' => 'Gambia', 'language' => 'English' ),
+			'google.gy'     => array( 'country' => 'Guyana', 'language' => 'English' ),
+			'google.ie'     => array( 'country' => 'Ireland', 'language' => 'English' ),
+			'google.io'     => array( 'country' => 'British Indian Ocean Territory', 'language' => 'English' ),
+			'google.com.jm' => array( 'country' => 'Jamaica', 'language' => 'English' ),
+			'google.co.ke'  => array( 'country' => 'Kenya', 'language' => 'English' ),
+			'google.ki'     => array( 'country' => 'Kiribati', 'language' => 'English' ),
+			'google.co.ls'  => array( 'country' => 'Lesotho', 'language' => 'English' ),
+			'google.ms'     => array( 'country' => 'Montserrat', 'language' => 'English' ),
+			'google.com.mt' => array( 'country' => 'Malta', 'language' => 'English' ),
+			'google.mu'     => array( 'country' => 'Mauritius', 'language' => 'English' ),
+			'google.mw'     => array( 'country' => 'Malawi', 'language' => 'English' ),
+			'google.com.my' => array( 'country' => 'Malaysia', 'language' => 'English' ),
+			'google.com.na' => array( 'country' => 'Namibia', 'language' => 'English' ),
+			'google.com.nf' => array( 'country' => 'Norfolk Island', 'language' => 'English' ),
+			'google.nr'     => array( 'country' => 'Nauru', 'language' => 'English' ),
+			'google.co.nz'  => array( 'country' => 'New Zealand', 'language' => 'English' ),
+			'google.com.pg' => array( 'country' => 'Papua New Guinea', 'language' => 'English' ),
+			'google.pn'     => array( 'country' => 'Pitcairn Islands', 'language' => 'English' ),
+			'google.rw'     => array( 'country' => 'Rwanda', 'language' => 'English' ),
+			'google.sh'     => array(
+				'country'  => 'Saint Helena, Ascension and Tristan da Cunha',
+				'language' => 'English'
+			),
+			'google.com.sl' => array( 'country' => 'Sierra Leone', 'language' => 'English' ),
+			'google.com.vc' => array( 'country' => 'Saint Vincent and the Grenadines', 'language' => 'English' ),
+			'google.vg'     => array( 'country' => 'British Virgin Islands', 'language' => 'English' ),
+			'google.co.vi'  => array( 'country' => 'United States Virgin Islands', 'language' => 'English' ),
+			'google.ws'     => array( 'country' => 'Samoa', 'language' => 'English' ),
+			'google.co.za'  => array( 'country' => 'South Africa', 'language' => 'English' ),
+			'google.com.ph' => array( 'country' => 'Philippines', 'language' => 'Filipino' ),
+			'google.fi'     => array( 'country' => 'Finland', 'language' => 'Finnish' ),
+			'google.bf'     => array( 'country' => 'Burkina Faso', 'language' => 'French' ),
+			'google.cd'     => array( 'country' => 'Democratic Republic of the Congo', 'language' => 'French' ),
+			'google.cg'     => array( 'country' => 'Republic of the Congo', 'language' => 'French' ),
+			'google.ci'     => array( 'country' => 'Ivory Coast', 'language' => 'French' ),
+			'google.ne'     => array( 'country' => 'Niger', 'language' => 'French' ),
+			'google.tg'     => array( 'country' => 'Togo', 'language' => 'French' ),
+			'google.bj'     => array( 'country' => 'Benin', 'language' => 'French' ),
+			'google.ca'     => array( 'country' => 'Canada', 'language' => 'French' ),
+			'google.cf'     => array( 'country' => 'Central African Republic', 'language' => 'French' ),
+			'google.fr'     => array( 'country' => 'France', 'language' => 'French' ),
+			'google.ga'     => array( 'country' => 'Gabon', 'language' => 'French' ),
+			'google.gf'     => array( 'country' => 'French Guiana', 'language' => 'French' ),
+			'google.gg'     => array( 'country' => 'Guernsey', 'language' => 'French' ),
+			'google.gp'     => array( 'country' => 'Guadeloupe', 'language' => 'French' ),
+			'google.ht'     => array( 'country' => 'Haiti', 'language' => 'French' ),
+			'google.je'     => array( 'country' => 'Jersey', 'language' => 'French' ),
+			'google.lu'     => array( 'country' => 'Luxembourg', 'language' => 'French' ),
+			'google.mg'     => array( 'country' => 'Madagascar', 'language' => 'French' ),
+			'google.ml'     => array( 'country' => 'Mali', 'language' => 'French' ),
+			'google.sn'     => array( 'country' => 'Senegal', 'language' => 'French' ),
+			'google.ge'     => array( 'country' => 'Georgia', 'language' => 'Georgian' ),
+			'google.ch'     => array( 'country' => 'Switzerland', 'language' => 'German' ),
+			'google.de'     => array( 'country' => 'Germany', 'language' => 'German' ),
+			'google.at'     => array( 'country' => 'Austria', 'language' => 'German' ),
+			'google.li'     => array( 'country' => 'Liechtenstein', 'language' => 'German' ),
+			'google.com.cy' => array( 'country' => 'Cyprus', 'language' => 'Greek' ),
+			'google.gr'     => array( 'country' => 'Greece', 'language' => 'Greek' ),
+			'google.gl'     => array( 'country' => 'Greenland', 'language' => 'Greenlandic' ),
+			'google.co.il'  => array( 'country' => 'Israel', 'language' => 'Hebrew' ),
+			'google.co.in'  => array( 'country' => 'India', 'language' => 'Hindi' ),
+			'google.hu'     => array( 'country' => 'Hungary', 'language' => 'Hungarian' ),
+			'google.is'     => array( 'country' => 'Iceland', 'language' => 'Icelandic' ),
+			'google.co.id'  => array( 'country' => 'Indonesia', 'language' => 'Indonesian' ),
+			'google.it'     => array( 'country' => 'Italy', 'language' => 'Italian' ),
+			'google.sm'     => array( 'country' => 'San Marino', 'language' => 'Italian' ),
+			'google.co.jp'  => array( 'country' => 'Japan', 'language' => 'Japanese' ),
+			'google.com.kh' => array( 'country' => 'Cambodia', 'language' => 'Khmer' ),
+			'google.bi'     => array( 'country' => 'Burundi', 'language' => 'Kirundi' ),
+			'google.co.kr'  => array( 'country' => 'South Korea', 'language' => 'Korean' ),
+			'google.la'     => array( 'country' => 'Laos', 'language' => 'Lao' ),
+			'google.lv'     => array( 'country' => 'Latvia', 'language' => 'Latvian' ),
+			'google.lt'     => array( 'country' => 'Lithuania', 'language' => 'Lithuanian' ),
+			'google.com.np' => array( 'country' => 'Nepal', 'language' => 'Maithili' ),
+			'google.com.bn' => array( 'country' => 'Brunei', 'language' => 'Malay' ),
+			'google.cn'     => array( 'country' => 'China', 'language' => 'Mandarin' ),
+			'google.com.tw' => array( 'country' => 'Taiwan', 'language' => 'Mandarin' ),
+			'google.co.tz'  => array( 'country' => 'Tanzania', 'language' => 'Mandarin' ),
+			'google.im'     => array( 'country' => 'Isle of Man', 'language' => 'Manx' ),
+			'google.mn'     => array( 'country' => 'Mongolia', 'language' => 'Mongolian' ),
+			'google.nu'     => array( 'country' => 'Niue', 'language' => 'Niuean' ),
+			'google.no'     => array( 'country' => 'Norway', 'language' => 'Norwegian' ),
+			'google.com.af' => array( 'country' => 'Afghanistan', 'language' => 'Pashto' ),
+			'google.pl'     => array( 'country' => 'Poland', 'language' => 'Polish' ),
+			'google.co.ao'  => array( 'country' => 'Angola', 'language' => 'Portuguese' ),
+			'google.com.br' => array( 'country' => 'Brazil', 'language' => 'Portuguese' ),
+			'google.cv'     => array( 'country' => 'Cape Verde', 'language' => 'Portuguese' ),
+			'google.co.mz'  => array( 'country' => 'Mozambique', 'language' => 'Portuguese' ),
+			'google.pt'     => array( 'country' => 'Portugal', 'language' => 'Portuguese' ),
+			'google.st'     => array( 'country' => 'São Tomé and Príncipe', 'language' => 'Portuguese' ),
+			'google.tl'     => array( 'country' => 'Timor-Leste', 'language' => 'Portuguese' ),
+			'google.md'     => array( 'country' => 'Moldova', 'language' => 'Romanian' ),
+			'google.ro'     => array( 'country' => 'Romania', 'language' => 'Romanian' ),
+			'google.kg'     => array( 'country' => 'Kyrgyzstan', 'language' => 'Russian' ),
+			'google.kz'     => array( 'country' => 'Kazakhstan', 'language' => 'Russian' ),
+			'google.ru'     => array( 'country' => 'Russia', 'language' => 'Russian' ),
+			'google.rs'     => array( 'country' => 'Serbia', 'language' => 'Serbian' ),
+			'google.lk'     => array( 'country' => 'Sri Lanka', 'language' => 'Sinhala' ),
+			'google.sk'     => array( 'country' => 'Slovakia', 'language' => 'Slovak' ),
+			'google.si'     => array( 'country' => 'Slovenia', 'language' => 'Slovene' ),
+			'google.es'     => array( 'country' => 'Spain', 'language' => 'Spanish' ),
+			'google.com.ni' => array( 'country' => 'Nicaragua', 'language' => 'Spanish' ),
+			'google.com.pa' => array( 'country' => 'Panama', 'language' => 'Spanish' ),
+			'google.com.pe' => array( 'country' => 'Peru', 'language' => 'Spanish' ),
+			'google.com.uy' => array( 'country' => 'Uruguay', 'language' => 'Spanish' ),
+			'google.co.ve'  => array( 'country' => 'Venezuela', 'language' => 'Spanish' ),
+			'google.com.ar' => array( 'country' => 'Argentina', 'language' => 'Spanish' ),
+			'google.com.bo' => array( 'country' => 'Bolivia', 'language' => 'Spanish' ),
+			'google.cl'     => array( 'country' => 'Chile', 'language' => 'Spanish' ),
+			'google.com.co' => array( 'country' => 'Colombia', 'language' => 'Spanish' ),
+			'google.co.cr'  => array( 'country' => 'Costa Rica', 'language' => 'Spanish' ),
+			'google.com.cu' => array( 'country' => 'Cuba', 'language' => 'Spanish' ),
+			'google.com.do' => array( 'country' => 'Dominican Republic', 'language' => 'Spanish' ),
+			'google.com.ec' => array( 'country' => 'Ecuador', 'language' => 'Spanish' ),
+			'google.ee'     => array( 'country' => 'Estonia', 'language' => 'Spanish' ),
+			'google.com.gt' => array( 'country' => 'Guatemala', 'language' => 'Spanish' ),
+			'google.hn'     => array( 'country' => 'Honduras', 'language' => 'Spanish' ),
+			'google.com.mx' => array( 'country' => 'Mexico', 'language' => 'Spanish' ),
+			'google.com.pr' => array( 'country' => 'Puerto Rico', 'language' => 'Spanish' ),
+			'google.com.py' => array( 'country' => 'Paraguay', 'language' => 'Spanish' ),
+			'google.com.sv' => array( 'country' => 'El Salvador', 'language' => 'Spanish' ),
+			'google.se'     => array( 'country' => 'Sweden', 'language' => 'Swedish' ),
+			'google.com.tj' => array( 'country' => 'Tajikistan', 'language' => 'Tajiki' ),
+			'google.co.th'  => array( 'country' => 'Thailand', 'language' => 'Thai' ),
+			'google.tk'     => array( 'country' => 'Tokelau', 'language' => 'Tokelauan' ),
+			'google.com.tr' => array( 'country' => 'Turkey', 'language' => 'Turkish' ),
+			'google.tm'     => array( 'country' => 'Turkmenistan', 'language' => 'Turkmen' ),
+			'google.com.ua' => array( 'country' => 'Ukraine', 'language' => 'Ukrainian' ),
+			'google.com.pk' => array( 'country' => 'Pakistan', 'language' => 'Urdu' ),
+			'google.co.uz'  => array( 'country' => 'Uzbekistan', 'language' => 'Uzbek' ),
+			'google.com.vn' => array( 'country' => 'Vietnam', 'language' => 'Vietnamese' ),
+		);
+
+		return $domains[ $domian_name ];
+	}
+
+	public function get_potentials( $number = 10 ) {
+		global $wpdb;
+
+		$result = $wpdb->get_results( "SELECT referred, hits, COUNT(*) as visitors FROM {$wpdb->prefix}statistics_visitor WHERE referred != '' AND referred LIKE '%google%' and referred NOT LIKE '%google.com%' GROUP BY referred ORDER BY `visitors` DESC LIMIT {$number}" );
+		$data   = array();
+
+		foreach ( $result as $item ) {
+			$country = $this->get_domain_info( $this->get_base_url( $item->referred ) );
+
+			$data[] = array(
+				'domain'            => $item->referred,
+				'country'           => ( isset( $country['country'] ) ? $country['country'] : '' ),
+				'potential_percent' => '',
+				'potential_leads'   => '',
+				'visitors'          => $item->visitors,
+				'hits'              => $item->hits,
+			);
+		}
+
+		return $data;
 	}
 }
--- includes/templates/suggestions/top-summary.php
+++ includes/templates/suggestions/top-summary.php
@@ -1,19 +1,21 @@
 <div class="wp-statistics-travod">
     <div class="header">
         <div class="left">
-            <p><a href="<?php bloginfo( 'url' ); ?>"><?php echo $this->get_base_url(); ?></a>, Go Global With TRAVOD!</p>
+            <p><a href="<?php bloginfo( 'url' ); ?>"><?php echo $base_url; ?></a>, Go Global With TRAVOD!</p>
         </div>
 
         <div class="right">
-            <p>Professional Translation Service by TRAVOD <a href="https://www.travod.com/" target="_blank"><img src="<?php echo plugins_url( 'wp-statistics/assets/images/TRAVOD-logo.png' ); ?>"></a></p>
-            <button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button>
+            <p>Professional Translation Service by TRAVOD <a href="https://www.travod.com/" target="_blank"><img
+                            src="<?php echo plugins_url( 'wp-statistics/assets/images/TRAVOD-logo.png' ); ?>"></a></p>
+            <button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span>
+            </button>
         </div>
     </div>
 
     <div class="column">
         <div class="columns column-1">
             <section>
-                <h2>Translate <?php echo $this->get_base_url(); ?> into<br> four languages and get:</h2>
+                <h2>Translate <?php echo $base_url; ?> into<br> four languages and get:</h2>
                 <ul>
                     <li><span class="dashicons dashicons-yes"></span> International Sales Growth</li>
                     <li><span class="dashicons dashicons-yes"></span> Increase Web Traffic</li>
@@ -36,29 +38,13 @@
                     </thead>
 
                     <tbody>
-                    <tr>
-                        <td>German</td>
-                        <td><span class="dashicons dashicons-arrow-up"></span> 120% (45,000 Visitors)</td>
-                        <td><span class="dashicons dashicons-arrow-up"></span> 400% (45 Leads)</td>
-                    </tr>
-
-                    <tr>
-                        <td>German</td>
-                        <td><span class="dashicons dashicons-arrow-up"></span> 120% (45,000 Visitors)</td>
-                        <td><span class="dashicons dashicons-arrow-up"></span> 400% (45 Leads)</td>
-                    </tr>
-
-                    <tr>
-                        <td>German</td>
-                        <td><span class="dashicons dashicons-arrow-up"></span> 120% (45,000 Visitors)</td>
-                        <td><span class="dashicons dashicons-arrow-up"></span> 400% (45 Leads)</td>
-                    </tr>
-
-                    <tr>
-                        <td>German</td>
-                        <td><span class="dashicons dashicons-arrow-up"></span> 120% (45,000 Visitors)</td>
-                        <td><span class="dashicons dashicons-arrow-up"></span> 400% (45 Leads)</td>
-                    </tr>
+					<?php foreach ( $this->get_potentials( 4 ) as $item ) : ?>
+                        <tr>
+                            <td><?php echo $item['country']; ?></td>
+                            <td><span class="dashicons dashicons-arrow-up"></span> 120% (<?php echo $item['visitors']; ?> Visitors)</td>
+                            <td><span class="dashicons dashicons-arrow-up"></span> 400% (45 Leads)</td>
+                        </tr>
+					<?php endforeach; ?>
                     </tbody>
                 </table>
             </section>
@@ -67,10 +53,13 @@
         <div class="columns column-3">
             <section>
                 <h1>REQUEST A FREE QUITE</h1>
-                <p>To Global. Generate 4x more sales from untapped markets. Simply complete the form, and our Translation Advisory Team will be in touch with you soon.</p>
+                <p>To Global. Generate 4x more sales from untapped markets. Simply complete the form, and our
+                    Translation Advisory Team will be in touch with you soon.</p>
                 <form method="post" action="">
-                    <input type="text" class="regular-text" value="<?php echo $this->get_current_username(); ?>" required="required"/>
-                    <input type="email" class="regular-text code" value="<?php echo get_option('admin_email'); ?>" required="required"/>
+                    <input type="text" class="regular-text" value="<?php echo $this->get_current_username(); ?>"
+                           required="required"/>
+                    <input type="email" class="regular-text code" value="<?php echo get_option( 'admin_email' ); ?>"
+                           required="required"/>
                     <input type="submit" class="button button-primary" value="GET A FREE ESTIMATE"/>
                 </form>
             </section>
