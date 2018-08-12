7251588baabfeb5cb7563f3c0532bc80aae3dede
Mostafa Soufi <mostafa.soufi@hotmail.com>
2018-08-12 17:02:29 +0430

    Added privacy content in the Privacy Policy Guide page.

--- includes/classes/class-wp-statistics-admin.php
+++ includes/classes/class-wp-statistics-admin.php
@@ -76,6 +76,9 @@ class WP_Statistics_Admin {
 
 		// Runs some scripts at the end of the admin panel inside the body tag.
 		add_action( 'admin_footer', array( $this, 'admin_footer_scripts' ) );
+
+		// Add privacy content in the Privacy Policy Guide page.
+		add_action( 'admin_init', array( $this, 'privacy_policy_template' ) );
 	}
 
 	/**
@@ -672,5 +675,22 @@ class WP_Statistics_Admin {
 				$headers
 			);
 		}
+	}
+
+	/**
+	 *
+	 * Register the WP-Statistics template for a privacy policy.
+	 *
+	 * Note, this is just a suggestion and should be customized to meet your businesses needs.
+	 *
+	 */
+	public function privacy_policy_template() {
+		if ( ! function_exists( 'wp_add_privacy_policy_content' ) ) {
+			return;
+		}
+
+		$content = __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'wp-statistics' ) . "\n";
+
+		wp_add_privacy_policy_content( 'WP Statistics', wpautop( $content ) );
 	}
 }
