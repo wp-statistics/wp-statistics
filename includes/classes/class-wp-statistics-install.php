f170e4c5b4939faeaf8022b70ae788d8d50e5ed3
Mehrshad Darzi <mehrshad198@gmail.com>
2018-11-19 16:43:23 +0330

    Added new table in wp-statistics mysql

--- includes/classes/class-wp-statistics-install.php
+++ includes/classes/class-wp-statistics-install.php
@@ -139,7 +139,6 @@ class WP_Statistics_Install {
 						ID bigint(20) NOT NULL AUTO_INCREMENT,
 						visitor_id bigint(20) NOT NULL,
 						page_id bigint(20) NOT NULL,
-						date datetime NOT NULL,
 						PRIMARY KEY  (ID),
 						KEY visitor_id (visitor_id),
 						KEY page_id (page_id)
