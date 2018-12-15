bb60363eba87c3ed8ee9bf9ab83ab23d8b3ede63
Mostafa Soufi <mostafa.soufi@hotmail.com>
2018-12-15 17:38:29 +0330

    Added veronalabs to about widget

--- includes/log/widgets/about.php
+++ includes/log/widgets/about.php
@@ -1,35 +1,25 @@
 <?php
 function wp_statistics_generate_about_postbox_content() {
-
-	global $wpdb, $WP_Statistics;
+	global $WP_Statistics;
 	?>
     <div style="text-align: center;">
-        <a href="http://wp-statistics.com" target="_blank"><img
-                    src="<?php echo plugins_url( 'wp-statistics/assets/images/logo-250.png' ); ?>"></a>
+        <a href="https://wp-statistics.com" target="_blank"><img src="<?php echo plugins_url( 'wp-statistics/assets/images/logo-250.png' ); ?>"></a>
     </div>
 
     <div id="about-links" style="text-align: center;">
-        <p><a href="http://wp-statistics.com" target="_blank"><?php _e( 'Website', 'wp-statistics' ); ?></a></p>
-        | <p>
-            <a href="https://wordpress.org/support/plugin/wp-statistics/reviews/?rate=5#new-post" target="_blank"><?php _e(
-					'Rate and Review',
-					'wp-statistics'
-				); ?></a>
-        </p>
+        <p><a href="https://wp-statistics.com" target="_blank"><?php _e( 'Website', 'wp-statistics' ); ?></a></p>
+        | <p><a href="https://wordpress.org/support/plugin/wp-statistics/reviews/?rate=5#new-post" target="_blank"><?php _e( 'Rate and Review', 'wp-statistics' ); ?></a></p>
 		<?php
-		if ( current_user_can(
-			wp_statistics_validate_capability( $WP_Statistics->get_option( 'manage_capability', 'manage_options' ) )
-		) ) {
+		if ( current_user_can( wp_statistics_validate_capability( $WP_Statistics->get_option( 'manage_capability', 'manage_options' ) ) ) ) {
 			?>
-            | <p>
-                <a href="?page=<?php echo WP_Statistics::$page['settings']; ?>&tab=about"><?php _e(
-						'More Information',
-						'wp-statistics'
-					); ?></a>
-            </p>
+            | <p><a href="?page=<?php echo WP_Statistics::$page['settings']; ?>&tab=about"><?php _e( 'More Information', 'wp-statistics' ); ?></a></p>
+            | <p><a href="admin.php?page=wps_welcome"><?php _e( 'What’s New', 'wp-statistics' ); ?>?</a></p>
 			<?php
 		}
 		?>
+        <div class="wps-postbox-veronalabs">
+            <a href="https://veronalabs.com" target="_blank" title="<?php _e('Power by VeronaLabs', 'wp-statistics'); ?>"><img src="https://veronalabs.com/wp-content/themes/veronalabs.com/assets/images/logo.svg"/></a>
+        </div>
     </div>
 	<?php
 }
