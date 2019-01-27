c436345f825846ee61fb6f09257587e4eb880057
Mehrshad Darzi <mehrshad198@gmail.com>
2019-01-27 17:29:24 +0330

    fix domain in top refer meta box

--- includes/log/widgets/referring.php
+++ includes/log/widgets/referring.php
@@ -1,44 +1,34 @@
 <?php
 function wp_statistics_generate_referring_postbox_content( $count = 10 ) {
 	global $wpdb, $WP_Statistics;
-	$get_urls = array();
-	$urls     = array();
-	$start    = 0;
 
+	//Get Top Referring
 	if ( false === ( $get_urls = get_transient( 'wps_top_referring' ) ) ) {
-		do {
-			$result = $wpdb->get_results( "SELECT referred FROM {$wpdb->prefix}statistics_visitor WHERE referred <> '' LIMIT {$start}, 10000" );
-			$start  += count( $result );
 
-			foreach ( $result as $item ) {
-				$url = parse_url( $item->referred );
-				if ( empty( $url['host'] ) || stristr( get_bloginfo( 'url' ), $url['host'] ) ) {
-					continue;
-				}
-				$urls[] = $url['host'];
-			}
-		} while ( 10000 == count( $result ) );
+		//Get Wordpress Domain
+		$site_url = wp_parse_url( get_site_url() );
+		$site_url = $site_url['scheme'] . "://" . $site_url['host'];
+		$result   = $wpdb->get_results( "SELECT SUBSTRING_INDEX(REPLACE( REPLACE( referred, 'http://', '') , 'https://' , '') , '/', 1 ) as `domain`, count(referred) as `number` FROM {$wpdb->prefix}statistics_visitor WHERE `referred` REGEXP \"^(https?://|www\\.)[\.A-Za-z0-9\-]+\\.[a-zA-Z]{2,4}\" AND referred <> '' AND LENGTH(referred) >=12 AND `referred` NOT LIKE '{$site_url}%' GROUP BY domain ORDER BY `number` DESC LIMIT $count" );
+		foreach ( $result as $items ) {
+			$get_urls[ $items->domain ] = $items->number;
+		}
 
-		$get_urls = array_count_values( $urls );
-		arsort( $get_urls );
-		$get_urls = array_slice( $get_urls, 0, $count );
-
-		// Put the results in a transient. Expire after 24 hours.
-		set_transient( 'wps_top_referring', $get_urls, 24 * HOUR_IN_SECONDS );
+		// Put the results in a transient. Expire after 12 hours.
+		set_transient( 'wps_top_referring', $get_urls, 12 * HOUR_IN_SECONDS );
 	}
 	?>
-    <table width="100%" class="widefat table-stats" id="last-referrer">
+    <table width="100%" class="widefat table-stats" id="top-referrer">
         <tr>
             <td width="10%"><?php _e( 'References', 'wp-statistics' ); ?></td>
             <td width="90%"><?php _e( 'Address', 'wp-statistics' ); ?></td>
         </tr>
 
 		<?php
-		foreach ( $get_urls as $items => $value ) {
-			$referrer_html = $WP_Statistics->html_sanitize_referrer( $items );
+		foreach ( $get_urls as $domain => $number ) {
+			$referrer_html = $WP_Statistics->html_sanitize_referrer( $domain );
 			echo "<tr>";
-			echo "<td><a href='" . WP_Statistics_Admin_Pages::admin_url( 'referrers', array( 'referr' => $referrer_html ) ) . "'>" . number_format_i18n( $value ) . "</a></td>";
-			echo "<td>" . $WP_Statistics->get_referrer_link( $items ) . "</td>";
+			echo "<td><a href='" . WP_Statistics_Admin_Pages::admin_url( 'referrers', array( 'referr' => $referrer_html ) ) . "'>" . number_format_i18n( $number ) . "</a></td>";
+			echo "<td>" . $WP_Statistics->get_referrer_link( $domain ) . "</td>";
 			echo "</tr>";
 		}
 		?>
