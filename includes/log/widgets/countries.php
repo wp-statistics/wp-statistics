81f436efe9c691d8120e9aa0b39e999fde4c3105
Mehrshad Darzi <mehrshad198@gmail.com>
2019-02-13 12:43:01 +0330

    minor improvement #228

--- includes/log/widgets/countries.php
+++ includes/log/widgets/countries.php
@@ -17,10 +17,10 @@ function wp_statistics_generate_countries_postbox_content() {
 			$i ++;
 			$item->location = strtoupper( $item->location );
 			echo "<tr>";
-			echo "<td style='text-align: left'>$i</td>";
-			echo "<td style='text-align: left'><img src='" . plugins_url( 'wp-statistics/assets/images/flags/' . $item->location . '.png' ) . "' title='{$ISOCountryCode[$item->location]}'/></td>";
-			echo "<td style='text-align: left !important'>{$ISOCountryCode[$item->location]}</td>";
-			echo "<td style='text-align: left !important'><a href='" . WP_Statistics_Admin_Pages::admin_url( 'countries', array( 'country' => $item->location ) ) . "'>" . number_format_i18n( $item->count ) . "</a></td>";
+			echo "<td style='text-align: " . ( is_rtl() ? 'right' : 'left' ) . "'>$i</td>";
+			echo "<td style='text-align: " . ( is_rtl() ? 'right' : 'left' ) . "'><img src='" . plugins_url( 'wp-statistics/assets/images/flags/' . $item->location . '.png' ) . "' title='{$ISOCountryCode[$item->location]}'/></td>";
+			echo "<td style='text-align: " . ( is_rtl() ? 'right' : 'left' ) . " !important'>{$ISOCountryCode[$item->location]}</td>";
+			echo "<td style='text-align: " . ( is_rtl() ? 'right' : 'left' ) . " !important'><a href='" . WP_Statistics_Admin_Pages::admin_url( 'countries', array( 'country' => $item->location ) ) . "'>" . number_format_i18n( $item->count ) . "</a></td>";
 			echo "</tr>";
 		}
 		?>
