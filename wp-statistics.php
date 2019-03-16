f13313424e3db29e705e9f1f758f2bbbf7c94160
Mehrshad Darzi <mehrshad198@gmail.com>
2019-03-16 09:43:18 +0330

    require_once main plugin file

--- wp-statistics.php
+++ wp-statistics.php
@@ -11,7 +11,7 @@
  */
 
 define( 'WP_STATISTICS_MAIN_FILE', __FILE__ );
-require plugin_dir_path( __FILE__ ) . 'includes/classes/class-wp-statistics.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-wp-statistics.php';
 
 $WP_Statistics = new WP_Statistics;
 $WP_Statistics->run();
