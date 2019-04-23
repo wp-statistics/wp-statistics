3b813f0bd3747887b9013afcced67a7b97de2e71
Mehrshad Darzi <mehrshad198@gmail.com>
2019-04-23 13:38:57 +0430

    Added $_SERVER List in Visitor IP Page

--- includes/settings/tabs/wps-visitor-ip.php
+++ includes/settings/tabs/wps-visitor-ip.php
@@ -21,7 +21,32 @@ if ( $wps_nonce_valid and $wps_admin ) {
 
 // Get IP Method
 $ip_method = WP_Statistics::getIPMethod();
+
+// Add TickBox
+add_thickbox();
+
 ?>
+    <!-- Show Help $_SERVER -->
+    <div id="list-of-php-server" style="display:none;">
+        <table>
+            <tr>
+                <td width="330" style="color: #3238fb; border-bottom: 1px solid #bcbeff;padding-top:10px;padding-bottom:10px;">
+                    <b><?php _e('$_SERVER', 'wp-statistics'); ?></b></td>
+                <td style="color: #3238fb; border-bottom: 1px solid #bcbeff;padding-top:10px;padding-bottom:10px;"> <?php _e('Value', 'wp-statistics'); ?></td>
+            </tr>
+			<?php
+			foreach ( $_SERVER as $key => $value ) {
+				?>
+                <tr>
+                    <td width="330" style="padding-top:10px;padding-bottom:10px;">
+                        <b><?php echo $key; ?></b></td>
+                    <td style="padding-top:10px;padding-bottom:10px;"> <?php echo ( $value == "" ? "-" : substr( str_replace( array( "\n", "\r" ), '', trim( $value ) ), 0, 200 ) ) . ( strlen( $value ) > 200 ? '..' : '' ); ?></td>
+                </tr>
+				<?php
+			}
+			?>
+        </table>
+    </div>
     <table class="form-table">
         <tbody>
         <tr valign="top">
@@ -34,15 +59,21 @@ $ip_method = WP_Statistics::getIPMethod();
             <th scope="row" colspan="2">
                 <code id="user_real_ip" style="padding: 15px;font-size: 30px;font-weight: 200; letter-spacing: 2px;font-family: 'Segoe UI',Roboto,Oxygen-Sans,Ubuntu,Cantarell,'Helvetica Neue',sans-serif;">
                     <script type="application/javascript">
-                        function wp_statistics_get_user_ip(json) {
-                            if (typeof json['ip'] != "undefined") {
-                                jQuery("code#user_real_ip").html(json['ip']);
-                            } else {
-                                jQuery("code#user_real_ip").html("<?php _e( 'Error connection to server. Please check your internet connection and try again.', 'wp-statistics' ); ?>");
-                            }
-                        }
+                        jQuery(document).ready(function () {
+                            jQuery.ajax({
+                                url: "https://api.ipify.org?format=json",
+                                dataType: 'json',
+                                error: function (jqXHR) {
+                                    if (jqXHR.status == 0) {
+                                        jQuery("code#user_real_ip").html("<?php _e( 'Please check your internet connection and try again.', 'wp-statistics' ); ?>");
+                                    }
+                                },
+                                success: function (json) {
+                                    jQuery("code#user_real_ip").html(json['ip']);
+                                }
+                            });
+                        });
                     </script>
-                    <script type="application/javascript" src="https://api.ipify.org?format=jsonp&callback=wp_statistics_get_user_ip"></script>
                 </code><br/><br/></th>
         </tr>
 
@@ -84,10 +115,10 @@ $ip_method = WP_Statistics::getIPMethod();
                         </td>
                         <td style="width: 250px;"> <?php echo __( 'Use Custom Header', 'wp-statistics' ); ?></td>
                         <td style="padding-left: 0px;">
-                            <input type="text" name="user_custom_header_ip_method" style="padding: 5px; width: 250px;height: 35px;" value="<?php if ( ! in_array( $ip_method, WP_Statistics::list_of_server_ip_variable() ) ) {
+                            <input type="text" name="user_custom_header_ip_method" autocomplete="off" style="padding: 5px; width: 250px;height: 35px;" value="<?php if ( ! in_array( $ip_method, WP_Statistics::list_of_server_ip_variable() ) ) {
 								echo $ip_method;
 							} ?>">
-                            <p class="description"><?php _e( 'if You are Using Custom $_SERVER in your site e.g. `HTTP_CF_CONNECTING_IP` for CloudFlare Service.', 'wp-statistics' ); ?></p>
+
                             <p class="description">
 								<?php if ( ! in_array( $ip_method, WP_Statistics::list_of_server_ip_variable() ) ) {
 									echo '<code>';
@@ -99,6 +130,8 @@ $ip_method = WP_Statistics::getIPMethod();
 								}
 								echo '</code>';
 								?></p>
+                            <p class="description"><?php _e( 'if You are Using Custom $_SERVER in your site e.g. `HTTP_CF_CONNECTING_IP` for CloudFlare Service.', 'wp-statistics' ); ?></p>
+                            <p class="description"><a href="#TB_inline?&width=850&height=600&inlineId=list-of-php-server" class="thickbox"><?php _e( 'Show all $_SERVER in Your Server.', 'wp-statistics' ); ?></a></p>
                         </td>
                     </tr>
                 </table>
