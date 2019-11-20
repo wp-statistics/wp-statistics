4874a5c816e567316321e3e73ab46f58cba72d17
Mehrshad Darzi <mehrshad198@gmail.com>
2019-11-20 19:04:03 +0330

    Fix Js Version

--- includes/classes/class-wp-statistics-frontend.php
+++ includes/classes/class-wp-statistics-frontend.php
@@ -59,7 +59,7 @@ class WP_Statistics_Frontend {
         global $WP_Statistics;
         if ( $WP_Statistics->use_cache ) {
             wp_enqueue_script( 'ua-parser', WP_Statistics::$reg['plugin-url'] . 'assets/js/ua-parser.min.js', true, '0.7.20' );
-            wp_enqueue_script( 'wp-statistics-js', WP_Statistics::$reg['plugin-url'] . 'assets/js/front.js', true, '1.0' );
+            wp_enqueue_script( 'wp-statistics-js', WP_Statistics::$reg['plugin-url'] . 'assets/js/front.js', true, WP_Statistics::$reg['version'] );
             wp_localize_script( 'wp-statistics-js', 'wps_statistics_object',
                 array(
                     'rest_url' => get_rest_url(),
