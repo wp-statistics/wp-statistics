8f6cf3a8ca4a4f0ee49b8af86f61c971a174032a
Mehrshad Darzi <mehrshad198@gmail.com>
2020-11-26 13:44:04 +0330

    Improvement Widget Referring

--- assets/dev/javascript/meta-box/referring.js
+++ assets/dev/javascript/meta-box/referring.js
@@ -4,15 +4,15 @@ wps_js.referring_meta_box = {
         let t = '';
         t += `<table width="100%" class="widefat table-stats wps-report-table"><tbody>
         <tr>
-            <td width="50%">${wps_js._('address')}</td>
-            <td width="50%">${wps_js.meta_box_lang('referring', 'references')}</td>
+            <td width="80%">${wps_js._('address')}</td>
+            <td width="20%">${wps_js.meta_box_lang('referring', 'references')}</td>
         </tr>`;
 
         args.forEach(function (value) {
             t += `<tr>
 			<td>` + wps_js.site_icon(value['domain']) + ` <a href='//${value['domain']}' title='${value['title']}' target="_blank">${value['domain']}</a>
-            <span style="display:block;padding: 0 15px;" class='wps-cursor-default'` + (value['country'].length > 2 ? ` title="${value['country']}"` : ``) + `>${value['ip']}</span></td>
-			<td style="vertical-align: middle;"><a href="${value['page_link']}">${value['number']}</a></td>
+            <span class='wps-cursor-default wps-referring-widget-ip'` + (value['country'].length > 2 ? ` title="${value['country']}"` : ``) + `>${value['ip']}</span></td>
+			<td class="wps-middle-vertical"><a href="${value['page_link']}">${value['number']}</a></td>
 			</tr>`;
         });
 
