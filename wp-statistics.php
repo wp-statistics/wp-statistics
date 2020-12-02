df4c80cd87ca7fbb76d1a9792a5093b9c9ef7eae
Mehrshad Darzi <mehrshad198@gmail.com>
2020-12-02 15:11:32 +0330

    minor improvement

--- wp-statistics.php
+++ wp-statistics.php
@@ -20,7 +20,7 @@ require_once 'includes/defines.php';
 add_filter('wp_statistics_show_welcome_page', function(){
     return false;
 }, 999);
-remove_action( 'upgrader_process_complete', 'WP_Statistics_Welcome::do_welcome', 99 );
+//remove_action( 'upgrader_process_complete', 'WP_Statistics_Welcome::do_welcome' );
 if (!class_exists('WP_Statistics_Welcome')) {
     class WP_Statistics_Welcome
     {
