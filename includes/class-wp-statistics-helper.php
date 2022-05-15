1cf5157adef92b51550ed6d714c2fca5ff0baa9e
Mostafa Soufi <mst404@gmail.com>
2022-05-15 20:56:48 +0300

    Tweak improvements in admin styles

--- includes/class-wp-statistics-helper.php
+++ includes/class-wp-statistics-helper.php
@@ -121,7 +121,7 @@ class Helper
      * @param string $style_extra
      * @return string
      */
-    public static function wp_admin_notice($text, $model = "info", $close_button = true, $id = false, $echo = true, $style_extra = 'padding:10px 0')
+    public static function wp_admin_notice($text, $model = "info", $close_button = true, $id = false, $echo = true, $style_extra = 'padding:6px 0')
     {
         $text = '
         <div class="notice notice-' . $model . '' . ($close_button === true ? " is-dismissible" : "") . '"' . ($id != false ? ' id="' . $id . '"' : '') . '>
