3053ec7ae6348387e55b927b2467be8eeb7b85c7
Ali Fallah <ali.fallah.rn@gmail.com>
2022-07-02 22:58:36 +0430

    Update online users component

--- assets/dev/javascript/meta-box/useronline.js
+++ assets/dev/javascript/meta-box/useronline.js
@@ -4,18 +4,18 @@ wps_js.useronline_meta_box = {
         let t = '';
         t += `<table class="widefat table-stats wps-report-table wps-table-fixed">
         <tr>
-        ` + (wps_js.is_active('geo_ip') ? `<td style='text-align: left;'>${wps_js._('country')}</td>` : ``) + `
-            <td style='text-align: left;'>${wps_js._('ip')}</td>
             <td width="35%" style='text-align: left;'>${wps_js._('page')}</td>
-            <td style='text-align: left;'>${wps_js._('referrer')}</td>
+            <td style='text-align: left;'>${wps_js._('referrer')}</td>`
+            + (wps_js.is_active('geo_ip') ? `<td style='text-align: left;'>${wps_js._('country')}</td>` : ``) + `
+            <td style='text-align: left;'>${wps_js._('ip')}</td>
         </tr>`;
 
         args.forEach(function (value) {
             t += `<tr>
-            ` + (wps_js.is_active('geo_ip') ? `<td style="text-align: left"><img src='${value['country']['flag']}' alt='${value['country']['name']}' title='${value['country']['name']}' class='log-tools wps-flag'/></td>` : ``) + `
-            <td style='text-align: left !important'>` + (value['hash_ip'] ? value['hash_ip'] : value['ip']['value']) + `</td>
             <td style='text-align: left !important;'><span class="wps-text-wrap">` + (value['page']['link'].length > 2 ? `<a href="${value['page']['link']}" title="${value['page']['title']}" target="_blank" class="wps-text-muted">` : ``) + value['page']['title'] + (value['page']['link'].length > 2 ? `</a>` : ``) + `</span></td>
-            <td style="text-align: left !important">${value['referred']}</td>
+            <td style="text-align: left !important">${value['referred']}</td>`
+                + (wps_js.is_active('geo_ip') ? `<td style="text-align: left"><img src='${value['country']['flag']}' alt='${value['country']['name']}' class='wps-flag'/> ${value['country']['name']}</td>` : ``) + `
+            <td style='text-align: left !important'><a href='${value['ip']['link']}'>` + (value['hash_ip'] ? value['hash_ip'] : value['ip']['value']) + `</a></td>
 			</tr>`;
         });
 
