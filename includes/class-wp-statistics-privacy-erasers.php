408dbbc87efcf76bd2467b5f78255d508d4c1745
Mostafa Soufi <mst404@gmail.com>
2022-07-19 17:19:32 +0430

    Support data privacy exporter and data eraser

--- includes/class-wp-statistics-privacy-erasers.php
+++ includes/class-wp-statistics-privacy-erasers.php
@@ -7,14 +7,14 @@ class PrivacyErasers
     /**
      * Finds and erases visitors' data by email address.
      *
-     * @param string $email_address The user email address.
+     * @param string $emailAddress The user email address.
      * @param int $page Page.
      *
      * @return array An array of personal data in name value pairs
      *
      * @since 13.2.5
      */
-    public function visitorsDataEraser($email_address, $page = 1)
+    public static function visitorsDataEraser($emailAddress, $page = 1)
     {
         $response = array(
             'items_removed'  => false,
@@ -22,6 +22,22 @@ class PrivacyErasers
             'messages'       => array(),
             'done'           => true,
         );
+
+        global $wpdb;
+
+        $visitor_table = DB::table('visitor');
+        $user          = get_user_by('email', $emailAddress);
+
+        if (!$user) {
+            return $response;
+        }
+
+        $visitors = $wpdb->query($wpdb->prepare("DELETE FROM {$visitor_table} WHERE `user_id` = %s", $user->ID));
+
+        if ($visitors) {
+            $response['messages']      = array(sprintf(__('Visitor data deleted for %s.', 'wp-statistics'), $emailAddress));
+            $response['items_removed'] = true;
+        }
 
         return $response;
     }
