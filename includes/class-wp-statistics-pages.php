8b0218e2afe8e4df3c6c3b993a0b1c2208ad47db
Ali Fallah <ali.fallah.rn@gmail.com>
2022-09-10 18:27:39 +0430

    Fix date filter issue in top pages component

--- includes/class-wp-statistics-pages.php
+++ includes/class-wp-statistics-pages.php
@@ -393,6 +393,18 @@ class Pages
 
         $args = wp_parse_args($args, $defaults);
 
+        // Check Default
+        if (empty($args['from']) and empty($args['to'])) {
+            if (array_key_exists($args['ago'], TimeZone::getDateFilters())) {
+                $dateFilter   = TimeZone::calculateDateFilter($args['ago']);
+                $args['from'] = $dateFilter['from'];
+                $args['to']   = $dateFilter['to'];
+            } elseif ($args['ago'] < 1) {
+                $args['ago'] = 'all';
+            }
+        }
+
+        // Prepare Count Day
         if (!empty($args['from']) and !empty($args['to'])) {
             $count_day = TimeZone::getNumberDayBetween($args['from'], $args['to']);
         } else {
