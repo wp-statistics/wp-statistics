5f337107086f595f10d436ec2335164931abdab8
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2022-10-10 13:02:10 +0330

    Imelemnt Filter page component

--- assets/js/datepicker/datepicker.js
+++ assets/js/datepicker/datepicker.js
@@ -1,10 +1,26 @@
-function triggerCustomDateEventListener() {
-    jQuery('.js-datepicker-input').unbind('change');
-    jQuery('.js-datepicker-input').on('change', function () {
-        console.log(jQuery(this).val())
-    })
-}
-
 jQuery( document ).ready(function() {
-    triggerCustomDateEventListener()
+    new easepick.create({
+        element: document.querySelector('.js-datepicker-input'),
+        css: [
+            wps_js.global.assets_url + '/css/datepicker/easepick.css',
+            wps_js.global.assets_url + '/css/datepicker/customize.css',
+        ],
+        plugins: ['RangePlugin'],
+        RangePlugin: {
+            tooltipNumber(num) {
+                return num;
+            },
+            locale: {
+                one: 'Day', // @todo Ali Fallah
+                other: 'Days',
+            },
+        },
+        setup(picker) {
+            picker.on('select', (e) => {
+                let startDate = new Date(e.detail.start).toISOString().slice(0, 10);
+                let endDate = new Date(e.detail.end).toISOString().slice(0, 10);
+                wps_js.run_meta_box(key, {'from': startDate, 'to': endDate});
+            });
+        },
+    });
 });
