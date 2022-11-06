60344671b9b2138d2a7c8495ed55ab1c9e053e54
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2022-11-06 12:37:31 +0330

    Replace easepick with DateRangePicker in top pages

--- assets/js/datepicker/datepicker.js
+++ assets/js/datepicker/datepicker.js
@@ -5,28 +5,10 @@ jQuery(document).on('click', function (event) {
 });
 
 jQuery( document ).ready(function() {
-    new easepick.create({
-        element: document.querySelector('.js-datepicker-input'),
-        css: [
-            wps_js.global.assets_url + '/css/datepicker/easepick.css',
-            wps_js.global.assets_url + '/css/datepicker/customize.css',
-        ],
-        plugins: ['RangePlugin'],
-        RangePlugin: {
-            tooltipNumber(num) {
-                return num;
-            },
-            locale: {
-                one: 'Day', // @todo Ali Fallah
-                other: 'Days',
-            },
-        },
-        setup(picker) {
-            picker.on('select', (e) => {
-                let startDate = new Date(e.detail.start).toISOString().slice(0, 10);
-                let endDate = new Date(e.detail.end).toISOString().slice(0, 10);
-                wps_js.run_meta_box(key, {'from': startDate, 'to': endDate});
-            });
-        },
+    const datePickerElement = jQuery('.js-datepicker-input');
+    datePickerElement.daterangepicker({"autoApply": true});
+    datePickerElement.on('apply.daterangepicker', function (ev, picker) {
+        console.log(picker.startDate.format('YYYY-MM-DD'))
+        console.log(picker.endDate.format('YYYY-MM-DD'))
     });
 });
