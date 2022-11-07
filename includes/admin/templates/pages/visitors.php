f67c5b203f55dca828bec15e56009cbde8c5f307
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2022-11-07 12:49:06 +0330

    Update the user column style on visitors table

--- includes/admin/templates/pages/visitors.php
+++ includes/admin/templates/pages/visitors.php
@@ -71,7 +71,7 @@
                                     <td style='text-align: left'><?php echo esc_attr($item['hits']); ?></td>
                                     <td style='text-align: left'>
                                         <?php if (isset($item['user']) and isset($item['user']['ID']) and $item['user']['ID'] > 0) { ?>
-                                            <a href="<?php echo esc_url(\WP_STATISTICS\Menus::admin_url('visitors', array('user_id' => $item['user']['ID']))); ?>" class="wps-text-success"><?php echo esc_attr($item['user']['user_login']); ?></a>
+                                            <a href="<?php echo esc_url(\WP_STATISTICS\Menus::admin_url('visitors', array('user_id' => $item['user']['ID']))); ?>"><?php echo esc_attr($item['user']['user_login']); ?></a>
                                         <?php } else { ?>
                                             <?php echo \WP_STATISTICS\Admin_Template::UnknownColumn(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
                                         <?php } ?>
