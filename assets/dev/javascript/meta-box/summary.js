dec3e1e1bad0a46db221e1a7a89f6367d93fb256
Ali Fallah <ali.fallah.rn@gmail.com>
2022-11-26 19:23:56 +0330

    Fixed summery component style

--- assets/dev/javascript/meta-box/summary.js
+++ assets/dev/javascript/meta-box/summary.js
@@ -8,9 +8,9 @@ wps_js.summary_meta_box = {
 
         // Show Visitors and Visits
         if (wps_js.is_active('visitors') || wps_js.is_active('visits')) {
-            t += `<tr><th width="60%"></th>`; /* @todo Add "Time" to first column. Or can I add it by myself? */
+            t += `<tr><th width="60%">` + wps_js._('time') + `</th>`;
             ["visitors", "visits"].forEach(function (key) {
-                t += `<th class="th-center">` + (wps_js.is_active(key) ? wps_js._(key) : ``) + `</th>`;
+                t += `<th class="th-right">` + (wps_js.is_active(key) ? wps_js._(key) : ``) + `</th>`;
             });
             t += `</tr>`;
             t += '</thead>';
@@ -20,7 +20,7 @@ wps_js.summary_meta_box = {
             for (let i = 0; i < summary_item.length; i++) {
                 t += `<tr><th>${wps_js._(summary_item[i])}</th>`;
                 ["visitors", "visits"].forEach(function (key) {
-                    t += `<td class="th-center">` + (wps_js.is_active(key) ? `<a href="${args[key][summary_item[i]]['link']}"><span>${args[key][summary_item[i]]['value']}</span></a>` : ``) + `</td>`;
+                    t += `<td class="th-right">` + (wps_js.is_active(key) ? `<a href="${args[key][summary_item[i]]['link']}"><span>${args[key][summary_item[i]]['value']}</span></a>` : ``) + `</td>`;
                 });
                 t += `</tr>`;
             }
