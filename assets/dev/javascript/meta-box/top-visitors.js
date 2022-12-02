00ee235803e5dbe0aed647e58c7297c5d65a37c0
Ali Fallah <ali.fallah.rn@gmail.com>
2022-12-03 01:36:39 +0330

    Fixed top visitors component style

--- assets/dev/javascript/meta-box/top-visitors.js
+++ assets/dev/javascript/meta-box/top-visitors.js
@@ -3,7 +3,7 @@ wps_js.top_visitors_meta_box = {
     view: function (args = []) {
         let t = '';
         t += `<div class="o-table-wrapper">`;
-        t += `<table class="o-table o-table--responsive"><tbody>
+        t += `<table width="100%" class="o-table o-table--responsive"><tbody>
         <tr>
             <td>${wps_js._('rank')}</td>
             <td>${wps_js._('hits')}</td>
