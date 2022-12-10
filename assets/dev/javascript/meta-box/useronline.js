e8f8c3ce290727d88cca1437f2373497bfd5d848
Ali Fallah <ali.fallah.rn@gmail.com>
2022-12-10 19:28:57 +0330

    Fixed useronline component hash_ip issue

--- assets/dev/javascript/meta-box/useronline.js
+++ assets/dev/javascript/meta-box/useronline.js
@@ -1,8 +1,8 @@
 wps_js.useronline_meta_box = {
 
     view: function (args = []) {
-        let t = '';
-        t += `<table class="o-table wps-report-table wps-table-fixed">
+        let t = '<div class="o-table-wrapper">';
+        t += `<table class="o-table table-stats wps-report-table wps-table-fixed">
         <tr>
             <td width="35%" style='text-align: left;'>${wps_js._('page')}</td>
             <td style='text-align: left;'>${wps_js._('referrer')}</td>`
@@ -15,11 +15,11 @@ wps_js.useronline_meta_box = {
             <td style='text-align: left !important;'><span class="wps-text-wrap">` + (value['page']['link'].length > 2 ? `<a href="${value['page']['link']}" title="${value['page']['title']}" target="_blank" class="wps-text-muted">` : ``) + value['page']['title'] + (value['page']['link'].length > 2 ? `</a>` : ``) + `</span></td>
             <td style="text-align: left !important">${value['referred']}</td>`
                 + (wps_js.is_active('geo_ip') ? `<td style="text-align: left"><img src='${value['country']['flag']}' alt='${value['country']['name']}' class='wps-flag'/> ${value['country']['name']}</td>` : ``) + `
-            <td style='text-align: left !important'><a href='${value['ip']['link']}'>` + (value['hash_ip'] ? value['hash_ip'] : value['ip']['value']) + `</a></td>
+            <td style='text-align: left !important'><a href='` + (value['hash_ip'] ? '#' : value['ip']['link']) + `'>` + (value['hash_ip'] ? value['hash_ip'] : value['ip']['value']) + `</a></td>
 			</tr>`;
         });
 
-        t += `</table>`;
+        t += `</table></div>`;
         return t;
     }
 
