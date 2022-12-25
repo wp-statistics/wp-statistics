65b5bccd77b64ff1d2fc59060b172c53c83bc97d
Mostafa Soufi <mst404@gmail.com>
2022-12-25 12:11:17 +0330

    Replace Add-On widgets urls to Data Plus

--- includes/admin/templates/meta-box/pages-browsers-preview.php
+++ includes/admin/templates/meta-box/pages-browsers-preview.php
@@ -1,7 +1,7 @@
 <div class="wps-admin-pages-component__preview" style="max-height: unset">
     <div class="wps-admin-pages-component__promotion">
-        <h3><?php _e('Are you looking for more reports with charts?', 'wp-statistics'); ?></h3>
-        <a href="https://wp-statistics.com/product/wp-statistics-widgets?utm_source=wp_statistics&utm_medium=display&utm_campaign=wordpress" class="button-primary" target="_blank"><?php _e('Unlock advanced widgets!', 'wp-statistics'); ?></a>
+        <h3><?php _e('Want to Include More Charts and Graphs in Your Reports?', 'wp-statistics'); ?></h3>
+        <a href="https://wp-statistics.com/2022/12/25/get-access-to-advanced-data-insights-with-data-plus-coming-soon/?utm_source=wp_statistics&utm_medium=display&utm_campaign=wordpress" class="button-primary" target="_blank"><?php _e('Get More Information', 'wp-statistics'); ?></a>
     </div>
 
     <img src="<?php echo esc_url(WP_STATISTICS_URL . 'assets/images/pages-browsers-preview.png'); ?>">
