f9d91b6667cc58a8285f904eb02bba02d836bd00
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2023-01-01 13:38:28 +0330

    Fix issues

--- assets/dev/javascript/meta-box/recent.js
+++ assets/dev/javascript/meta-box/recent.js
@@ -10,7 +10,7 @@ wps_js.recent_meta_box = {
             ` + (wps_js.is_active('geo_ip') ? `<th>${wps_js._('country')}</th>` : ``) + `
             ` + (wps_js.is_active('geo_city') ? `<th>${wps_js._('city')}</th>` : ``) + `
             <th>${wps_js._('date')}</th>
-            <th>${wps_js._('hits')}</th>
+            <th class="o-table__td--sm-width">${wps_js._('hits')}</th>
             <th>${wps_js._('ip')}</th>
             <th>${wps_js._('referrer')}</th>
         </tr></thead><tbody>`;
@@ -19,10 +19,10 @@ wps_js.recent_meta_box = {
             t += `<tr>
             <td style="text-align: left !important;" class="row-id">${++index}</td>
             <td><a class="is-normal-text" href="${value['browser']['link']}" title="${value['browser']['name']}"><img src="${value['browser']['logo']}" alt="${value['browser']['name']}" class='wps-flag log-tools' title='${value['browser']['name']}'/> ${value['browser']['name']}</a></td>
-            ` + (wps_js.is_active('geo_ip') ? `<td><img src='${value['country']['flag']}' alt='${value['country']['name']}' title='${value['country']['name']}' class='log-tools wps-flag'/> ${value['country']['name']}</td>` : ``) + `
+            ` + (wps_js.is_active('geo_ip') ? `<td><img src='${value['country']['flag']}' alt='${value['country']['name']}' title='${value['country']['name']}' class='wps-flag'/> ${value['country']['name']}</td>` : ``) + `
             ` + (wps_js.is_active('geo_city') ? `<td>${value['city']}</td>` : ``) + `
             <td>${value['date']}</td>
-            <td>${value['hits']}</td>
+            <td class="o-table__td--sm-width">${value['hits']}</td>
             <td class="o-table__link">` + (value['hash_ip'] ? value['hash_ip'] : `<a href='${value['ip']['link']}'>${value['ip']['value']}</a>`) + `</td>
             <td>${value['referred']}</td>
 			</tr>`;
