68cfc541792a40c60196b4bd5bfb8f08f1d47027
Mostafa Soufi <mostafa@veronalabs.com>
2023-01-15 14:57:24 +0330

    Improvement single page overview screenshots and select page alignment

--- includes/admin/templates/meta-box/pages-useronline-preview.php
+++ includes/admin/templates/meta-box/pages-useronline-preview.php
@@ -1,8 +1,9 @@
 <div class="wps-admin-pages-component__preview">
     <div class="wps-admin-pages-component__promotion">
-        <h3><?php _e('Get access to advanced data insights with Data Plus. Coming soon!', 'wp-statistics'); ?></h3>
-        <a href="https://wp-statistics.com/2022/12/25/get-access-to-advanced-data-insights-with-data-plus-coming-soon/?utm_source=wp_statistics&utm_medium=display&utm_campaign=wordpress" class="button-primary" target="_blank"><?php _e('Get More Information', 'wp-statistics'); ?></a>
+        <h3><?php _e('Are you looking for more reports with charts?', 'wp-statistics'); ?></h3>
+        <p><?php _e('Get access to advanced data insights with Data Plus', 'wp-statistics'); ?></p>
+        <a href="https://wp-statistics.com/product/wp-statistics-data-plus?utm_source=wp_statistics&utm_medium=display&utm_campaign=wordpress" class="button-primary" target="_blank"><?php _e('Get More Information', 'wp-statistics'); ?></a>
     </div>
 
-    <img src="<?php echo esc_url(WP_STATISTICS_URL . 'assets/images/pages-useronline-preview.png'); ?>">
+    <img src="<?php echo esc_url(WP_STATISTICS_URL . 'assets/images/pages-visitors-preview.png'); ?>">
 </div>
