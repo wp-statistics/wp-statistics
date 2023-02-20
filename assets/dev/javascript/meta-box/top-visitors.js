1651ce31433b9f227103fe2795e758e65075e3a4
Mostafa Soufi <mostafa@veronalabs.com>
2023-02-20 15:47:45 +0330

    Add class for top visitor ip

--- assets/dev/javascript/meta-box/top-visitors.js
+++ assets/dev/javascript/meta-box/top-visitors.js
@@ -22,7 +22,7 @@ wps_js.top_visitors_meta_box = {
             <td class="o-table__td--sm-width">${value['hits']}</td>
             ` + (wps_js.is_active('geo_ip') ? `<td><img src='${value['country']['flag']}' alt='${value['country']['name']}' title='${value['country']['name']}' class='log-tools wps-flag'/> ${value['country']['name']}</td>` : ``) + `
             ` + (wps_js.is_active('geo_city') ? `<td>${value['city']}</td>` : ``) + `
-            <td>` + (value['hash_ip'] ? value['hash_ip'] : `<a href='${value['ip']['link']}'>${value['ip']['value']}</a>`) + `</td>
+            <td class="wps-admin-column__ip">` + (value['hash_ip'] ? value['hash_ip'] : `<a href='${value['ip']['link']}'>${value['ip']['value']}</a>`) + `</td>
             <td><a class="is-normal-text" href="${value['browser']['link']}" title="${value['browser']['name']}"><img src="${value['browser']['logo']}" alt="${value['browser']['name']}" class='wps-flag log-tools' title='${value['browser']['name']}'/> ${value['browser']['name']}</a></td>
             <td>${value['platform']}</td>
             <td>${value['version']}</td>
