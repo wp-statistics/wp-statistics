22246fe86e47495a857457c39aafd1f041b21cb1
ighorbani <imanghorbani.ir@gmail.com>
2023-05-17 12:04:48 +0330

    Improve Notifications Default Settigns
    Make Statistical Reports enable by default

---
+++ includes/admin/templates/emails/default.php
@@ -0,0 +1,16 @@
+<?php
+return '<div class="RichText3-paragraph--withVSpacingNormal RichText3-paragraph Typography Typography--m" style = "text-align: left;" > Hello, Here is the statistical report for your website: </div>
+<ul>
+ 	<li class="RichText3-paragraph--withVSpacingNormal RichText3-paragraph Typography Typography--m" >Online Users: <strong > [wpstatistics stat = usersonline]</strong></li>
+ 	<li class="RichText3-paragraph--withVSpacingNormal RichText3-paragraph Typography Typography--m" >Today\'s Visitors: <strong> [wpstatistics stat=visitors time=today]</strong></li>
+ 	<li class="RichText3-paragraph--withVSpacingNormal RichText3-paragraph Typography Typography--m" >Today\'s Visits: <strong> [wpstatistics stat = visits time = today]</strong></li>
+ 	<li class="RichText3-paragraph--withVSpacingNormal RichText3-paragraph Typography Typography--m" >Yesterday\'s Visitors: <strong> [wpstatistics stat=visitors time=yesterday]</strong></li>
+ 	<li class="RichText3-paragraph--withVSpacingNormal RichText3-paragraph Typography Typography--m" >Yesterday\'s Visits: <strong> [wpstatistics stat = visits time = yesterday]</strong></li>
+ 	<li class="RichText3-paragraph--withVSpacingNormal RichText3-paragraph Typography Typography--m" >Total Visitors: <strong> [wpstatistics stat = visitors time = total]</strong></li>
+ 	<li class="RichText3-paragraph--withVSpacingNormal RichText3-paragraph Typography Typography--m" >Total Visits: <strong> [wpstatistics stat = visits time = total]</strong></li>
+</ul>
+<div class="RichText3-paragraph--withVSpacingNormal RichText3-paragraph Typography Typography--m" ></div>
+<div></div>
+<div class="RichText3-paragraph--withVSpacingNormal RichText3-paragraph Typography Typography--m" > Thank you for using WP Statistics!</div>
+<div class="RichText3-paragraph--withVSpacingNormal RichText3-paragraph Typography Typography--m" > Best regards,</div>';
+?>
