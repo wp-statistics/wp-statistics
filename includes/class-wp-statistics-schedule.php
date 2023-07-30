7ba3aac32474121cbf73bc11a5cf29eab62fb01a
ighorbani <imanghorbani.ir@gmail.com>
2023-07-30 14:34:07 +0330

    Improve Variable Naming

--- includes/class-wp-statistics-schedule.php
+++ includes/class-wp-statistics-schedule.php
@@ -215,8 +215,8 @@ class Schedule
     public function send_report()
     {
         // apply Filter ShortCode for email content
-        $final_text_report = Option::get('content_report');
-        $final_text_report = do_shortcode($final_text_report);
+        $email_content = Option::get('content_report');
+        $email_content = do_shortcode($email_content);
 
         // Type Send Report
         $type = Option::get('send_report');
@@ -228,7 +228,7 @@ class Schedule
              * Filter for email template content
              * @usage wp-statistics-advanced-reporting
              */
-            $email_content = apply_filters('wp_statistics_final_text_report_email', $final_text_report);
+            $final_report_text = apply_filters('wp_statistics_final_text_report_email', $email_content);
 
             /**
              * Filter for enable/disable sending email by template.
@@ -246,7 +246,7 @@ class Schedule
             $result_email = Helper::send_mail(
                 $email_receivers,
                 __('Statistical reporting', 'wp-statistics'),
-                $email_content,
+                $final_report_text,
                 $email_template
             );
 
