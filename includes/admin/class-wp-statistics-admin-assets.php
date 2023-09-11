8a5d84cdd518b423bfd2ff741d1c312b821e195b
Mostafa Soufi <mostafa.soufi@hotmail.com>
2023-09-11 18:51:55 +0330

    Improved error messages when the plugin endpoints are blocked by Ad-Blockers

--- includes/admin/class-wp-statistics-admin-assets.php
+++ includes/admin/class-wp-statistics-admin-assets.php
@@ -320,7 +320,7 @@ class Admin_Assets
             'er_valid_ip'   => __('Please enter a valid ip.', 'wp-statistics'),
             'please_wait'   => __('Please Wait ...', 'wp-statistics'),
             'user'          => __('User', 'wp-statistics'),
-            'rest_connect'  => __('Error connecting to WordPress REST API. Unblock /wp-json/wp-statistics/v2/metabox in your ad-blocker if used.', 'wp-statistics'),
+            'rest_connect'  => __('Error connecting to WordPress REST API. Disable ad-blocker for this page or unblock /wp-json/wp-statistics/v2/metabox in the ad-blocker configuration.', 'wp-statistics'),
         );
 
         // Rest-API Meta Box Url
