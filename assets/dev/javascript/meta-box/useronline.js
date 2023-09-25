a882889b44119626624aed5945ff353e24fab6bc
Ali Fallah <ali.fallah.rn@gmail.com>
2023-09-25 17:25:05 +0330

    Add User column to the Online Users component

--- assets/dev/javascript/meta-box/useronline.js
+++ assets/dev/javascript/meta-box/useronline.js
@@ -8,6 +8,7 @@ wps_js.useronline_meta_box = {
             <td>${wps_js._('referrer')}</td>`
             + (wps_js.is_active('geo_ip') ? `<td>${wps_js._('country')}</td>` : ``) + `
             <td>${wps_js._('ip')}</td>
+            <td>${wps_js._('user')}</td>
         </tr>`;
 
         args.forEach(function (value) {
@@ -15,8 +16,9 @@ wps_js.useronline_meta_box = {
             <td><span class="wps-text-wrap">` + (value['page']['link'].length > 2 ? `<a href="${value['page']['link']}" title="${value['page']['title']}" target="_blank" class="wps-text-muted is-normal-text">` : ``) + value['page']['title'] + (value['page']['link'].length > 2 ? `</a>` : ``) + `</span></td>
             <td><div class="table-cell-scroller">${value['referred']}</div></td>`
                 + (wps_js.is_active('geo_ip') ? `<td><img src='${value['country']['flag']}' alt='${value['country']['name']}' class='wps-flag wps-flag--first'/> ${value['country']['name']}</td>` : ``) + `
-            <td><a href='` + (value['hash_ip'] ? '#' : value['ip']['link']) + `'>` + (value['hash_ip'] ? value['hash_ip'] : value['ip']['value']) + `</a></td>
-			</tr>`;
+            <td><a href='` + (value['hash_ip'] ? '#' : value['ip']['link']) + `'>` + (value['hash_ip'] ? value['hash_ip'] : value['ip']['value']) + `</a></td>`
+                + (typeof value['user'] != 'undefined' ? `<td><a href='` + wps_js.global.admin_url + `user-edit.php?user_id=` + value['user']['ID'] + `'>` + value['user']['name'] + ` (ID: #` + value['user']['ID'] + `)</a></td>` : `-`) +
+                `</tr>`;
         });
 
         t += `</table></div>`;
