04e4b4f96b3dde402699ec59dfa5fed2e74872c5
alifallahrn <ali.fallahrn@gmail.com>
2023-11-11 03:49:11 +0330

    Update settings/rules and settings/dashboard copies

--- includes/admin/templates/layout/tabs-settings.php
+++ includes/admin/templates/layout/tabs-settings.php
@@ -4,8 +4,8 @@
         <li class="tab-link" data-tab="ip-configuration-settings"><?php _e('IP Detection Settings', 'wp-statistics'); ?></li>
         <li class="tab-link" data-tab="privacy-settings"><?php _e('Privacy', 'wp-statistics'); ?></li>
         <li class="tab-link" data-tab="notifications-settings"><?php _e('Notifications', 'wp-statistics'); ?></li>
-        <li class="tab-link" data-tab="overview-display-settings"><?php _e('Dashboard', 'wp-statistics'); ?></li>
-        <li class="tab-link" data-tab="access-settings"><?php _e('Roles', 'wp-statistics'); ?></li>
+        <li class="tab-link" data-tab="overview-display-settings"><?php _e('Dashboard Widgets', 'wp-statistics'); ?></li>
+        <li class="tab-link" data-tab="access-settings"><?php _e('Access Control', 'wp-statistics'); ?></li>
         <li class="tab-link" data-tab="exclusions-settings"><?php _e('Exclusions', 'wp-statistics'); ?></li>
         <li class="tab-link" data-tab="externals-settings"><?php _e('Externals', 'wp-statistics'); ?></li>
         <li class="tab-link" data-tab="maintenance-settings"><?php _e('Maintenance', 'wp-statistics'); ?></li>
--- includes/admin/templates/settings/overview-display.php
+++ includes/admin/templates/settings/overview-display.php
@@ -5,43 +5,31 @@ if ($wps_admin) {
     <div class="postbox">
         <table class="form-table">
             <tbody>
-
             <tr valign="top">
-                <td scope="row" colspan="2"><?php _e('The following items are global to all users.',
-                        'wp-statistics'); ?></td>
-            </tr>
-
-            <tr valign="top">
-                <th scope="row" colspan="2"><h3><?php _e('Dashboard', 'wp-statistics'); ?></h3></th>
+                <th scope="row" colspan="2"><h3><?php _e('Dashboard Widgets', 'wp-statistics'); ?></h3></th>
             </tr>
 
             <tr valign="top">
                 <th scope="row">
-                    <label for="disable-map"><?php _e('Dashboard Widgets:', 'wp-statistics'); ?></label>
+                    <label for="disable-map"><?php _e('Display WP Statistics Widgets', 'wp-statistics'); ?></label>
                 </th>
 
                 <td>
                     <input id="disable-dashboard" type="checkbox" value="1" name="wps_disable_dashboard" <?php echo WP_STATISTICS\Option::get('disable_dashboard') == true ? "checked='checked'" : ''; ?>>
                     <label for="disable-dashboard"><?php _e('Disable', 'wp-statistics'); ?></label>
-                    <p class="description"><?php _e('This option will disable dashboard widgets.',
-                            'wp-statistics'); ?></p>
+                    <p class="description"><?php _e('Toggle this setting to enable or disable the display of WP Statistics widgets on your WordPress dashboard.', 'wp-statistics'); ?></p>
                 </td>
             </tr>
 
             <tr valign="top">
-                <th scope="row" colspan="2"><h3><?php _e('Map', 'wp-statistics'); ?></h3></th>
-            </tr>
-
-            <tr valign="top">
                 <th scope="row">
-                    <label for="disable-map"><?php _e('Map Display:', 'wp-statistics'); ?></label>
+                    <label for="disable-map"><?php _e('Enable Geographic Map', 'wp-statistics'); ?></label>
                 </th>
 
                 <td>
                     <input id="disable-map" type="checkbox" value="1" name="wps_disable_map" <?php echo WP_STATISTICS\Option::get('disable_map') == true ? "checked='checked'" : ''; ?>>
                     <label for="disable-map"><?php _e('Disable', 'wp-statistics'); ?></label>
-                    <p class="description"><?php _e('This option will disable the map display.',
-                            'wp-statistics'); ?></p>
+                    <p class="description"><?php _e('Activate this setting to display a geographic map on your dashboard, providing insights into your visitors\' locations.', 'wp-statistics'); ?></p>
                 </td>
             </tr>
 
