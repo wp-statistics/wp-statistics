a356a18bb8e76311420f162001407e252e05d8e5
alifallahrn <ali.fallahrn@gmail.com>
2023-11-13 00:37:56 +0330

    Update optimization/historical menu title

--- includes/admin/templates/layout/tabs-optimization.php
+++ includes/admin/templates/layout/tabs-optimization.php
@@ -4,5 +4,5 @@
     <li class="tab-link" data-tab="purging"><?php _e('Data Cleanup', 'wp-statistics'); ?></li>
     <li class="tab-link" data-tab="database"><?php _e('Database Management', 'wp-statistics'); ?></li>
     <li class="tab-link" data-tab="updates"><?php _e('Plugin Maintenance', 'wp-statistics'); ?></li>
-    <li class="tab-link" data-tab="historical"><?php _e('Archived Data', 'wp-statistics'); ?></li>
+    <li class="tab-link" data-tab="historical"><?php _e('Historical Data', 'wp-statistics'); ?></li>
 </ul>
