4ac54ba8bc9f4c5c6af917c998bed105ec3e19b2
alifallahrn <ali.fallahrn@gmail.com>
2023-12-05 11:34:59 +0330

    Update settings/exclusions copies

--- includes/admin/templates/layout/tabs-settings.php
+++ includes/admin/templates/layout/tabs-settings.php
@@ -6,7 +6,7 @@
         <li class="tab-link" data-tab="notifications-settings"><?php _e('Admin Notifications', 'wp-statistics'); ?></li>
         <li class="tab-link" data-tab="overview-display-settings"><?php _e('Dashboard Widgets', 'wp-statistics'); ?></li>
         <li class="tab-link" data-tab="access-settings"><?php _e('Access Control', 'wp-statistics'); ?></li>
-        <li class="tab-link" data-tab="exclusions-settings"><?php _e('Exclusions', 'wp-statistics'); ?></li>
+        <li class="tab-link" data-tab="exclusions-settings"><?php _e('Filtering & Exceptions', 'wp-statistics'); ?></li>
         <li class="tab-link" data-tab="externals-settings"><?php _e('Externals', 'wp-statistics'); ?></li>
         <li class="tab-link" data-tab="maintenance-settings"><?php _e('Maintenance', 'wp-statistics'); ?></li>
         <li class="tab-link" data-tab="reset-settings"><?php _e('Restore Default Settings', 'wp-statistics'); ?></li>
