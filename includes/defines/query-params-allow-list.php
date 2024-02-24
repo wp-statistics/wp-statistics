f7c611ab46e66dd4fdcbdd10bb3bc40e1eb2415a
foadadeli <foad.adeli@gmail.com>
2024-02-24 11:52:23 +0330

    Reorder default query params

--- includes/defines/query-params-allow-list.php
+++ includes/defines/query-params-allow-list.php
@@ -5,12 +5,12 @@
 $wps_query_params_allow_list_array = array(
     'ref',
     'source',
-    'utm_id',
     'utm_source',
     'utm_medium',
     'utm_campaign',
     'utm_content',
     'utm_term',
+    'utm_id',
     's',
     'p'
 );
