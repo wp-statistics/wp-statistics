865af4aa430c515d2dfb9e87ac05e2f4bcd21625
foadadeli <foad.adeli@gmail.com>
2024-02-28 19:17:04 +0330

    Revert "Implement new design - part 1"
    This reverts commit 492f284bf6ecef87ad15b99c7eb8eb3d35dcf39e.

--- includes/admin/templates/layout/title-after.php
+++ includes/admin/templates/layout/title-after.php
@@ -1,6 +1,4 @@
-<div class="wps-wrap__title">
-    <h2 class="wps_title"><?php _e('Setting', 'wp-statistics') ?></h2>
-    <?php do_action('wp_statistics_after_admin_page_title'); ?>
-</div>
 <div class="wps-wrap__main">
+    <h2 class="wps_title"></h2>
+    <?php do_action('wp_statistics_after_admin_page_title'); ?>
     <div class="wp-clearfix"></div>
