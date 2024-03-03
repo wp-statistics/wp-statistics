f82310fc2cb8b7ed5150a8fb7a797da0fa7b62e7
foadadeli <foad.adeli@gmail.com>
2024-03-03 16:22:40 +0330

    Add tooltip to tabbed page header

--- includes/admin/templates/layout/tabbed-page-header.php
+++ includes/admin/templates/layout/tabbed-page-header.php
@@ -1,6 +1,14 @@
 <div class="wps-wrap__main">
     <div class="wps-page-header">
-        <h2 class="wps_title"><?php echo(isset($title) ? esc_attr($title) : (function_exists('get_admin_page_title') ? get_admin_page_title() : '')); ?></h2>
+        <h2 class="wps_title">
+            <span><?php echo(isset($title) ? esc_html($title) : (function_exists('get_admin_page_title') ? get_admin_page_title() : '')); ?></span>
+            <?php if (!empty($tooltip)) : ?>
+                <span class="wps-tooltip" title="<?php echo esc_attr($tooltip); ?>"><i class="wps-tooltip-icon info"></i></span>
+            <?php endif; ?>
+        </h2>
+
+        <p class="wps_description"><?php echo !empty($description) ? esc_html($description) : '' ?></p>
+
         <?php do_action('wp_statistics_after_admin_page_title'); ?>
         <div class="wp-clearfix"></div>
         <div class="wps-datepicker">
@@ -12,11 +20,10 @@
                     <li class="wps-tab-link <?php echo esc_attr($tab['class']); ?>">
                         <a href="<?php echo esc_attr($tab['link']); ?>">
                             <?php echo esc_html($tab['title']); ?>
-                            <?php if (isset($tab['tooltip']) && !empty($tab['tooltip'])) : ?>
-                                <span class="wps-tooltip" title="<?php echo esc_html($tab['tooltip']); ?>"><i class="wps-tooltip-icon info"></i></span>
+                            <?php if (!empty($tab['tooltip'])) : ?>
+                                <span class="wps-tooltip" title="<?php echo esc_attr($tab['tooltip']); ?>"><i class="wps-tooltip-icon info"></i></span>
                             <?php endif; ?>
                         </a>
-
                     </li>
                 <?php } ?>
             </ul>
