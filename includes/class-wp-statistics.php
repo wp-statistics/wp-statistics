ca6ac28ba2b1a21bcdb6ffffb4cd5d48929c986b
Foad <foad.adeli@gmail.com>
2024-04-02 09:45:15 +0330

    Refactor author analytics functionality

--- includes/class-wp-statistics.php
+++ includes/class-wp-statistics.php
@@ -2,6 +2,7 @@
 
 # Exit if accessed directly
 use WP_STATISTICS\Helper;
+use WP_Statistics\Service\AuthorAnalytics\AuthorAnalyticsManager;
 use WP_Statistics\Service\PrivacyAudit\PrivacyAuditManager;
 
 defined('ABSPATH') || exit;
@@ -202,13 +203,13 @@ final class WP_Statistics
             require_once WP_STATISTICS_DIR . 'includes/admin/pages/class-wp-statistics-admin-page-visitors.php';
             require_once WP_STATISTICS_DIR . 'includes/admin/pages/class-wp-statistics-admin-page-country.php';
             require_once WP_STATISTICS_DIR . 'includes/admin/pages/class-wp-statistics-admin-page-taxonomies.php';
-            require_once WP_STATISTICS_DIR . 'includes/admin/pages/class-wp-statistics-admin-page-authors.php';
             require_once WP_STATISTICS_DIR . 'includes/admin/pages/class-wp-statistics-admin-page-browsers.php';
             require_once WP_STATISTICS_DIR . 'includes/admin/pages/class-wp-statistics-admin-page-platforms.php';
             require_once WP_STATISTICS_DIR . 'includes/admin/pages/class-wp-statistics-admin-page-top-visitors-today.php';
             require_once WP_STATISTICS_DIR . 'includes/admin/pages/class-wp-statistics-admin-page-exclusions.php';
 
-            $privacyAudit = new PrivacyAuditManager();
+            $privacyAudit    = new PrivacyAuditManager();
+            $authorAnalytics = new AuthorAnalyticsManager();
         }
 
         // WordPress ShortCode and Widget
---
+++ src/Service/AuthorAnalytics/AuthorAnalyticsManager.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace WP_Statistics\Service\AuthorAnalytics;
+
+class AuthorAnalyticsManager
+{
+
+    public function __construct()
+    {
+        add_filter('wp_statistics_admin_menu_list', [$this, 'addMenuItem']);
+    }
+
+    /**
+     * Add menu item
+     *
+     * @param array $items
+     * @return array
+     */
+    public function addMenuItem($items)
+    {
+        $newItem = [
+            'author_analytics' => [
+                'sub'       => 'overview',
+                'pages'     => array('pages' => true),
+                'title'     => esc_html__('Author Analytics', 'wp-statistics'),
+                'page_url'  => 'author-analytics',
+                'callback'  => AuthorAnalyticsPage::class,
+            ]
+        ];
+
+        array_splice($items, 13, 0, $newItem);
+
+        return $items;
+    }
+
+}
