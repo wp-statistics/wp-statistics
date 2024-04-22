d90b1a1715a5c33453306b7832cc9a5d2b45d722
Vilhelm Tamm <vilhelm.tamm@gmail.com>
2024-04-23 00:41:38 +0200

    Update class-wp-statistics-helper.php
    Flip $a and $b variables in `compare_uri_hits_int` method

--- includes/class-wp-statistics-helper.php
+++ includes/class-wp-statistics-helper.php
@@ -986,9 +986,9 @@ class Helper
      */
     public static function compare_uri_hits_int($a, $b)
     {
-        if ($a[1] == $b[1]) return 0;
-        if ($a[1] > $b[1]) return 1;
-        if ($a[1] < $b[1]) return -1;
+        if ($b[1] == $a[1]) return 0;
+        if ($b[1] > $a[1]) return 1;
+        if ($b[1] < $a[1]) return -1;
 
     }
 
