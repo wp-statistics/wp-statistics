a23dc67152994a3b731f72a06e2fe0ec2a924324
Foad <foad.adeli@gmail.com>
2024-04-23 10:59:56 +0330

    Refactor code for further extension

--- src/Service/ContentAnalytics/ContentAnalyticsPage.php
+++ src/Service/ContentAnalytics/ContentAnalyticsPage.php
@@ -8,10 +8,6 @@ use InvalidArgumentException;
 
 class ContentAnalyticsPage
 {
-    private static $tabs = [
-        'post',
-        'page',
-    ];
 
     public function __construct()
     {
@@ -28,10 +24,63 @@ class ContentAnalyticsPage
             }
 
             // Throw error when invalid tab provided
-            if (isset($_GET['tab']) && !in_array($_GET['tab'], self::$tabs)) {
+            if (isset($_GET['tab']) && !in_array($_GET['tab'], array_keys($this->getTabs()))) {
                 throw new InvalidArgumentException(esc_html__('Invalid tab provided.', 'wp-statistics'));
             }
         }
+    }
+
+    /**
+     * Get active tab
+     * 
+     * @return string
+     */
+    public function getCurrentTab()
+    {
+        return isset($_GET['tab']) ? sanitize_text_field($_GET['tab']) : 'post';
+    }
+
+    /**
+     * Get available tabs
+     * 
+     * @return array
+     */
+    public function getTabs()
+    {
+        $currentTab = $this->getCurrentTab();
+
+        $tabs = [
+            'post' => [
+                'link'    => Menus::admin_url(Menus::get_page_slug('content-analytics'), ['tab' => 'post']),
+                'title'   => esc_html__('Posts', 'wp-statistics'),
+                'tooltip' => esc_html__('Tab Tooltip', 'wp-statistics'),
+                'class'   => $currentTab === 'post' ? 'current' : '',
+            ],
+            'page' => [
+                'link'    => Menus::admin_url(Menus::get_page_slug('content-analytics'), ['tab' => 'page']),
+                'title'   => esc_html__('Pages', 'wp-statistics'),
+                'tooltip' => esc_html__('Tab Tooltip', 'wp-statistics'),
+                'class'   => $currentTab === 'page' ? 'current' : '',
+            ]
+        ];
+
+        return apply_filters('wp_statistics_content_analytics_tabs', $tabs, $currentTab);
+    }
+
+    /**
+     * Get tab data
+     * 
+     * @param string
+     * @return array
+     */
+    public function getTabData($currentTab)
+    {
+        $data = [];
+
+        // Todo: get data based on current tab
+
+        $data = apply_filters('wp_statistics_content_analytics_tab_data', $data, $currentTab);
+        return $data;
     }
 
     /**
@@ -47,7 +96,7 @@ class ContentAnalyticsPage
      */
     private function contentView()
     {
-        $currentTab = isset($_GET['tab']) ? sanitize_text_field($_GET['tab']) : 'post';
+        $currentTab = $this->getCurrentTab();
 
         $args = [
             'title'      => esc_html__('Content Analytics', 'wp-statistics'),
@@ -56,20 +105,8 @@ class ContentAnalyticsPage
             'pagination' => Admin_Template::getCurrentPaged(),
             'custom_get' => ['tab' => $currentTab],
             'DateRang'   => Admin_Template::DateRange(),
-            'tabs'       => [
-                [
-                    'link'    => Menus::admin_url(Menus::get_page_slug('content-analytics'), ['tab' => 'post']),
-                    'title'   => esc_html__('Posts', 'wp-statistics'),
-                    'tooltip' => esc_html__('Tab Tooltip', 'wp-statistics'),
-                    'class'   => $currentTab === 'post' ? 'current' : '',
-                ],
-                [
-                    'link'    => Menus::admin_url(Menus::get_page_slug('content-analytics'), ['tab' => 'page']),
-                    'title'   => esc_html__('Pages', 'wp-statistics'),
-                    'tooltip' => esc_html__('Tab Tooltip', 'wp-statistics'),
-                    'class'   => $currentTab === 'page' ? 'current' : '',
-                ]
-            ]
+            'tabs'       => $this->getTabs(),
+            'data'       => $this->getTabData($currentTab)
         ];
 
         Admin_Template::get_template(['layout/header', 'layout/tabbed-page-header', "pages/content-analytics/$currentTab", 'layout/postbox.hide', 'layout/footer'], $args);
