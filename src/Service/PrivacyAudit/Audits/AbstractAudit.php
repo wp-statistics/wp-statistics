e9a83bf2692a3ab54e71efd4d437ed6b16feef61
Foad <foad.adeli@gmail.com>
2024-05-01 14:35:40 +0330

    Add resolve method to the abstract audit

--- src/Service/PrivacyAudit/Audits/AbstractAudit.php
+++ src/Service/PrivacyAudit/Audits/AbstractAudit.php
@@ -3,6 +3,8 @@ namespace WP_Statistics\Service\PrivacyAudit\Audits;
 
 abstract class AbstractAudit 
 {
+    abstract static public function resolve();
+    
     abstract static public function getStatus();
     
     abstract static public function getStates();
