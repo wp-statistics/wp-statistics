b88d8a7e69aea1ecb8019579dd83c4a8018e1809
Foad <foad.adeli@gmail.com>
2024-05-01 10:36:09 +0330

    Minor improvements

--- src/Service/PrivacyAudit/Audits/RecordUserPageVisits.php
+++ src/Service/PrivacyAudit/Audits/RecordUserPageVisits.php
@@ -42,12 +42,12 @@ class RecordUserPageVisits extends AbstractAudit
                 'status'        => 'warning',
                 'title'         => esc_html__('The “Record User Page Visits” feature is currently enabled on your website.', 'wp-statistics'),
                 'notes'         => __('<p>This status means that individual user page visits and WordPress user IDs are being actively tracked. While this functionality provides valuable insights into user behavior, it’s important to handle the collected data responsibly.</p><p>Why is this important?</p>
-                        <p>Enabling this feature necessitates a careful approach to privacy and data protection. To maintain compliance with privacy laws such as GDPR and CCPA, and to uphold user trust, please ensure the following:</p>
+                    <p>Enabling this feature necessitates a careful approach to privacy and data protection. To maintain compliance with privacy laws such as GDPR and CCPA, and to uphold user trust, please ensure the following:</p>
                     <ol>
-                            <li><b>Transparency:</b> Your website’s privacy policy should clearly describe the data collection practices, including the specific types of data collected and their intended use.</li>
-                            <li><b>Informed Consent:</b> Adequate measures are in place to inform users about the data collection and to obtain their consent where necessary. This may include consent banners, notifications, or other user interfaces that clearly communicate this information.</li>
-                            <li><b>Review and Action:</b> Regularly review the necessity of keeping this feature enabled. If the feature is no longer needed, or if you wish to enhance user privacy, consider disabling it. Refer to our guide on <a href="https://chat.openai.com/c/42e80126-57c8-4608-9440-b13d86b8bf5a#" target="_blank">Adjusting Your Privacy Settings</a> for detailed instructions on managing this feature.</li>
-                        </ol>', 'wp-statistics'),
+                        <li><b>Transparency:</b> Your website’s privacy policy should clearly describe the data collection practices, including the specific types of data collected and their intended use.</li>
+                        <li><b>Informed Consent:</b> Adequate measures are in place to inform users about the data collection and to obtain their consent where necessary. This may include consent banners, notifications, or other user interfaces that clearly communicate this information.</li>
+                        <li><b>Review and Action:</b> Regularly review the necessity of keeping this feature enabled. If the feature is no longer needed, or if you wish to enhance user privacy, consider disabling it. Refer to our guide on <a href="https://chat.openai.com/c/42e80126-57c8-4608-9440-b13d86b8bf5a#" target="_blank">Adjusting Your Privacy Settings</a> for detailed instructions on managing this feature.</li>
+                    </ol>', 'wp-statistics'),
                 'compliance'    => [
                     'key'   => 'action_required',
                     'value' => esc_html__('Action Required', 'wp-statistics'),
