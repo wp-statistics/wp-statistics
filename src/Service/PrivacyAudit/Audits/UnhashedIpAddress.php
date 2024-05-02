26a1d20fd621e3c69b588e351d7a5a733f60ec7b
Foad <foad.adeli@gmail.com>
2024-05-02 16:55:05 +0330

    Minor improvements

--- src/Service/PrivacyAudit/Audits/UnhashedIpAddress.php
+++ src/Service/PrivacyAudit/Audits/UnhashedIpAddress.php
@@ -49,7 +49,7 @@ class UnhashedIpAddress extends AbstractAudit
                     'value'         => esc_html__('Resolve', 'wp-statistics'),
                     'confirm'       => true,
                     'removable'     => true,
-                    'confirm_text'  => esc_html__('This will replace all IP addresses in the database with hash values and cannot be undo, are you sure?', 'wp-statistics'),
+                    'confirm_text'  => esc_html__('This will replace all IP addresses in the database with hash values and cannot be undone, are you sure?', 'wp-statistics'),
                     'success_text'  => esc_html__('Successfully anonymized IP addresses using hash values.', 'wp-statistics')
                 ]
             ]
