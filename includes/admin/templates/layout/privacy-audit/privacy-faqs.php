2f5659dd948e033d9be8250fc42aeb03f6579a8c
Foad <foad.adeli@gmail.com>
2024-05-05 12:27:49 +0330

    Remove unnecessary files

---
+++ includes/admin/templates/layout/privacy-audit/privacy-faqs.php
@@ -0,0 +1,34 @@
+<div class="wps-flex wps-privacy-head">
+    <div class="postbox-container wps-half-card">
+        <div class="postbox wps-postbox-wrap wps-privacy-questions">
+            <div class="postbox-header">
+                <h2><?php esc_html_e('Popular Questions', 'wp-statistics'); ?></h2>
+                <p><?php esc_html_e('Audit List: Monitor Compliance Status', 'wp-statistics'); ?></p>
+            </div>
+            <div class="wps-privacy-list__items loading">
+                <!-- load faq items using js -->
+            </div>
+        </div>
+    </div>
+    <div class="postbox-container wps-half-card">
+        <div class="postbox wps-postbox-wrap wps-privacy-resources">
+            <div class="postbox-header">
+                <h2><?php esc_html_e('Useful Privacy Resources and References', 'wp-statistics'); ?></h2>
+                <p><?php esc_html_e('Discover essential resources, laws, and articles to help you navigate the complexities of online privacy and ensure your site\'s compliance', 'wp-statistics'); ?></p>
+            </div>
+            <div class="postbox-content">
+                <ul>
+                    <li>
+                        <?php echo _e('<a href="https://wp-statistics.com/resources/avoiding-pii-data-collection/?utm_source=wp-statistics&utm_medium=link&utm_campaign=privacy?" target="blank">Configuring for Maximum Privacy: A Guide to Avoiding PII Data Collection</a>', 'wp-statistics'); ?>
+                    </li>
+                    <li>
+                        <?php echo _e('<a href="https://wp-statistics.com/resources/what-we-collect/?utm_source=wp-statistics&utm_medium=link&utm_campaign=privacy" target="blank">GDPR, CCPA and cookie law compliant site analytics</a>', 'wp-statistics'); ?>
+                    </li>
+                    <li>
+                        <?php echo _e('<a href="https://wp-statistics.com/resources/counting-unique-visitors-without-cookies/?utm_source=wp-statistics&utm_medium=link&utm_campaign=privacy" target="blank">Counting Unique Visitors Without Cookies</a>', 'wp-statistics'); ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
