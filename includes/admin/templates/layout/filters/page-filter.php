906e22f5d2d5965d858f65a41564dffd99d24a3b
Foad <foad.adeli@gmail.com>
2024-05-06 16:06:31 +0330

    Make page filter dynamic

--- includes/admin/templates/layout/filters/page-filter.php
+++ includes/admin/templates/layout/filters/page-filter.php
@@ -1,12 +1,34 @@
- <div class="wps-filter-page wps-head-filters__item loading" action="/src"   method="post">
-    <div class="wps-dropdown">
-        <label class="selectedItemLabel"><?php  esc_html_e('Page:', 'wp-statistics'); ?> </label>
-        <button type="button" class="dropbtn"><?php  esc_html_e('All', 'wp-statistics'); ?></button>
-        <div class="dropdown-content">
-            <a href="#" data-index="0" class="selected"><?php  esc_html_e('All', 'wp-statistics'); ?></a>
-            <a href="#" data-index="1" title="Home Page: Home">Home Page: Home</a>
-            <a href="#" data-index="2" title="Portfolio 3 Column">Portfolio 3 Column</a>
-            <a href="#" data-index="2" title="Resume">Resume</a>
+<?php 
+$queryKey       = 'pid';
+$postType       = isset($_GET['pt']) ? sanitize_text_field($_GET['pt']) : false;
+$postId         = isset($_GET[$queryKey]) ? intval($_GET[$queryKey]) : false;
+$selectedOption = $postId ? get_the_title($postId) : __('All', 'wp-statistics');
+$baseUrl        = remove_query_arg($queryKey);
+
+$pageQuery = ['post_status' => 'publish', 'posts_per_page' => -1];
+if ($postType) {
+    $pageQuery['post_type'] = $postType;
+}
+$posts = new WP_Query($pageQuery);
+?>
+
+<?php if ($posts->have_posts()) : ?>
+    <div class="wps-filter-page wps-head-filters__item loading">
+        <div class="wps-dropdown">
+            <label class="selectedItemLabel"><?php esc_html_e('Page:', 'wp-statistics'); ?> </label>
+            <button type="button" class="dropbtn"><?php echo esc_html($selectedOption); ?></button>
+            <div class="dropdown-content">
+                <a href="<?php echo esc_url($baseUrl) ?>" data-index="0" class="<?php echo !$postId ? 'selected' : '' ?>"><?php  esc_html_e('All', 'wp-statistics'); ?></a>
+
+                <?php while ($posts->have_posts()) : $posts->the_post(); ?>
+                    <?php $url = add_query_arg([$queryKey => get_the_ID()]); ?>
+
+                    <a href="<?php echo esc_url($url) ?>" data-index="<?php echo esc_attr($key + 1) ?>" title="<?php echo esc_attr(get_the_title()) ?>" class="<?php echo get_the_ID() == $postId ? 'selected' : '' ?>">
+                        <?php the_title() ?>
+                    </a>
+                <?php endwhile; ?>
+                <?php wp_reset_query(); ?>
+            </div>
         </div>
     </div>
-</div>
+<?php endif; ?>
