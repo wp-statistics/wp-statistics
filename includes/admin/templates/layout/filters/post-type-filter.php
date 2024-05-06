62f2c6ca4c516b5edec5ae8b1801107870ac53fa
Foad <foad.adeli@gmail.com>
2024-05-06 15:38:49 +0330

    Make post type filter dynamic

--- includes/admin/templates/layout/filters/post-type-filter.php
+++ includes/admin/templates/layout/filters/post-type-filter.php
@@ -1,11 +1,24 @@
-<div class="wps-filter-post-type loading wps-head-filters__item" action="/src"   method="post">
+<?php 
+$queryKey         = 'pt';
+$baseUrl          = remove_query_arg($queryKey);
+$postTypes        = array_values(array_diff(get_post_types(['public' => true]), ['attachment']));
+$selectedPostType = isset($_GET[$queryKey]) ? sanitize_text_field($_GET[$queryKey]) : false;
+?>
+
+<div class="wps-filter-post-type wps-head-filters__item loading">
     <div class="wps-dropdown">
-        <label class="selectedItemLabel"><?php  esc_html_e('Post Type:', 'wp-statistics'); ?> </label>
-        <button type="button" class="dropbtn"><?php  esc_html_e('Post', 'wp-statistics'); ?></button>
+        <label class="selectedItemLabel"><?php esc_html_e('Post Type:', 'wp-statistics'); ?> </label>
+        <button type="button" class="dropbtn"><?php echo $selectedPostType ? esc_html(ucfirst($selectedPostType)) : esc_html__('All', 'wp-statistics'); ?></button>
         <div class="dropdown-content">
-            <a href="#" data-index="0" class="selected" title=""><?php  esc_html_e('Post', 'wp-statistics'); ?></a>
-            <a href="#" data-index="1" title="Landing Page">Landing Page</a>
-            <a href="#" data-index="2" title="Template">Template</a>
+            <a href="<?php echo esc_url($baseUrl) ?>" data-index="0" class="<?php echo !$selectedPostType ? 'selected' : '' ?>"><?php esc_html_e('All', 'wp-statistics'); ?></a>
+
+            <?php foreach ($postTypes as $key => $postType) : ?>
+                <?php $url = add_query_arg([$queryKey => $postType]); ?>
+
+                <a href="<?php echo esc_url($url) ?>" data-index="<?php echo esc_attr($key + 1) ?>" title="<?php echo esc_attr(ucfirst($postType)) ?>" class="<?php echo $selectedPostType == $postType ? 'selected' : '' ?>">
+                    <?php echo esc_html(ucfirst($postType)) ?>
+                </a>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
