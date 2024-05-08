ea93d9cb5c7fde3f62ee747e735d3f208208a33d
Foad <foad.adeli@gmail.com>
2024-05-08 14:21:58 +0330

    Add php-doc for readability

--- src/Service/PrivacyAudit/Audits/AbstractAudit.php
+++ src/Service/PrivacyAudit/Audits/AbstractAudit.php
@@ -8,8 +8,18 @@ abstract class AbstractAudit
 {
     static public $optionKey;
 
+    /**
+     * Returns an array of all states with details an audit item could have.
+     * 
+     * @return array
+     */
     abstract static public function getStates();
         
+    /**
+     * Returns current the current state details of the audit item based on its current status
+     * 
+     * @return array
+     */
     static public function getState() 
     {
         $states = static::getStates();
@@ -19,6 +29,11 @@ abstract class AbstractAudit
         return $currentState;
     }
 
+    /**
+     * Returns a boolean value indicating whether the audit item has any action or not
+     * 
+     * @return bool
+     */
     static public function hasAction()
     {
         $states     = static::getStates();
@@ -26,26 +41,49 @@ abstract class AbstractAudit
         return !empty($hasAction) ? true : false;
     }
 
+    /**
+     * Get the privacy status of the audit item stored in `wp_statistics_privacy_status` option.
+     * If the audit is resolved from Privacy Audit page, return 'passed' otherwise 'action_required'.
+     * 
+     * @return string
+     */
     public static function getStatus()
     {
         return PrivacyStatusOption::get(static::$optionKey, 'action_required');
     }
 
-    public static function isOptionEnabled()
-    {
-        return Option::get(static::$optionKey) == true;
-    }
-
+    /**
+     * Based on the `wp_statistics_options` option, return the privacy status of the audit item. 
+     * If the option is enabled in the settings, returns 'passed', otherwise 'action_required'
+     * 
+     * @return string
+     */
     public static function getStatusByOption()
     {
         return self::isOptionEnabled() ? 'passed' : 'action_required';
     }
 
+    /**
+     * If the option related to the audit item is enabled in the settings return true, otherwise false
+     * 
+     * @return bool
+     */
+    public static function isOptionEnabled()
+    {
+        return Option::get(static::$optionKey) == true;
+    }
+
+    /**
+     * Update audit item status to 'passed' when resolve button is clicked.
+     */
     public static function resolve()
     {
         PrivacyStatusOption::update(static::$optionKey, 'passed');
     }
 
+    /**
+     * Update audit item status to 'action_required' when undo button is clicked.
+     */
     public static function undo()
     {
         PrivacyStatusOption::update(static::$optionKey, 'action_required');
