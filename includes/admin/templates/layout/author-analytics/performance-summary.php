0ca0f21ea8218b1f625d7aeb21c8563696222b05
Foad <foad.adeli@gmail.com>
2024-05-13 13:25:04 +0330

    Improve author performance page

--- includes/admin/templates/layout/author-analytics/performance-summary.php
+++ includes/admin/templates/layout/author-analytics/performance-summary.php
@@ -1,30 +1,34 @@
-<div class="wps-card wps-card__icon wps-card__icon--<?php echo $icon_class ?>">
+<div class="wps-card wps-card__icon wps-card__icon--<?php echo esc_attr($icon_class) ?>">
     <div class="wps-card__title">
         <h2>
             <?php echo $title_text ?>
-            <?php if(isset($tooltip_text)):?>
+            <?php if (isset($tooltip_text)) : ?>
                 <span class="wps-tooltip" title="<?php echo esc_attr($tooltip_text); ?>"><i class="wps-tooltip-icon info"></i></span>
             <?php endif?>
         </h2>
     </div>
+    
     <div class="wps-card__summary">
         <div class="wps-card__summary--title">
-            <span><?php echo $total ?></span>
+            <span><?php echo esc_html($total) ?></span>
             <span><?php echo esc_html__('Total', 'wp-statistics') ?></span>
         </div>
-        <?php if(isset($active)):?>
+
+        <?php if (isset($active)) : ?>
             <div class="wps-card__summary--active">
-                <span><?php echo $active ?></span>
+                <span><?php echo esc_html($active) ?></span>
                 <span><?php echo esc_html__('Active', 'wp-statistics') ?></span>
             </div>
-        <?php endif?>
+        <?php endif ?>
+
         <div class="wps-card__summary--avg">
-            <?php if(isset($avg)):?>
-                <span><?php echo $avg ?></span>
-            <?php endif?>
-            <?php if(isset($avg_title)):?>
-                <span><?php echo $avg_title ?></span>
-            <?php endif?>
+            <?php if (isset($avg)) : ?>
+                <span><?php echo esc_html($avg) ?></span>
+            <?php endif ?>
+
+            <?php if (isset($avg_title)) : ?>
+                <span><?php echo esc_html($avg_title) ?></span>
+            <?php endif ?>
         </div>
      </div>
 </div>
--- includes/admin/templates/pages/author-analytics/authors-performance.php
+++ includes/admin/templates/pages/author-analytics/authors-performance.php
@@ -1,57 +1,70 @@
+<?php use WP_STATISTICS\Admin_Template; ?>
+
 <div class="metabox-holder" id="authors-performance">
     <div class="postbox-container" id="wps-postbox-container-1">
         <?php
-        use WP_STATISTICS\Admin_Template;
+            $items = [
+                [
+                    'title_text'   => esc_html__('Authors', 'wp-statistics'),
+                    'tooltip_text' => esc_html__('Authors tooltip', 'wp-statistics'),
+                    'icon_class'   => 'authors',
+                    'total'        => '2.5K',
+                    'active'       => '46',
+                    'avg'          => '10.56',
+                    'avg_title'    => esc_html__('Post/Authors', 'wp-statistics')
+                ],
+                [
+                    'title_text'   => esc_html__('Views', 'wp-statistics'),
+                    'tooltip_text' => esc_html__('Views tooltip', 'wp-statistics'),
+                    'icon_class'   => 'views',
+                    'total'        => '35.1M',
+                    'avg'          => '16.2K',
+                    'avg_title'    => esc_html__('Avg. Per Post', 'wp-statistics')
+                ],
+                [
+                    'title_text'   => esc_html__('Words', 'wp-statistics'),
+                    'tooltip_text' => esc_html__('Words tooltip', 'wp-statistics'),
+                    'icon_class'   => 'words',
+                    'total'        => '25.2M',
+                    'avg'          => '8.2K',
+                    'avg_title'    => esc_html__('Avg. Per Post', 'wp-statistics')
+                ],
+                [
+                    'title_text'   => esc_html__('Comments', 'wp-statistics'),
+                    'tooltip_text' => esc_html__('Comments tooltip', 'wp-statistics'),
+                    'icon_class'   => 'comments',
+                    'total'        => '61K',
+                    'avg'          => '300',
+                    'avg_title'    => esc_html__('Avg. Per Post', 'wp-statistics')
+                ],
+            ];
 
-        function generate_item_args($title, $tooltip, $icon, $total, $active = null, $avg = null, $avg_title = null) {
-            return array(
-                'title_text'   => esc_html__($title, 'wp-statistics'),
-                'tooltip_text' => esc_html__($tooltip, 'wp-statistics'),
-                'icon_class'   => $icon,
-                'total'        => $total,
-                'active'       => $active,
-                'avg'          => $avg,
-                'avg_title'    => esc_html__($avg_title ?? 'Avg. Per Post', 'wp-statistics'),
-            );
-        }
-
-        $items = array(
-            generate_item_args('Authors', 'Authors tooltip', 'authors', '2.5K', '46', '10.56', 'Post/Authors'),
-            generate_item_args('Views', 'Views tooltip', 'views', '35.1M', null, '16.2K'),
-            generate_item_args('Words', 'Words tooltip', 'words', '25.2M', null, '8.2K'),
-            generate_item_args('Comments', 'Comments tooltip', 'comments', '61K', null, '300')
-        );
-
-        foreach ($items as $args) {
-            Admin_Template::get_template(array('layout/author-analytics/performance-summary'), $args);
-        }
+            foreach ($items as $args) {
+                Admin_Template::get_template(['layout/author-analytics/performance-summary'], $args);
+            }
         ?>
     </div>
 
     <div class="postbox-container" id="wps-postbox-container-2">
         <?php
-            $overview_args = array(
+            $overview_args = [
                 'title_text'   => esc_html__('Publishing Overview', 'wp-statistics'),
                 'tooltip_text'   => esc_html__('Publishing Overview tooltip', 'wp-statistics'),
                 'title_description'   => esc_html__('Last 12 Months', 'wp-statistics'),
-             );
-            Admin_Template::get_template(array('layout/author-analytics/publishing-overview'), $overview_args);
-        ?>
-
-        <?php
-        $top_authors_args = array(
-            'title_text'   => esc_html__('Top Authors', 'wp-statistics'),
-            'tooltip_text'   => esc_html__('Top Authors tooltip', 'wp-statistics'),
-        );
-        Admin_Template::get_template(array('layout/author-analytics/top-authors'), $top_authors_args);
-        ?>
-
-        <?php
-        $posts_args = array(
-            'title_text'   => esc_html__('Views/Published Posts', 'wp-statistics'),
-            'tooltip_text'   => esc_html__('Views/Published Posts tooltip', 'wp-statistics'),
-        );
-        Admin_Template::get_template(array('layout/author-analytics/published-posts'), $posts_args);
+            ];
+            Admin_Template::get_template(['layout/author-analytics/publishing-overview'], $overview_args);
+            
+            $top_authors_args = [
+                'title_text'    => esc_html__('Top Authors', 'wp-statistics'),
+                'tooltip_text'  => esc_html__('Top Authors tooltip', 'wp-statistics'),
+            ];
+            Admin_Template::get_template(['layout/author-analytics/top-authors'], $top_authors_args);
+            
+            $posts_args = [
+                'title_text'   => esc_html__('Views/Published Posts', 'wp-statistics'),
+                'tooltip_text'   => esc_html__('Views/Published Posts tooltip', 'wp-statistics'),
+            ];
+            Admin_Template::get_template(['layout/author-analytics/published-posts'], $posts_args);
         ?>
     </div>
 </div>
