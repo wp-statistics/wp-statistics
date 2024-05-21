53ea8d4a3a138a53232f3384d7c3d7dc5a488fda
Mostafa Soufi <mostafa.soufi@hotmail.com>
2024-05-21 13:43:41 +0200

    Add Models

---
+++ src/Models/DataProvider.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace WP_Statistics\Models;
+
+use WP_STATISTICS\DB;
+use WP_STATISTICS\Helper;
+
+/**
+ * Todo object cache, hooks, filters, etc
+ */
+abstract class DataProvider
+{
+    public function __construct()
+    {
+        global $wpdb;
+
+        $this->db = $wpdb;
+    }
+
+    /**
+     * Generates SQL conditions based on the given arguments.
+     *
+     * @param array $args An array of arguments to generate the SQL conditions.
+     * @return string The generated SQL conditions.
+     */
+    protected function generateSqlConditions($args)
+    {
+        $sql = " WHERE post_status = 'publish'";
+
+        // Post type condition
+        if (!empty($args['post_type'])) {
+            $sql .= $this->db->prepare(" AND post_type = '%s'", $args['post_type']);
+        } else {
+            $postTypes = "'" . implode("', '", Helper::get_list_post_type()) . "'";
+            $sql       .= " AND post_type IN ($postTypes)";
+        }
+
+        // Date condition
+        if (!empty($args['from']) && !empty($args['to'])) {
+            $sql .= $this->db->prepare(' AND (Date(post_date) BETWEEN %s AND %s)', $args['from'], $args['to']);
+        }
+
+        return $sql;
+    }
+
+    protected function exceute($sql)
+    {
+        return $this->db->query($sql);
+    }
+
+    protected function getVar($sql)
+    {
+        return $this->db->get_var($sql);
+    }
+
+    protected function lastQuery()
+    {
+        return $this->db->last_query;
+    }
+
+    protected function visitorTable()
+    {
+        return DB::table('visitor');
+    }
+
+    protected function visitTable()
+    {
+        return DB::table('visit');
+    }
+}
