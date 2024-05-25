2a89fcd1b3914644ec6978d02922a59de002d3ac
Foad <foad.adeli@gmail.com>
2024-05-25 21:27:20 +0330

    Minor improvements

--- includes/admin/templates/layout/author-analytics/top-authors.php
+++ includes/admin/templates/layout/author-analytics/top-authors.php
@@ -19,47 +19,49 @@ use WP_STATISTICS\Menus;
                 <label for="author-views"><?php esc_html_e('Views', 'wp-statistics') ?></label>
                 <div class="wps-author-tabs__content">
                     <?php
-                    $users = ["Navid" => "15.1K", "Mostafa" => "12.5K", "Byimrez" => "8.3K", "James" => "5.6K", "Emily" => "4.7K"];
-                    $counter = 1; // Initialize counter variable
-                    foreach ($users as $name => $pageviews) : ?>
-                        <a class="wps-author-tabs__item" href="<?php echo esc_url(admin_url('admin.php?page=wps_author-analytics_page&author_id=1')); ?>">
-                            <div class="wps-author-tabs__item--image">
-                                <?php $user = wp_get_current_user();
-                                    if ($user) : ?>
-                                    <span>#<?php echo esc_html($counter); ?></span>
-                                    <img src="<?php echo esc_url(get_avatar_url($user->ID)); ?>" alt="<?php echo esc_html($name); ?>"/>
-                                <?php endif ?>
-                            </div>
-                            <div class="wps-author-tabs__item--content">
-                                <h3><?php echo esc_html($name); ?></h3>
-                                <span><?php echo esc_html($pageviews); ?> <?php esc_html_e('page views', 'wp-statistics') ?></span>
-                            </div>
-                        </a>
-                        <?php $counter++;?>
-                    <?php endforeach; ?>
+                        $users = ["Navid" => "15.1K", "Mostafa" => "12.5K", "Byimrez" => "8.3K", "James" => "5.6K", "Emily" => "4.7K"];
+                        $counter = 1; 
+                        foreach ($users as $name => $views) : ?>
+                            <a class="wps-author-tabs__item" href="<?php echo esc_url(admin_url('admin.php?page=wps_author-analytics_page&author_id=1')); ?>">
+                                <div class="wps-author-tabs__item--image">
+                                    <?php $user = wp_get_current_user();
+                                        if ($user) : ?>
+                                        <span># <?php echo esc_html($counter); ?></span>
+                                        <img src="<?php echo esc_url(get_avatar_url($user->ID)); ?>" alt="<?php echo esc_html($name); ?>"/>
+                                    <?php endif ?>
+                                </div>
+                                <div class="wps-author-tabs__item--content">
+                                    <h3><?php echo esc_html($name); ?></h3>
+                                    <span><?php echo esc_html($views); ?> <?php esc_html_e('page views', 'wp-statistics') ?></span>
+                                </div>
+                            </a>
+                            <?php $counter++;
+                        endforeach; 
+                    ?>
                 </div>
                 <input type="radio" name="tabs" id="author-publishing">
                 <label for="author-publishing"><?php esc_html_e('Publishing', 'wp-statistics') ?></label>
                 <div class="wps-author-tabs__content">
                     <?php
-                    $users = ["Navid" => "15.1K", "Mostafa" => "12.5K", "Byimrez" => "8.3K", "James" => "5.6K"];
-                    $counter = 1; 
-                    foreach ($users as $name => $pageviews) : ?>
-                        <a class="wps-author-tabs__item" href="">
-                            <div class="wps-author-tabs__item--image">
-                                <?php $user = wp_get_current_user();
-                                    if ($user) : ?>
-                                    <span>#<?php echo esc_html($counter); ?></span>
-                                    <img src="<?php echo esc_url(get_avatar_url($user->ID)); ?>" alt="<?php echo esc_html($name); ?>"/>
-                                <?php endif ?>
-                            </div>
-                            <div class="wps-author-tabs__item--content">
-                                <h3><?php echo esc_html($name); ?></h3>
-                                <span><?php echo esc_html($pageviews); ?> <?php esc_html_e('page publish', 'wp-statistics') ?></span>
-                            </div>
-                        </a>
-                        <?php $counter++;?>
-                    <?php endforeach; ?>
+                        $users = ["Navid" => "15.1K", "Mostafa" => "12.5K", "Byimrez" => "8.3K", "James" => "5.6K"];
+                        $counter = 1; 
+                        foreach ($users as $name => $publishes) : ?>
+                            <a class="wps-author-tabs__item" href="">
+                                <div class="wps-author-tabs__item--image">
+                                    <?php $user = wp_get_current_user();
+                                        if ($user) : ?>
+                                        <span># <?php echo esc_html($counter); ?></span>
+                                        <img src="<?php echo esc_url(get_avatar_url($user->ID)); ?>" alt="<?php echo esc_html($name); ?>"/>
+                                    <?php endif ?>
+                                </div>
+                                <div class="wps-author-tabs__item--content">
+                                    <h3><?php echo esc_html($name); ?></h3>
+                                    <span><?php echo esc_html($publishes); ?> <?php esc_html_e('page publish', 'wp-statistics') ?></span>
+                                </div>
+                            </a>
+                            <?php $counter++;
+                        endforeach; 
+                    ?>
                 </div>
             </div>
         </div>
@@ -69,72 +71,75 @@ use WP_STATISTICS\Menus;
                 <label for="comments-post"><?php esc_html_e('Comments/Post', 'wp-statistics') ?></label>
                 <div class="wps-author-tabs__content">
                     <?php
-                    $users = ["Navid" => "15.1Avg", "Mostafa" => "12.5Avg", "Byimrez" => "8.3Avg", "James" => "5.6Avg", "Emily" => "4.7Avg"];
-                    $counter = 1; 
-                    foreach ($users as $name => $pageviews) : ?>
-                        <a class="wps-author-tabs__item" href="">
-                            <div class="wps-author-tabs__item--image">
-                                <?php $user = wp_get_current_user();
-                                if ($user) : ?>
-                                    <span>#<?php echo esc_html($counter); ?></span>
-                                    <img src="<?php echo esc_url(get_avatar_url($user->ID)); ?>" alt="<?php echo esc_html($name); ?>"/>
-                                <?php endif ?>
-                            </div>
-                            <div class="wps-author-tabs__item--content">
-                                <h3><?php echo esc_html($name); ?></h3>
-                                <span><?php echo esc_html($pageviews) . ' ' . esc_html__('comments/post', 'wp-statistics'); ?> </span>
-                            </div>
-                        </a>
-                        <?php $counter++;?>
-                    <?php endforeach; ?>
+                        $users = ["Navid" => "15.1Avg", "Mostafa" => "12.5Avg", "Byimrez" => "8.3Avg", "James" => "5.6Avg", "Emily" => "4.7Avg"];
+                        $counter = 1; 
+                        foreach ($users as $name => $avgComments) : ?>
+                            <a class="wps-author-tabs__item" href="">
+                                <div class="wps-author-tabs__item--image">
+                                    <?php $user = wp_get_current_user();
+                                    if ($user) : ?>
+                                        <span># <?php echo esc_html($counter); ?></span>
+                                        <img src="<?php echo esc_url(get_avatar_url($user->ID)); ?>" alt="<?php echo esc_html($name); ?>"/>
+                                    <?php endif ?>
+                                </div>
+                                <div class="wps-author-tabs__item--content">
+                                    <h3><?php echo esc_html($name); ?></h3>
+                                    <span><?php echo esc_html($avgComments) . ' ' . esc_html__('comments/post', 'wp-statistics'); ?> </span>
+                                </div>
+                            </a>
+                            <?php $counter++;
+                        endforeach; 
+                    ?>
                 </div>
 
                 <input type="radio" name="side-tabs" id="views-post">
                 <label for="views-post"><?php esc_html_e('Views/Post', 'wp-statistics') ?></label>
                 <div class="wps-author-tabs__content">
                     <?php
-                    $users = ["Navid" => "15.1Avg", "Mostafa" => "12.5Avg", "Byimrez" => "8.3Avg", "James" => "5.6Avg"];
-                    $counter = 1; 
-                    foreach ($users as $name => $pageviews) : ?>
-                        <a class="wps-author-tabs__item" href="">
-                            <div class="wps-author-tabs__item--image">
-                                <?php $user = wp_get_current_user();
-                                if ($user) : ?>
-                                    <span>#<?php echo esc_html($counter); ?></span>
-                                    <img src="<?php echo esc_url(get_avatar_url($user->ID)); ?>" alt="<?php echo esc_html($name); ?>"/>
-                                <?php endif ?>
-                            </div>
-                            <div class="wps-author-tabs__item--content">
-                                <h3><?php echo esc_html($name); ?></h3>
-                                <span><?php echo esc_html($pageviews); ?> <?php esc_html_e('views/post', 'wp-statistics') ?></span>
-                            </div>
-                        </a>
-                        <?php $counter++;?>
-                    <?php endforeach; ?>
+                        $users = ["Navid" => "15.1Avg", "Mostafa" => "12.5Avg", "Byimrez" => "8.3Avg", "James" => "5.6Avg"];
+                        $counter = 1; 
+                        foreach ($users as $name => $avgPosts) : ?>
+                            <a class="wps-author-tabs__item" href="">
+                                <div class="wps-author-tabs__item--image">
+                                    <?php $user = wp_get_current_user();
+                                    if ($user) : ?>
+                                        <span># <?php echo esc_html($counter); ?></span>
+                                        <img src="<?php echo esc_url(get_avatar_url($user->ID)); ?>" alt="<?php echo esc_html($name); ?>"/>
+                                    <?php endif ?>
+                                </div>
+                                <div class="wps-author-tabs__item--content">
+                                    <h3><?php echo esc_html($name); ?></h3>
+                                    <span><?php echo esc_html($avgPosts); ?> <?php esc_html_e('views/post', 'wp-statistics') ?></span>
+                                </div>
+                            </a>
+                            <?php $counter++;
+                        endforeach; 
+                    ?>
                 </div>
 
                 <input type="radio" name="side-tabs" id="words-post">
                 <label for="words-post"><?php esc_html_e('Words/Post', 'wp-statistics') ?></label>
                 <div class="wps-author-tabs__content">
                     <?php
-                    $users = ["Navid" => "15.1Avg", "Mostafa" => "12.5Avg", "Byimrez" => "8.3Avg"];
-                    $counter = 1; 
-                    foreach ($users as $name => $pageviews) : ?>
-                        <a class="wps-author-tabs__item" href="">
-                            <div class="wps-author-tabs__item--image">
-                                <?php $user = wp_get_current_user();
-                                if ($user) : ?>
-                                    <span>#<?php echo esc_html($counter); ?></span>
-                                    <img src="<?php echo esc_url(get_avatar_url($user->ID)); ?>" alt="<?php echo esc_html($name); ?>"/>
-                                <?php endif ?>
-                            </div>
-                            <div class="wps-author-tabs__item--content">
-                                <h3><?php echo esc_html($name); ?></h3>
-                                <span><?php echo esc_html($pageviews); ?> <?php esc_html_e('words/post', 'wp-statistics') ?></span>
-                            </div>
-                        </a>
-                        <?php $counter++;?>
-                    <?php endforeach; ?>
+                        $users = ["Navid" => "15.1Avg", "Mostafa" => "12.5Avg", "Byimrez" => "8.3Avg"];
+                        $counter = 1; 
+                        foreach ($users as $name => $avgWords) : ?>
+                            <a class="wps-author-tabs__item" href="">
+                                <div class="wps-author-tabs__item--image">
+                                    <?php $user = wp_get_current_user();
+                                    if ($user) : ?>
+                                        <span># <?php echo esc_html($counter); ?></span>
+                                        <img src="<?php echo esc_url(get_avatar_url($user->ID)); ?>" alt="<?php echo esc_html($name); ?>"/>
+                                    <?php endif ?>
+                                </div>
+                                <div class="wps-author-tabs__item--content">
+                                    <h3><?php echo esc_html($name); ?></h3>
+                                    <span><?php echo esc_html($avgWords); ?> <?php esc_html_e('words/post', 'wp-statistics') ?></span>
+                                </div>
+                            </a>
+                            <?php $counter++;
+                        endforeach; 
+                    ?>
                 </div>
             </div>
         </div>
