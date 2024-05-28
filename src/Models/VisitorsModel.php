79c0fc4e836c15ba100331e3b38e1566dadd6b0d
Foad <foad.adeli@gmail.com>
2024-05-28 12:11:50 +0330

    Add visitors model

---
+++ src/Models/VisitorsModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace WP_Statistics\Models;
+use WP_Statistics\Utils\Query;
+
+
+class VisitorsModel extends DataProvider
+{
+
+    public function countVisitors($args = [], $bypassCache = false)
+    {
+        $args = $this->parseArgs($args, [
+            'from'      => '',
+            'to'        => '',
+            'post_type' => '',
+            'author_id' => '',
+            'post_id'   => ''
+        ]);
+
+        $result = Query::select('COUNT(DISTINCT visitor_id) as total_visitors')
+            ->from('visitor_relationships')
+            ->join('pages', ['visitor_relationships.page_id', 'pages.page_id'], [], 'LEFT')
+            ->join('posts', ['posts.ID', 'pages.id'], [], 'LEFT')
+            ->where('post_type', 'IN', $args['post_type'])
+            ->whereDate('visitor_relationships.date', [$args['from'], $args['to']])
+            ->where('post_author', '=', $args['author_id'])
+            ->where('posts.ID', '=', $args['post_id'])
+            ->bypassCache($bypassCache)
+            ->getVar();
+
+        return $result ? $result : 0;
+    }
+
+}
