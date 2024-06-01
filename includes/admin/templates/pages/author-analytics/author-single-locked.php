437c7ab9cc8dc4d8fc09ccdc88e8955218108b12
Nora <mwdrad@gmail.com>
2024-06-01 13:28:19 +0330

    Add author stats locked page

--- includes/admin/templates/pages/author-analytics/author-single-locked.php
+++ includes/admin/templates/pages/author-analytics/author-single-locked.php
@@ -1,1 +1,37 @@
-<?php echo 'LOCKED SINGLE AUTHOR VIEW!'; ?>
+<div class="wps-author-premium-feature">
+    <div class="wps-author-premium-feature__head">
+        <h1>
+            <?php echo esc_html__('This Feature is Part of the', 'wp-statistics') ?>
+            <span><?php echo esc_html__('DataPlus Add-On', 'wp-statistics') ?></span>
+        </h1>
+        <p>
+            <?php echo esc_html__('The settings on this page are part of the DataPlus add-on, which enhances WP Statistics by expanding tracking capabilities and providing detailed visitor insights.', 'wp-statistics') ?>
+        </p>
+    </div>
+    <div class="wps-author-premium__items">
+        <div class="wps-author-premium__item">
+            <?php echo esc_html__('Track custom post types and taxonomies.', 'wp-statistics') ?>
+        </div>
+        <div class="wps-author-premium__item">
+            <?php echo esc_html__('Use advanced filtering for specific query parameters and UTM tags.', 'wp-statistics') ?>
+        </div>
+        <div class="wps-author-premium__item">
+            <?php echo esc_html__('Monitor outbound link clicks and downloads.', 'wp-statistics') ?>
+        </div>
+        <div class="wps-author-premium__item">
+            <?php echo esc_html__('Compare weekly traffic and view hourly visitor patterns.', 'wp-statistics') ?>
+        </div>
+        <div class="wps-author-premium__item">
+            <?php echo esc_html__('Analyze individual content pieces with detailed widgets.', 'wp-statistics') ?>
+        </div>
+    </div>
+    <div class="wps-author-premium__info">
+        <?php echo esc_html__('Unlock deeper insights into your website\'s performance with DataPlus.', 'wp-statistics') ?>
+    </div>
+    <a target="_blank" class="button button-primary" href="">
+        <?php echo esc_html__('Upgrade Now.', 'wp-statistics') ?>
+    </a>
+</div>
+<div class="wps-author-premium-image">
+    <img  src="<?php echo esc_url(WP_STATISTICS_URL . 'assets/images/author.png'); ?>"/>
+</div>
--- src/Service/AuthorAnalytics/Views/SingleAuthorView.php
+++ src/Service/AuthorAnalytics/Views/SingleAuthorView.php
@@ -2,11 +2,17 @@
 namespace WP_Statistics\Service\AuthorAnalytics\Views;
 
 use WP_STATISTICS\Admin_Template;
+use WP_STATISTICS\Menus;
 
 class SingleAuthorView
 {
     public function view()
     {
-        Admin_Template::get_template(['layout/header', 'layout/title', 'pages/author-analytics/author-single-locked', 'layout/footer']);
+        $args = [
+            'title'      => esc_html__('Detailed Author Stats Locked: DataPlus Add-On Required', 'wp-statistics'),
+            'backUrl'       => Menus::admin_url('author-analytics'),
+            'backTitle'     => esc_html__('Authors Performance', 'wp-statistics'),
+            ];
+        Admin_Template::get_template(['layout/header', 'layout/title', 'pages/author-analytics/author-single-locked', 'layout/footer'], $args);
     }
 }
