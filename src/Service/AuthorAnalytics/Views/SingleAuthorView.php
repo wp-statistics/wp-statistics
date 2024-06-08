f71bbf9aeff819d3ba7348d710b0c22f537e8fbe
Foad <foad.adeli@gmail.com>
2024-06-08 10:41:14 +0330

    Minor improvements

--- src/Service/AuthorAnalytics/Views/SingleAuthorView.php
+++ src/Service/AuthorAnalytics/Views/SingleAuthorView.php
@@ -9,10 +9,11 @@ class SingleAuthorView
     public function view()
     {
         $args = [
-            'title'      => esc_html__('Detailed Author Stats Locked: DataPlus Add-On Required', 'wp-statistics'),
-            'backUrl'       => Menus::admin_url('author-analytics'),
-            'backTitle'     => esc_html__('Authors Performance', 'wp-statistics'),
-            ];
+            'title'     => esc_html__('Detailed Author Stats Locked: DataPlus Add-On Required', 'wp-statistics'),
+            'backUrl'   => Menus::admin_url('author-analytics'),
+            'backTitle' => esc_html__('Authors Performance', 'wp-statistics'),
+        ];
+
         Admin_Template::get_template(['layout/header', 'layout/title', 'pages/author-analytics/author-single-locked', 'layout/footer'], $args);
     }
 }
