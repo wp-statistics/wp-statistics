faad4e00d951245d766d9e5eac127c4c3bf7d1b5
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-06-08 17:31:01 +0330

    Use AssetNameObfuscator in Assets class

--- src/Components/Assets.php
+++ src/Components/Assets.php
@@ -20,22 +20,32 @@ class Assets
     public static $plugin_url = WP_STATISTICS_URL;
 
     /**
+     * Plugin DIR in WordPress
+     *
+     * @var     string
+     * @example http://srv/www/wp.site/wp-content/plugins/my-plugin/
+     */
+    public static $plugin_dir = WP_STATISTICS_DIR;
+
+    /**
      * Enqueue a script.
      *
-     * @param string $handle The script handle.
-     * @param string $src The source URL of the script.
-     * @param array $deps An array of script dependencies.
-     * @param array $localize An array of data to be localized.
-     * @param bool $inFooter Whether to enqueue the script in the footer.
-     * @return void
-     * @example Assets::script('admin', 'dist/admin.js', ['jquery'], ['foo' => 'bar'], true);
+     * @param   string  $handle     The script handle.
+     * @param   string  $src        The source URL of the script.
+     * @param   array   $deps       An array of script dependencies.
+     * @param   array   $localize   An array of data to be localized.
+     * @param   bool    $inFooter   Whether to enqueue the script in the footer.
+     * @param   bool    $obfuscate  Ofuscate/Randomize asset's file name.
+     * 
+     * @return  void
+     * @example Assets::script('admin', 'dist/admin.js', ['jquery'], ['foo' => 'bar'], true, false);
      */
-    public static function script($handle, $src, $deps = [], $localize = [], $inFooter = false)
+    public static function script($handle, $src, $deps = [], $localize = [], $inFooter = false, $obfuscate = false)
     {
         $object = self::getObject($handle);
         $handle = self::getHandle($handle);
 
-        wp_enqueue_script($handle, self::getSrc($src), $deps, WP_STATISTICS_VERSION, $inFooter);
+        wp_enqueue_script($handle, self::getSrc($src, $obfuscate), $deps, WP_STATISTICS_VERSION, $inFooter);
 
         if ($localize) {
             $localize = apply_filters("wp_statistics_localize_{$handle}", $localize);
@@ -47,15 +57,17 @@ class Assets
     /**
      * Register a script.
      *
-     * @param string $handle The script handle.
-     * @param string $src The source URL of the script.
-     * @param array $deps An array of script dependencies.
-     * @param string|null $version Optional. The version of the script. Defaults to plugin version.
-     * @param bool $inFooter Whether to enqueue the script in the footer.
-     * @return void
-     * @example Assets::registerScript('chartjs', 'js/chart.min.js', [], '3.7.1');
+     * @param   string      $handle     The script handle.
+     * @param   string      $src        The source URL of the script.
+     * @param   array       $deps       An array of script dependencies.
+     * @param   string|null $version    Optional. The version of the script. Defaults to plugin version.
+     * @param   bool        $inFooter   Whether to enqueue the script in the footer.
+     * @param   bool        $obfuscate  Ofuscate/Randomize asset's file name.
+     * 
+     * @return  void
+     * @example Assets::registerScript('chartjs', 'js/chart.min.js', [], '3.7.1', false, false);
      */
-    public static function registerScript($handle, $src, $deps = [], $version = null, $inFooter = false)
+    public static function registerScript($handle, $src, $deps = [], $version = null, $inFooter = false, $obfuscate = false)
     {
         // Get the handle for the script
         $handle = self::getHandle($handle);
@@ -66,22 +78,24 @@ class Assets
         }
 
         // Register the script with WordPress
-        wp_register_script($handle, self::getSrc($src), $deps, $version, $inFooter);
+        wp_register_script($handle, self::getSrc($src, $obfuscate), $deps, $version, $inFooter);
     }
 
     /**
      * Enqueue a style.
      *
-     * @param string $handle The style handle.
-     * @param string $src The source URL of the style.
-     * @param array $deps An array of style dependencies.
-     * @param string $media The context which style needs to be loaded: all, print, or screen
-     * @return void
-     * @example Assets::style('admin', 'dist/admin.css', ['jquery'], 'all');
+     * @param   string  $handle     The style handle.
+     * @param   string  $src        The source URL of the style.
+     * @param   array   $deps       An array of style dependencies.
+     * @param   string  $media      The context which style needs to be loaded: all, print, or screen
+     * @param   bool    $obfuscate  Ofuscate/Randomize asset's file name.
+     * 
+     * @return  void
+     * @example Assets::style('admin', 'dist/admin.css', ['jquery'], 'all', false);
      */
-    public static function style($handle, $src, $deps = [], $media = 'all')
+    public static function style($handle, $src, $deps = [], $media = 'all', $obfuscate = false)
     {
-        wp_enqueue_style(self::getHandle($handle), self::getSrc($src), $deps, WP_STATISTICS_VERSION, $media);
+        wp_enqueue_style(self::getHandle($handle), self::getSrc($src, $obfuscate), $deps, WP_STATISTICS_VERSION, $media);
     }
 
     /**
@@ -98,11 +112,18 @@ class Assets
     /**
      * Get the source URL for the script/style.
      *
-     * @param string $src The source URL.
-     * @return string
+     * @param   string  $src        The source URL.
+     * @param   bool    $obfuscate  Ofuscate/Randomize asset's file name.
+     *
+     * @return  string
      */
-    private static function getSrc($src)
+    private static function getSrc($src, $obfuscate = false)
     {
+        if ($obfuscate) {
+            $file = new AssetNameObfuscator(self::$asset_dir . '/' . $src);
+            return $file->getHashedFileUrl();
+        }
+
         return self::$plugin_url . self::$asset_dir . '/' . $src;
     }
 
