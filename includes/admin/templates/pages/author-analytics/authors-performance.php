0757ea68bdd0d8cb99263a6806d8241777f5ebb7
Foad <foad.adeli@gmail.com>
2024-06-10 14:27:49 +0330

    Update copies

--- includes/admin/templates/pages/author-analytics/authors-performance.php
+++ includes/admin/templates/pages/author-analytics/authors-performance.php
@@ -3,8 +3,9 @@ use WP_STATISTICS\Helper;
 use WP_Statistics\Utils\Request;
 use WP_STATISTICS\Admin_Template;
 
-$postType       = Request::get('pt', 'post');
-$postTypeLabel  = Helper::getPostTypeName($postType, true);
+$postType            = Request::get('pt', 'post');
+$postTypeSingularLabel       = Helper::getPostTypeName($postType, true);
+$postTypePluralLabel = Helper::getPostTypeName($postType);
 ?>
 
 <div class="metabox-holder" id="authors-performance">
@@ -13,39 +14,54 @@ $postTypeLabel  = Helper::getPostTypeName($postType, true);
             $items = [
                 [
                     'title'        => esc_html__('Authors', 'wp-statistics'),
-                    'tooltip'      => esc_html__('Total number of authors contributing content. Active authors have published at least one post in the selected period.', 'wp-statistics'),
+                    'tooltip'      => sprintf(
+                        esc_html__('Total number of authors contributing content. Active authors have published at least one %s in the selected period.', 'wp-statistics'), 
+                        strtolower($postTypeSingularLabel)
+                    ),
                     'icon_class'   => 'authors',
                     'total'        => Helper::formatNumberWithUnit($data['authors']['total']),
                     'active'       => Helper::formatNumberWithUnit($data['authors']['active']),
                     'avg'          => Helper::formatNumberWithUnit($data['authors']['avg']),
-                    'avg_title'    => sprintf(esc_html__('%s/Authors', 'wp-statistics'), $postTypeLabel)
+                    'avg_title'    => sprintf(esc_html__('%s/Authors', 'wp-statistics'), $postTypeSingularLabel)
                 ],
                 [
                     'title'        => esc_html__('Views', 'wp-statistics'),
-                    'tooltip'      => esc_html__('Total number of views across all posts by all authors. Average views per post is calculated by dividing total views by the number of posts.', 'wp-statistics'),
+                    'tooltip'      => sprintf(
+                        esc_html__('Total number of views across all %1$s by all authors. Average views per %2$s is calculated by dividing total views by the number of %1$s.', 'wp-statistics'), 
+                        strtolower($postTypePluralLabel), 
+                        strtolower($postTypeSingularLabel)
+                    ),
                     'icon_class'   => 'views',
                     'total'        => Helper::formatNumberWithUnit($data['views']['total']),
                     'avg'          => Helper::formatNumberWithUnit($data['views']['avg']),
-                    'avg_title'    => sprintf(esc_html__('Avg. Per %s', 'wp-statistics'), $postTypeLabel)
+                    'avg_title'    => sprintf(esc_html__('Avg. Per %s', 'wp-statistics'), $postTypeSingularLabel)
                 ],
                 [
                     'title'        => esc_html__('Words', 'wp-statistics'),
-                    'tooltip'      => esc_html__('Total number of words written by all authors. Average words per post is calculated by dividing total words by the number of posts.', 'wp-statistics'),
+                    'tooltip'      => sprintf(
+                        esc_html__('Total number of words written by all authors. Average words per %1$s is calculated by dividing total words by the number of %2$s.', 'wp-statistics'), 
+                        strtolower($postTypeSingularLabel), 
+                        strtolower($postTypePluralLabel)
+                    ),
                     'icon_class'   => 'words',
                     'total'        => Helper::formatNumberWithUnit($data['posts']['words']['total']),
                     'avg'          => Helper::formatNumberWithUnit($data['posts']['words']['avg']),
-                    'avg_title'    => sprintf(esc_html__('Avg. Per %s', 'wp-statistics'), $postTypeLabel)
+                    'avg_title'    => sprintf(esc_html__('Avg. Per %s', 'wp-statistics'), $postTypeSingularLabel)
                 ]
             ];
 
             if (post_type_supports($postType, 'comments')) {
                 $items[] = [
                     'title'        => esc_html__('Comments', 'wp-statistics'),
-                    'tooltip'      => esc_html__('Total number of comments received on posts by all authors. Average comments per post is calculated by dividing total comments by the number of posts.', 'wp-statistics'),
+                    'tooltip'      => sprintf(
+                        esc_html__('Total number of comments received on %1$s by all authors. Average comments per %2$s is calculated by dividing total comments by the number of %1$s.', 'wp-statistics'), 
+                        strtolower($postTypePluralLabel), 
+                        strtolower($postTypeSingularLabel)
+                    ),
                     'icon_class'   => 'comments',
                     'total'        => Helper::formatNumberWithUnit($data['posts']['comments']['total']),
                     'avg'          => Helper::formatNumberWithUnit($data['posts']['comments']['avg']),
-                    'avg_title'    => sprintf(esc_html__('Avg. Per %s', 'wp-statistics'), $postTypeLabel)
+                    'avg_title'    => sprintf(esc_html__('Avg. Per %s', 'wp-statistics'), $postTypeSingularLabel)
                 ];
             }
 
@@ -59,20 +75,29 @@ $postTypeLabel  = Helper::getPostTypeName($postType, true);
         <?php
             Admin_Template::get_template(['layout/author-analytics/publishing-overview'], [
                 'title'         => esc_html__('Publishing Overview', 'wp-statistics'),
-                'tooltip'       => esc_html__('This heatmap displays the publishing activity of authors over the past 12 months. Darker squares represent more published posts.', 'wp-statistics'),
+                'tooltip'       => sprintf(
+                    esc_html__('This heatmap displays the publishing activity of authors over the past 12 months. Darker squares represent more published %s.', 'wp-statistics'), 
+                    strtolower($postTypePluralLabel)
+                ),
                 'description'   => esc_html__('Last 12 Months', 'wp-statistics'),
                 'data'          => $data
             ]);
             
             Admin_Template::get_template(['layout/author-analytics/top-authors'], [
                 'title'    => esc_html__('Top Authors', 'wp-statistics'),
-                'tooltip'  => esc_html__('This section ranks authors based on various performance metrics such as views, publishing frequency, comments per post, and average words per post. Use the tabs to switch between different metrics to see how each author is performing.', 'wp-statistics'),
+                'tooltip'  => sprintf(
+                    esc_html__('This section ranks authors based on various performance metrics such as views, publishing frequency, comments per %1$s, and average words per %1$s. Use the tabs to switch between different metrics to see how each author is performing.', 'wp-statistics'), 
+                    strtolower($postTypeSingularLabel)
+                ),
                 'data'     => $data
             ]);
             
             Admin_Template::get_template(['layout/author-analytics/published-posts'], [
-                'title'     => sprintf(esc_html__('Views/Published %s', 'wp-statistics'), Helper::getPostTypeName($postType)),
-                'tooltip'   => esc_html__('This scatter plot shows the relationship between the number of posts published by an author and the number of views those posts have received. Each point represents an author.', 'wp-statistics'),
+                'title'     => sprintf(esc_html__('Views/Published %s', 'wp-statistics'), $postTypePluralLabel),
+                'tooltip'   => sprintf(
+                    esc_html__('This scatter plot shows the relationship between the number of %1$s published by an author and the number of views those %1$s have received. Each point represents an author.', 'wp-statistics'), 
+                    strtolower($postTypePluralLabel)
+                ),
                 'data'      => $data
             ]);
         ?>
