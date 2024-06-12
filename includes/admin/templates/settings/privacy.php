6a6533555b252e5ae1b290ef2b5e6287a98246f2
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-06-12 14:41:34 +0330

    Add recommended category paragraph

--- includes/admin/templates/settings/privacy.php
+++ includes/admin/templates/settings/privacy.php
@@ -2,7 +2,11 @@
     <table class="form-table">
         <tbody>
         <tr valign="top">
-            <th scope="row" colspan="2"><h3><?php esc_html_e('Data Protection', 'wp-statistics'); ?> <a href="#" class="wps-tooltip" title="<?php esc_html_e('Ensure your website adheres to data protection standards.', 'wp-statistics') ?>"><i class="wps-tooltip-icon"></i></a></h3></th>
+            <th scope="row" colspan="2"><h3><?php
+
+use WP_Statistics\Service\Admin\PrivacyAudit\Faqs\RequireConsent;
+
+ esc_html_e('Data Protection', 'wp-statistics'); ?> <a href="#" class="wps-tooltip" title="<?php esc_html_e('Ensure your website adheres to data protection standards.', 'wp-statistics') ?>"><i class="wps-tooltip-icon"></i></a></h3></th>
         </tr>
 
         <tr valign="top">
@@ -96,6 +100,21 @@
                 </select>
                 <p class="description"><?php esc_html_e("Enable WP Consent Level API integration to ensure WP Statistics complies with user-selected privacy preferences. When enabled, WP Statistics will only trigger tracking based on the user's chosen consent category.", 'wp-statistics'); ?></p>
                 <p class="description"><?php _e('<b>Note</b>: This integration requires a compatible consent management provider.', 'wp-statistics'); ?></p>
+                <p class="description">
+                    <?php echo sprintf(
+						// translators: %s: Consent option.
+						__('Recommended Category: <b>%s</b>', 'wp-statistics'),
+						RequireConsent::getStatus() === 'success' ? esc_html__('Statistics-Anonymous', 'wp-statistics') : esc_html__('Statistics', 'wp-statistics')
+					); ?>
+                    <br />
+                    <?php echo RequireConsent::getStatus() === 'success' ? 
+                        esc_html__('WP Statistics, based on your settings, does not use cookies or other personally identifiable information (PII). Therefore, you could use it without notifying users. However, informing users about this can improve your transparency and demonstrate respect for their privacy.', 'wp-statistics') :
+                        sprintf(
+                            // translators: %s: Privacy Audit page link.
+                            __('WP Statistics tracks personally identifiable information (PII) such as IP addresses based on your current settings. We recommend selecting the "Statistics" category. For more detailed information, refer to the <a href="%s" target="_blank">Privacy Audit</a>.', 'wp-statistics'),
+                            esc_url(admin_url('admin.php?page=wps_privacy-audit_page'))
+                        ); ?>
+                </p>
             </td>
         </tr>
 
