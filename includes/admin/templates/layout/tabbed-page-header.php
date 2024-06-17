05798c56724915fc70b38386bab56c6e1f01c713
Foad <foad.adeli@gmail.com>
2024-06-17 14:48:28 +0330

    Add todo

--- includes/admin/templates/layout/tabbed-page-header.php
+++ includes/admin/templates/layout/tabbed-page-header.php
@@ -31,7 +31,9 @@
                 <li class="wps-tab-link <?php echo esc_attr($tab['class']); ?>">
                     <?php if (isset($tab['coming_soon'])): ?>
                         <span class="wps-tooltip wps-tooltip--coming_soon" title="<?php echo esc_html__('Coming soon', 'wp-statistics') ?>"><?php echo esc_html($tab['title']); ?> <i class="wps-tooltip-icon coming-soon"></i></span>
-                     <?php else: ?>
+                    <?php elseif (isset($tab['locked'])) : ?>
+                        <!-- TODO: SHOW LOCKED ICON FOR TABS -->
+                    <?php else: ?>
                         <a href="<?php echo esc_attr($tab['link']); ?>">
                             <?php echo esc_html($tab['title']); ?>
                             <?php if (!empty($tab['tooltip'])) : ?>
