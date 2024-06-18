47a90d4c2213cfcb310eff73658c970ce5d7525f
Foad <foad.adeli@gmail.com>
2024-06-18 10:46:47 +0330

    Add single content analytics view

--- src/Service/Admin/ContentAnalytics/Views/SingleView.php
+++ src/Service/Admin/ContentAnalytics/Views/SingleView.php
@@ -2,15 +2,27 @@
 
 namespace WP_Statistics\Service\Admin\ContentAnalytics\Views;
 
-use WP_Statistics\Abstracts\BaseView;
+use WP_STATISTICS\Helper;
+use WP_Statistics\Utils\Request;
 use WP_STATISTICS\Admin_Template;
+use WP_Statistics\Abstracts\BaseView;
 
 class SingleView extends BaseView 
 {
+    public function isLocked()
+    {
+        return !Helper::isAddOnActive('data-plus');
+    }
+
     public function render()
     {
-        $args = [];
+        $args       = [];
+        $template   = 'single';
 
-        Admin_Template::get_template(['layout/header', 'layout/title', 'pages/content-analytics/single-locked', 'layout/footer'], $args);
+        if ($this->isLocked()) {
+            $template = 'single-locked';
+        }
+
+        Admin_Template::get_template(['layout/header', 'layout/title', "pages/content-analytics/$template", 'layout/footer'], $args);
     }
 }
