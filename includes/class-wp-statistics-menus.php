2ac024159de04ef2dfe87e0e052b719a24b1edb5
Foad <foad.adeli@gmail.com>
2024-06-18 12:21:31 +0330

    Remove legacy taxonomies

--- includes/class-wp-statistics-menus.php
+++ includes/class-wp-statistics-menus.php
@@ -26,7 +26,6 @@ class Menus
         'optimization'      => 'optimization',
         'settings'          => 'settings',
         'plugins'           => 'plugins',
-        'taxonomies'        => 'taxonomies',
         'author-analytics'  => 'author-analytics',
         'privacy-audit'     => 'privacy-audit',
         'geographic'        => 'geographic',
@@ -208,13 +207,6 @@ class Menus
                 'title'    => __('Post Types', 'wp-statistics'),
                 'page_url' => 'pages',
                 'method'   => 'pages',
-            ),
-            'taxonomies'   => array(
-                'require'  => array('visits' => true),
-                'sub'      => 'overview',
-                'title'    => __('Taxonomies', 'wp-statistics'),
-                'page_url' => 'taxonomies',
-                'method'   => 'taxonomies',
             ),
             'top.visitors' => array(
                 'require'  => array('visitors' => true),
