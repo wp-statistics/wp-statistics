e455c82e603d7a971f37c5cc81df369b6942cb5b
Nora <mwdrad@gmail.com>
2024-06-18 17:49:26 +0330

    Minor improvement

--- includes/admin/templates/layout/content-analytics/post-type-header.php
+++ includes/admin/templates/layout/content-analytics/post-type-header.php
@@ -5,7 +5,11 @@
     <div>
         <div class="wps-content-analytics-header__title">
             <h2 class="wps_title">The Role of Artificial Intelligence in SEO and Analytics</h2>
-            <a href="" target="_blank"></a>
+            <a href="" target="_blank" title="The Role of Artificial Intelligence in SEO and Analytics">
+                <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.66667 2V3.33333H1.33333V10.6667H8.66667V7.33333H10V11.3333C10 11.7015 9.70153 12 9.33333 12H0.666667C0.29848 12 0 11.7015 0 11.3333V2.66667C0 2.29848 0.29848 2 0.666667 2H4.66667ZM12 0V5.33333H10.6667L10.6666 2.27533L5.4714 7.4714L4.52859 6.5286L9.72327 1.33333H6.66667V0H12Z" fill="#404BF2"/>
+                </svg>
+            </a>
         </div>
         <div class="wps-content-analytics-header__info">
             <span class="wps-content-analytics-header__type">Post</span>
