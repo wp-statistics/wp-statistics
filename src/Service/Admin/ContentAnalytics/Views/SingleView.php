556dff68cd84e3223b309716d33d32f17d2128e4
Foad <foad.adeli@gmail.com>
2024-06-18 11:06:39 +0330

    Minor improvements

--- src/Service/Admin/ContentAnalytics/Views/SingleView.php
+++ src/Service/Admin/ContentAnalytics/Views/SingleView.php
@@ -6,12 +6,27 @@ use WP_STATISTICS\Helper;
 use WP_Statistics\Utils\Request;
 use WP_STATISTICS\Admin_Template;
 use WP_Statistics\Abstracts\BaseView;
+use WP_Statistics\Exception\SystemErrorException;
 
 class SingleView extends BaseView 
 {
+    private $postId;
+
+    public function __construct()
+    {
+        $this->postId = Request::get('post_id', false, 'number');
+
+        // If post does not exist, show error
+        if (!$this->postId || !get_post($this->postId)) {
+            throw new SystemErrorException(
+                esc_html__('Invalid post id provided.', 'wp-statistics')
+            );
+        }
+    }
+
     public function isLocked()
     {
-        return !Helper::isAddOnActive('data-plus');
+        return !Helper::isAddOnActive('data-plus') && in_array(get_post_type($this->postId), Helper::getCustomPostTypes());
     }
 
     public function render()
