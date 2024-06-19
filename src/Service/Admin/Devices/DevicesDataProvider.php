748f93bcc5f46b8cdcc0663425510ba3ab082df1
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-06-19 14:41:42 +0330

    Remove extra query in single view pages (#501)

--- src/Service/Admin/Devices/DevicesDataProvider.php
+++ src/Service/Admin/Devices/DevicesDataProvider.php
@@ -91,7 +91,6 @@ class DevicesDataProvider
         return [
             'visitors' => $this->visitorsModel->getVisitorsDevicesVersions($args),
             'total'    => $this->visitorsModel->countColumnDistinct($args),
-            'views'    => $this->visitorsModel->countTotalViews($args),
         ];
     }
 
@@ -113,7 +112,6 @@ class DevicesDataProvider
         return [
             'visitors' => $this->visitorsModel->getVisitorsDevicesVersions($args),
             'total'    => $this->visitorsModel->countColumnDistinct($args),
-            'views'    => $this->visitorsModel->countTotalViews($args),
         ];
     }
 
@@ -135,7 +133,6 @@ class DevicesDataProvider
         return [
             'visitors' => $this->visitorsModel->getVisitorsDevicesVersions($args),
             'total'    => $this->visitorsModel->countColumnDistinct($args),
-            'views'    => $this->visitorsModel->countTotalViews($args),
         ];
     }
 }
