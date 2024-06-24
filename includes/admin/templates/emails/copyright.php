bd5ac734fe57f141c83cf84e5077b15fa1e25086
Nora <mwdrad@gmail.com>
2024-06-24 12:04:04 +0330

    Update copy in the footer of the free reports email

--- includes/admin/templates/emails/copyright.php
+++ includes/admin/templates/emails/copyright.php
@@ -1,6 +1,12 @@
 <div class="footer" style="box-sizing: border-box; font-family: 'Roboto',Arial,Helvetica,sans-serif; margin: 0; margin-top: 48px; padding: 0; text-align: center; text-decoration: none;">
     <span style="box-sizing: border-box; color: #56585a; font-family: 'Roboto',Arial,Helvetica,sans-serif; font-size: 14px; font-style: italic; font-weight: 500; line-height: 16.41px; margin: 0; padding: 0; text-decoration: none;">
-        <?php esc_html_e('Powered by:', 'wp-statistics'); ?>
-        <a href="https://wp-statistics.com?utm_source=wp_statistics&utm_medium=display&utm_campaign=email_report" target="_blank" title="<?php esc_html_e('WP Statistics', 'wp-statistics'); ?>" style="box-sizing: border-box; color: #175da4; font-family: 'Roboto',Arial,Helvetica,sans-serif; font-size: 14px; font-style: italic; font-weight: 500; line-height: 16.41px; margin: 0; padding: 0; text-decoration: none;"><?php esc_html_e('WP Statistics', 'wp-statistics'); ?></a>
+        <?php esc_html_e('This email was auto-generated and sent from', 'wp-statistics'); ?>
+        <a href="https://veronalabs.com?utm_source=wp_statistics&utm_medium=display&utm_campaign=email_report" target="_blank" title="<?php esc_html_e('VeronaLabs', 'wp-statistics'); ?>" style="box-sizing: border-box; color: #175da4; font-family: 'Roboto',Arial,Helvetica,sans-serif; font-size: 14px; font-style: italic; font-weight: 500; line-height: 16.41px; margin: 0; padding: 0">
+            <?php esc_html_e('VeronaLabs', 'wp-statistics'); ?>
+        </a>.
+         <?php esc_html_e('Learn', 'wp-statistics'); ?>
+        <a href="https://wp-statistics.com/resources/how-to-manage-automated-report-delivery/?utm_source=wp-statistics&utm_medium=email&utm_campaign=disable-report" target="_blank" title="<?php esc_html_e('how to disable it', 'wp-statistics'); ?>" style="box-sizing: border-box; color: #175da4; font-family: 'Roboto',Arial,Helvetica,sans-serif; font-size: 14px; font-style: italic; font-weight: 500; line-height: 16.41px; margin: 0; padding: 0">
+            <?php esc_html_e('how to disable it', 'wp-statistics'); ?>
+        </a>.
     </span>
 </div>
