5b0b009604049e6a43ea9056345a129bb30acd31
Foad <foad.adeli@gmail.com>
2024-06-27 18:35:51 +0330

    Minor improvements

--- src/Service/Admin/ContentAnalytics/ContentAnalyticsDataProvider.php
+++ src/Service/Admin/ContentAnalytics/ContentAnalyticsDataProvider.php
@@ -36,7 +36,7 @@ class ContentAnalyticsDataProvider
             $date       = date('Y-m-d', strtotime("-$i days"));
             $dateFilter = ['date' => ['from' => $date, 'to' => $date]];
 
-            $result['labels'][]     = date_i18n('j M', strtotime($date));
+            $result['labels'][]     = date_i18n(get_option('date_format'), strtotime($date));
             $result['visitors'][]   = $this->visitorsModel->countVisitors(array_merge($this->args, $dateFilter));
             $result['views'][]      = $this->viewsModel->countViews(array_merge($this->args, $dateFilter));
             $result['posts'][]      = $this->postsModel->countPosts(array_merge($this->args, $dateFilter));
