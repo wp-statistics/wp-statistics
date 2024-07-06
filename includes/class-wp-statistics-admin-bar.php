b565bcfbc04bd9ccbdbc54a45efbd31f973ede8f
Nora <mwdrad@gmail.com>
2024-07-06 08:51:32 +0330

    Minor improvement

--- includes/class-wp-statistics-admin-bar.php
+++ includes/class-wp-statistics-admin-bar.php
@@ -129,7 +129,7 @@ class AdminBar
                     ],
                 ),
                 'wp-statistics-footer-page'         => array(
-                    'parent' => 'wp-statistic-menu-global-data',
+                    'parent' => 'wp-statistic-menu',
                     'title'  => sprintf('<img src="%s"/>
                         <a href="' . esc_url(admin_url('admin.php?page=wps_overview_page')) . '">
                         <span class="wps-admin-bar__chart__unlock-button">%s</span>
