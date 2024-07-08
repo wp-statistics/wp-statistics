9c7cfd6992d8ba6e3e0667ae7af542d6d5e2a550
Nora <mwdrad@gmail.com>
2024-07-08 14:10:57 +0330

    Minor improvement

--- includes/admin/templates/pages/category-analytics/category-pages-locked.php
+++ includes/admin/templates/pages/category-analytics/category-pages-locked.php
@@ -4,6 +4,6 @@ use WP_STATISTICS\Admin_Template;
 
 $catSingle = [
     'campaign' => 'category-analytics',
-    'src'  => 'assets/images/locked/category.jpg',
+    'src'  => 'assets/images/locked/category-pages.jpg',
 ];
 Admin_Template::get_template(['layout/partials/data-plus-locked-page'], $catSingle);
