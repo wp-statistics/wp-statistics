2c8eb5cca028881c4f54fa4f32aff085a065681a
Foad <foad.adeli@gmail.com>
2024-07-09 14:23:14 +0330

    Minor fixes

--- includes/class-wp-statistics-timezone.php
+++ includes/class-wp-statistics-timezone.php
@@ -136,7 +136,7 @@ class TimeZone
             return false;
         }
 
-        if (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $date) and strtotime($date) != false) {
+        if (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $date) && strtotime($date) !== false) {
             return true;
         }
         return false;
