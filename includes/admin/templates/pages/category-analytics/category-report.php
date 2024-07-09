f57b4d41736966cf23534f21d2ae7f9f9c8dcf77
Nora <mwdrad@gmail.com>
2024-07-09 09:28:47 +0330

    Add category report page

--- includes/admin/templates/pages/category-analytics/category-report.php
+++ includes/admin/templates/pages/category-analytics/category-report.php
@@ -1,1 +1,77 @@
-<?php echo 'CATEGORY REPORT' ?>
+<div class="postbox-container wps-postbox-full">
+    <div class="metabox-holder">
+        <div class="meta-box-sortables">
+            <div class="postbox">
+                <div class="inside">
+                    <div class="o-table-wrapper">
+                        <table width="100%" class="o-table wps-new-table">
+                            <thead>
+                            <tr>
+                                <th class="wps-pd-l">
+                                    <a href="" class="sort">
+                                        <?php esc_html_e('Term', 'wp-statistics'); ?>
+                                    </a>
+                                </th>
+                                <th class="wps-pd-l">
+                                    <a href="" class="sort desc">
+                                        <?php esc_html_e('Views', 'wp-statistics'); ?>
+                                    </a>
+                                </th>
+                                <th class="wps-pd-l">
+                                    <a href="" class="sort">
+                                        <?php esc_html_e('Published ', 'wp-statistics'); ?>
+                                    </a>
+                                </th>
+                                <th class="wps-pd-l">
+                                    <a href="" class="sort">
+                                        <?php esc_html_e('Words ', 'wp-statistics'); ?>
+                                    </a>
+                                </th>
+                                <th class="wps-pd-l">
+                                    <a href="" class="sort">
+                                        <?php esc_html_e('Views/Content ', 'wp-statistics'); ?>
+                                    </a>
+                                </th>
+                                <th class="wps-pd-l">
+                                    <a href="" class="sort">
+                                        <?php esc_html_e('Words/Content ', 'wp-statistics'); ?>
+                                    </a>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td class="wps-pd-l">
+                                    <span class="wps-ellipsis-parent" title="Data Analytics">
+                                        <span class="wps-ellipsis-text">
+                                             Data Analytics
+                                        </span>
+                                    </span>
+                                </td>
+                                <td class="wps-pd-l">
+                                    873
+                                </td>
+                                <td class="wps-pd-l">
+                                    126
+                                </td>
+                                <td class="wps-pd-l">
+                                    123,556
+                                </td>
+                                <td class="wps-pd-l">
+                                    12,230
+                                </td>
+                                <td class="wps-pd-l">
+                                    1,230
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="o-wrap o-wrap--no-data wps-center">
+                        <?php esc_html_e('No recent data available.', 'wp-statistics'); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
