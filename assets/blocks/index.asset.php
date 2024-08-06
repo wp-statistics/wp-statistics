f030d4c86ab3cf24827243983762237a33c0608c
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-08-07 01:28:31 +0330

    Add icon to panel

--- assets/blocks/index.asset.php
+++ assets/blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => '58d9bd6ee6c573a1f48c');
+<?php return array('dependencies' => array('react', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => 'ba0fa4efdb77c7d29251');
