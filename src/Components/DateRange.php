78173c7d131d4b6a286eb16fa474c55ca359d038
Foad <foad.adeli@gmail.com>
2024-08-13 11:13:19 +0330

    Add periods to the DateRange class

--- src/Components/DateRange.php
+++ src/Components/DateRange.php
@@ -2,6 +2,7 @@
 
 namespace WP_Statistics\Components;
 
+use InvalidArgumentException;
 use WP_STATISTICS\TimeZone;
 use WP_STATISTICS\User;
 
@@ -11,11 +12,7 @@ class DateRange
 
     public static function getDefault()
     {
-        $date = [
-            'from'  => date('Y-m-d', strtotime('-29 days')),
-            'to'    => date('Y-m-d')
-        ];
-        return apply_filters('wp_statistics_default_date_range', $date);
+        return apply_filters('wp_statistics_default_date_range', self::get('30days'));
     }
 
     /**
@@ -47,18 +44,169 @@ class DateRange
     }
 
     /**
-     * @todo Add complete list of needed string dates such as today, yesterday, month, last-month, etc 
+     * Gets a string and returns the specified date range.
+     *
+     * @param string $name The name of the date range.
+     * @param bool $prevPeriod Whether to retrieve the previous period. Defaults to false.
+     * @param bool $excludeToday Whether to exclude today from the date range. Defaults to false.
+     * @throws InvalidArgumentException If the specified date range is invalid.
+     * @return array The date range.
      */
-    public function get($item = false)
+    public static function get($name, $prevPeriod = false, $excludeToday = false)
     {
-        $items = [
-            
-        ];
+        $periods = self::getPeriods();
 
-        if (!isset($items[$item])) {
-            throw new \ErrorException(esc_html__('Invalid date range.'));
+        if (!isset($periods[$name])) {
+            throw new InvalidArgumentException(esc_html__('Invalid date range.', 'wp-statistics'));
         }
 
-        return !empty($item) ? $items[$item] : $items;
+        $range = $periods[$name]['period'];
+
+        if ($prevPeriod) {
+            $range = $periods[$name]['prev_period'];
+        }
+
+        if ($excludeToday) {
+            if ($range['to'] === date('Y-m-d')) {
+                $range['to'] = date('Y-m-d', '-1 day');
+            }
+        }
+
+        return $range;
+    }
+
+    /**
+     * Returns an array of predefined date periods.
+     *
+     * Each date period is represented as an array with two keys: 'period' and 'prev_period'.
+     * The 'period' key represents the actual date period of the given string
+     * The 'prev_period' key represents the date range before to the current date period.
+     *
+     * @return array An array of predefined date periods.
+     */
+    public static function getPeriods()
+    {
+        return [
+            'today'     => [
+                'period'    => [
+                    'from'  => date('Y-m-d'),
+                    'to'    => date('Y-m-d')
+                ],
+                'prev_period' => [
+                    'from'  => date('Y-m-d', strtotime('-1 day')),
+                    'to'    => date('Y-m-d', strtotime('-1 day'))
+                ],
+            ],
+
+            'yesterday' => [
+                'period' => [
+                    'from'  => date('Y-m-d', strtotime('-1 day')),
+                    'to'    => date('Y-m-d', strtotime('-1 day')),
+                ],
+                'prev_period' => [
+                    'from'  => date('Y-m-d', strtotime('-2 day')),
+                    'to'    => date('Y-m-d', strtotime('-2 day')),
+                ],
+            ],
+
+            'this_month' => [
+                'period' => [
+                    'from'  => date('Y-m-d', strtotime('first day of this month')),
+                    'to'    => date('Y-m-d', strtotime('last day of this month')),
+                ],
+                'prev_period' => [
+                    'from'  => date('Y-m-d', strtotime('first day of last month')),
+                    'to'    => date('Y-m-d', strtotime('last day of last month')),
+                ]
+            ],
+
+            'last_month' => [
+                'period' => [
+                    'from'  => date('Y-m-d', strtotime('first day of -1 month')),
+                    'to'    => date('Y-m-d', strtotime('last day of -1 month')),
+                ],
+                'prev_period' => [
+                    'from'  => date('Y-m-d', strtotime('first day of -2 months')),
+                    'to'    => date('Y-m-d', strtotime('last day of -2 months')),
+                ]
+            ],
+
+            '7days'     => [
+                'period' => [
+                    'from'  => date('Y-m-d', strtotime('-6 days')),
+                    'to'    => date('Y-m-d')
+                ],
+                'prev_period' => [
+                    'from'  => date('Y-m-d', strtotime('-13 days')),
+                    'to'    => date('Y-m-d', strtotime('-7 days'))
+                ]
+            ],
+
+            '14days'    => [
+                'period' => [
+                    'from'  => date('Y-m-d', strtotime('-13 days')),
+                    'to'    => date('Y-m-d')
+                ],
+                'prev_period' => [
+                    'from'  => date('Y-m-d', strtotime('-27 days')),
+                    'to'    => date('Y-m-d', strtotime('-14 days'))
+                ]
+            ],
+
+            '30days'    => [
+                'period' => [
+                    'from'  => date('Y-m-d', strtotime('-29 days')),
+                    'to'    => date('Y-m-d')
+                ],
+                'prev_period' => [
+                    'from'  => date('Y-m-d', strtotime('-59 days')),
+                    'to'    => date('Y-m-d', strtotime('-30 days'))
+                ]
+            ],
+
+            '90days'    => [
+                'period' => [
+                    'from'  => date('Y-m-d', strtotime('-89 days')),
+                    'to'    => date('Y-m-d')
+                ],
+                'prev_period' => [
+                    'from'  => date('Y-m-d', strtotime('-179 days')),
+                    'to'    => date('Y-m-d', strtotime('-90 days'))
+                ]
+            ],
+
+            '12months'  => [
+                'period' => [
+                    'from'  => date('Y-m-d', strtotime('-12 months')),
+                    'to'    => date('Y-m-d'),
+                ],
+                'prev_period' => [
+                    'from'  => date('Y-m-d', strtotime('-24 months')),
+                    'to'    => date('Y-m-d', strtotime('-12 months')),
+                ]
+            ],
+
+            'this_year' => [
+                'period' => [
+                    'from'  => date('Y-01-01'),
+                    'to'    => date('Y-12-31'),
+                ],
+                'prev_period' => [
+                    'from'  => date('Y-01-01', strtotime('-1 year')),
+                    'to'    => date('Y-12-31', strtotime('-1 year')),
+                ]
+            ],
+
+            'last_year' => [
+                'period' => [
+                    'from'  => date('Y-01-01', strtotime('-1 year')),
+                    'to'    => date('Y-12-31', strtotime('-1 year')),
+                ],
+                'prev_period' => [
+                    'from'  => date('Y-01-01', strtotime('-2 years')),
+                    'to'    => date('Y-12-31', strtotime('-2 years')),
+                ]
+            ]
+        ];
     }
 }
