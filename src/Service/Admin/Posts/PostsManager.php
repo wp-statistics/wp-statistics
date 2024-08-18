3f01e20ee12eaa8a75bcedb381ca9903c6d99178
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-08-18 22:57:47 +0330

    Merge PostSummaryManager class with PostsManager

--- src/Service/Admin/Posts/PostsManager.php
+++ src/Service/Admin/Posts/PostsManager.php
@@ -2,6 +2,8 @@
 
 namespace WP_Statistics\Service\Admin\Posts;
 
+use WP_Statistics\Components\Assets;
+
 class PostsManager
 {
     /**
@@ -11,11 +13,12 @@ class PostsManager
 
     public function __construct()
     {
-        new PostSummaryManager();
         $this->wordsCount = new WordCountService();
 
         add_action('save_post', [$this, 'addWordsCountCallback'], 99, 3);
         add_action('delete_post', [$this, 'removeWordsCountCallback'], 99, 2);
+
+        add_action('enqueue_block_editor_assets', [$this, 'enqueueSidebarPanelAssets']);
     }
 
     /**
@@ -39,5 +42,100 @@ class PostsManager
     public function removeWordsCountCallback($postId, $post)
     {
         $this->wordsCount->removeWordsCountMeta($postId, $post);
+    }
+
+    /**
+     * Enqueues assets for "Statistics - Summary" panel in the Gutenberg editor sidebar.
+     *
+     * @return	void
+     *
+     * @hooked	action: `enqueue_block_editor_assets` - 10
+     */
+    public function enqueueSidebarPanelAssets()
+    {
+        global $pagenow;
+        if ($pagenow === 'post-new.php') {
+            return;
+        }
+
+        global $post;
+        if (empty($post)) {
+            return;
+        }
+
+        $postSummary = $this->getPostStatisticsSummary($post);
+        if (empty($postSummary)) {
+            return;
+        }
+
+        Assets::script('editor-sidebar', 'blocks/index.js', ['wp-plugins', 'wp-editor'], $postSummary);
+
+        $styleFileName = is_rtl() ? 'style-index-rtl.css' : 'style-index.css';
+        Assets::style('editor-sidebar', "blocks/$styleFileName");
+    }
+
+    /**
+     * Returns the data needed for "Statistics - Summary" widget/panel in edit posts.
+     *
+     * @param   \WP_Post    $post
+     *
+     * @return  array|null          Keys: 
+     *  - `postId`
+     *  - `fromString`
+     *  - `toString`
+     *  - `totalVisitors`
+     *  - `totalViews`
+     *  - `topReferrer`
+     *  - `topReferrerCount`
+     *  - `thisPeriodVisitors`
+     *  - `thisPeriodViews`
+     *  - `thisPeriodTopReferrer`
+     *  - `thisPeriodTopReferrerCount`
+     *  - `postChartData`
+     *  - `contentAnalyticsUrl`
+     */
+    public function getPostStatisticsSummary($post)
+    {
+        $dataProvider = null;
+        try {
+            $dataProvider = new PostSummaryDataProvider($post);
+        } catch (\Exception $e) {
+            return null;
+        }
+
+        $topReferrerAndCountTotal      = $dataProvider->getTopReferrerAndCount(true);
+        $topReferrerAndCountThisPeriod = $dataProvider->getTopReferrerAndCount();
+
+        // Data for the sidebar chart
+        $chartData    = [];
+        $dailyViews   = $dataProvider->getDailyViews();
+        $wpDateFormat = get_option('date_format');
+        foreach ($dailyViews as $dailyView) {
+            if (empty($dailyView->date) || empty($dailyView->views)) {
+                continue;
+            }
+
+            $chartData[] = [
+                'views'     => intval($dailyView->views),
+                'shortDate' => date('d M', strtotime($dailyView->date)),
+                'fullDate'  => date($wpDateFormat, strtotime($dailyView->date)),
+            ];
+        }
+
+        return [
+            'postId'                     => $post->ID,
+            'fromString'                 => $dataProvider->getFromString(),
+            'toString'                   => $dataProvider->getToString(),
+            'totalVisitors'              => $dataProvider->getVisitors(true),
+            'totalViews'                 => $dataProvider->getViews(true),
+            'topReferrer'                => $topReferrerAndCountTotal['url'],
+            'topReferrerCount'           => $topReferrerAndCountTotal['count'],
+            'thisPeriodVisitors'         => $dataProvider->getVisitors(),
+            'thisPeriodViews'            => $dataProvider->getViews(),
+            'thisPeriodTopReferrer'      => $topReferrerAndCountThisPeriod['url'],
+            'thisPeriodTopReferrerCount' => $topReferrerAndCountThisPeriod['count'],
+            'postChartData'              => $chartData,
+            'contentAnalyticsUrl'        => $dataProvider->getContentAnalyticsUrl(),
+        ];
     }
 }
