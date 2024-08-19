a7e13d46e2b29bcd950a1d47a914ee3442d3ceaa
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-08-19 14:20:21 +0330

    Remove chart from 'Daily Traffic Trend' meta-box (#529)

--- assets/dev/javascript/meta-box/post.js
+++ assets/dev/javascript/meta-box/post.js
@@ -1,30 +1,17 @@
 wps_js.post_meta_box = {
-
     params: function () {
-        return {'ID': wps_js.global['page']['ID']};
+        return { 'ID': wps_js.global['page']['ID'] };
     },
 
     view: function (args = []) {
-        return args.hasOwnProperty('content') ? ' <div class="wps-center" style="padding: 15px;"> ' + args['content'] + '</div>' : '<canvas id="' + wps_js.chart_id('post') + '" height="85"></canvas>' + '<div class="wps-wrap wps-meta-box-footer">' + args['visitors'] + '</div>';
+        return args.hasOwnProperty('content') ?
+            ' <div class="wps-center" style="padding: 15px;"> ' + args['content'] + '</div>' :
+            '<div class="wps-wrap wps-meta-box-footer">' + args['visitors'] + '</div>';
     },
 
     meta_box_init: function (args = []) {
-        if (!args.hasOwnProperty('content')) {
-            this.post_hits_chart(wps_js.chart_id('post'), args);
-        } else {
+        if (args.hasOwnProperty('content')) {
             jQuery("#" + wps_js.getMetaBoxKey('post') + " button[onclick]").remove();
         }
     },
-
-    post_hits_chart: function (tag_id, args = []) {
-        wps_js.line_chart(tag_id, args['title'], args['date'], [{
-            label: args['post_title'],
-            data: args['state'],
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1,
-            fill: true,
-            tension: 0.4
-        }]);
-    }
 };
