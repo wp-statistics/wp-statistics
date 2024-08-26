1e88d921943b3634375fcf745f1abce4dcdbd6fd
Foad <foad.adeli@gmail.com>
2024-08-26 14:20:34 +0330

    fix: pass proper search engine data from search metabox

--- includes/admin/meta-box/wp-statistics-meta-box-search.php
+++ includes/admin/meta-box/wp-statistics-meta-box-search.php
@@ -2,6 +2,7 @@
 
 namespace WP_STATISTICS\MetaBox;
 
+use WP_Statistics\Models\VisitorsModel;
 use WP_STATISTICS\Option;
 use WP_STATISTICS\SearchEngine;
 use WP_STATISTICS\TimeZone;
@@ -41,56 +42,17 @@ class search extends MetaBoxAbstract
         // Filter By Date
         self::filterByDate($args);
 
-        // Get List Of Days
-        $days_time_list = array_keys(self::$daysList);
-        foreach (self::$daysList as $k => $v) {
-            $date[]          = $v['format'];
-            $total_daily[$k] = 0;
-        }
+        $range = array_keys(self::$daysList);
 
-        // Set Title
-        if (end($days_time_list) == TimeZone::getCurrentDate("Y-m-d")) {
-            $title = sprintf(__('Referrals from Search Engines in the Past %s Days', 'wp-statistics'), self::$countDays);
-        } else {
-            $title = sprintf(__('Search Engine Referrals Between %1$s and %2$s', 'wp-statistics'), $args['from'], $args['to']);
-        }
+        $visitorsModel = new VisitorsModel();
 
-        // Get List Of Search Engine
-        $search_engines = SearchEngine::getList();
+        $data = $visitorsModel->getSearchEnginesChartData([
+            'date' => [
+                'from'  => reset($range),
+                'to'    => end($range)
+            ]
+        ]);
 
-        // Push List to data
-        foreach ($search_engines as $se) {
-
-            // Get Search engine information
-            $search_engine_list[] = $se;
-
-            // Get Number Search every Days
-            foreach ($days_time_list as $d) {
-                $getStatic            = wp_statistics_searchengine($se['tag'], $d);
-                $stats[$se['name']][] = $getStatic;
-                $total_daily[$d]      = $total_daily[$d] + $getStatic;
-            }
-        }
-
-        // Prepare Response
-        $response = array(
-            'title'         => $title,
-            'date'          => $date,
-            'stat'          => $stats,
-            'search-engine' => $search_engine_list,
-            'total'         => array(
-                'active' => 1,
-                'color'  => '180, 180, 180',
-                'stat'   => array_values($total_daily)
-            )
-        );
-
-        // Check For No Data Meta Box
-        if (count(array_filter($total_daily)) < 1 and !isset($args['no-data'])) {
-            $response['no_data'] = 1;
-        }
-
-        // Response
-        return self::response($response);
+        return $data;
     }
 }
