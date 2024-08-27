d9a0c44c84731d30f1dc13644320987152dccbac
Nora <mwdrad@gmail.com>
2024-08-27 19:43:33 +0330

    perf: Add No Data Message

--- assets/dev/javascript/meta-box/pages.js
+++ assets/dev/javascript/meta-box/pages.js
@@ -2,7 +2,8 @@ wps_js.pages_meta_box = {
 
     view: function (args = []) {
         let t = '';
-        t += `<div class="o-table-wrapper"><table width="100%" class="o-table wps-new-table o-table--pages ">
+        if (args.pages && args.pages.length > 0) {
+            t += `<div class="o-table-wrapper"><table width="100%" class="o-table wps-new-table o-table--pages ">
         <thead>
             <tr>
                 <th class="wps-pd-l">${wps_js._('title')}</th>
@@ -11,19 +12,23 @@ wps_js.pages_meta_box = {
             </tr>
         </thead>
         <tbody> `;
-        const siteUrl = wps_js.global.admin_url.replace('/wp-admin/', '');
-        let i = 1;
-        args.pages.forEach(function (value) {
-            t += `<tr>
+            const siteUrl = wps_js.global.admin_url.replace('/wp-admin/', '');
+            let i = 1;
+            args.pages.forEach(function (value) {
+                t += `<tr>
 			<td class="wps-pd-l"><div class="wps-ellipsis-parent" title="${value['title']}"><span class="wps-ellipsis-text">${value['title']}</span></div></td>
 			<td class="wps-pd-l"><a href="${value['hits_page']}">${value['number']}</a></td>
 		    <td class="wps-pd-l"><a target="_blank" class="wps-view-content" href="${value['link']}">${wps_js._('view_content')}</a></td>
 			</tr>`;
-            i++;
-        });
+                i++;
+            });
 
-        t += `</tbody></table></div>`;
-        return t;
+            t += `</tbody></table></div>`;
+            return t;
+        } else {
+            return wps_js.no_meta_box_data()
+        }
+
     }
 
 };
