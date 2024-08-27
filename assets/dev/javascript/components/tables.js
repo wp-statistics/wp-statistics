c16d9f35800c1efa3c49cdd800b9765ba0293e43
Nora <mwdrad@gmail.com>
2024-08-27 12:20:15 +0330

    style : Update table header style, remove table hover

--- assets/dev/javascript/components/tables.js
+++ assets/dev/javascript/components/tables.js
@@ -1,21 +1,3 @@
-function addLinkToTableRow() {
-    const tableInspectRows = document.querySelectorAll('.wps-table-inspect tbody tr');
-    tableInspectRows.forEach(function (row) {
-        row.addEventListener('click', function (event) {
-            if (!event.target.closest('.view-more')) {
-                const link = row.querySelector('td:first-child a');
-                if (link) {
-                    if (link.target === '_blank') {
-                        window.open(link.href, '_blank');
-                    } else {
-                        window.location.href = link.href;
-                    }
-                }
-            }
-        });
-    });
-}
-
 // Check for the presence of the elements
 const unlockColumnExists = document.querySelector('.wps-admin-column__unlock');
 const tbodyElement = document.querySelector('.wp-list-table');
@@ -23,5 +5,3 @@ const tbodyElement = document.querySelector('.wp-list-table');
 if (unlockColumnExists && tbodyElement) {
     tbodyElement.classList.add('wps-admin-mini-chart__unlock');
 }
-
-addLinkToTableRow();
