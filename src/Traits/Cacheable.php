616a92f4cb3b443689b9520bc94c482645e9162a
mostafa <mst404@gmail.com>
2024-09-01 18:21:22 +0300

    feat: Added ObjectCacheTrait

--- src/Traits/Cacheable.php
+++ src/Traits/Cacheable.php
@@ -2,20 +2,46 @@
 
 namespace WP_Statistics\Traits;
 
+/**
+ * Trait to handle caching logic.
+ * @doc https://github.com/wp-statistics/wp-statistics/wiki/Cacheable.md
+ */
 trait Cacheable
 {
+    /**
+     * Get the cache key for the given query.
+     *
+     * @param string $query
+     *
+     * @return string
+     */
     protected function getCacheKey($query)
     {
         $hash = substr(md5($query), 0, 10);
         return sprintf('wp_statistics_cache_%s', $hash);
     }
 
+    /**
+     * Get the cached result for the given query.
+     *
+     * @param string $query
+     *
+     * @return mixed
+     */
     protected function getCachedResult($query)
     {
         $cacheKey = $this->getCacheKey($query);
         return get_transient($cacheKey);
     }
 
+    /**
+     * Set the cached result for the given query.
+     *
+     * @param string $query
+     * @param mixed $result
+     *
+     * @return bool
+     */
     protected function setCachedResult($query, $result)
     {
         $cacheKey = $this->getCacheKey($query);
