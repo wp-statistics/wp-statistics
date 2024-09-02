836590ac3a95b8a6def30c2fc604c3c079d492b3
Foad <foad.adeli@gmail.com>
2024-09-02 10:59:57 +0330

    Add code comments

--- src/Service/Charts/ChartDataProviderFactory.php
+++ src/Service/Charts/ChartDataProviderFactory.php
@@ -8,16 +8,34 @@ use WP_Statistics\Service\Charts\DataProvider\TrafficChartDataProvider;
 
 class ChartDataProviderFactory
 {
+    /**
+     * Returns a new instance of PerformanceChartDataProvider.
+     *
+     * @param array $args The arguments to pass to the PerformanceChartDataProvider constructor.
+     * @return PerformanceChartDataProvider
+     */
     public static function performanceChart($args)
     {
         return new PerformanceChartDataProvider($args);
     }
 
+    /**
+     * Returns a new instance of SearchEngineChartDataProvider.
+     *
+     * @param array $args The arguments to pass to the SearchEngineChartDataProvider constructor.
+     * @return SearchEngineChartDataProvider
+     */
     public static function searchEngineChart($args)
     {
         return new SearchEngineChartDataProvider($args);
     }
 
+    /**
+     * Returns a new instance of TrafficChartDataProvider.
+     *
+     * @param array $args The arguments to pass to the TrafficChartDataProvider constructor.
+     * @return TrafficChartDataProvider
+     */
     public static function trafficChart($args)
     {
         return new TrafficChartDataProvider($args);
