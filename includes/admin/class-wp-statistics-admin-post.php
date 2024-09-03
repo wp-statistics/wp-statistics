0fdb79dc29dc76951275ec1c3a33d697f8b49288
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-09-03 13:47:17 +0330

    Reformat and refactor Admin_Post class

--- includes/admin/class-wp-statistics-admin-post.php
+++ includes/admin/class-wp-statistics-admin-post.php
@@ -11,41 +11,48 @@ class Admin_Post
      */
     public function __construct()
     {
-
-        // Add Hits Column in All Admin Post-Type Wp_List_Table
-        if (User::Access('read') and !Option::get('disable_column')) {
-            add_action('admin_init', array($this, 'init'));
+        // Add Hits column in edit lists of all post types
+        if (User::Access('read') && !Option::get('disable_column')) {
+            add_action('admin_init', [$this, 'init']);
         }
 
-        // Remove Post Hits when Post Id deleted
-        add_action('deleted_post', array($this, 'modify_delete_post'));
+        // Remove post hits on post delete
+        add_action('deleted_post', [$this, 'modifyDeletePost']);
     }
 
     /**
-     * Init Hook
+     * Initializes hits column.
+     *
+     * @return void
+     *
+     * @hooked action: `admin_init` - 10
      */
     public function init()
     {
         $hitColumnHandler = new HitColumnHandler();
 
         foreach (Helper::get_list_post_type() as $type) {
-            add_action('manage_' . $type . '_posts_columns', array($hitColumnHandler, 'addHitColumn'), 10, 2);
-            add_action('manage_' . $type . '_posts_custom_column', array($hitColumnHandler, 'renderHitColumn'), 10, 2);
-            add_filter('manage_edit-' . $type . '_sortable_columns', array($hitColumnHandler, 'modifySortableColumns'));
+            add_action("manage_{$type}_posts_columns", [$hitColumnHandler, 'addHitColumn'], 10, 2);
+            add_action("manage_{$type}_posts_custom_column", [$hitColumnHandler, 'renderHitColumn'], 10, 2);
+            add_filter("manage_edit-{$type}_sortable_columns", [$hitColumnHandler, 'modifySortableColumns']);
         }
-        add_filter('posts_clauses', array($hitColumnHandler, 'handleOrderByHits'), 10, 2);
+
+        add_filter('posts_clauses', [$hitColumnHandler, 'handleOrderByHits'], 10, 2);
     }
 
     /**
-     * Delete All Post Hits When Post is Deleted
+     * Deletes all post hits when the post is deleted.
      *
-     * @param $post_id
+     * @param int $postId
+     *
+     * @return void
      */
-    public static function modify_delete_post($post_id)
+    public static function modifyDeletePost($postId)
     {
         global $wpdb;
+
         $wpdb->query(
-            $wpdb->prepare("DELETE FROM `" . DB::table('pages') . "` WHERE `id` = %d AND (`type` = 'post' OR `type` = 'page' OR `type` = 'product');", esc_sql($post_id))
+            $wpdb->prepare("DELETE FROM `" . DB::table('pages') . "` WHERE `id` = %d AND (`type` = 'post' OR `type` = 'page' OR `type` = 'product');", esc_sql($postId))
         );
     }
 }
