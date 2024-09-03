a1cb11c8ee90a9374efa2e3d9c762c3ba98b8d0b
Foad <foad.adeli@gmail.com>
2024-09-03 16:22:57 +0330

    refactor: use chart response trait in search engine data provider

--- src/Service/Charts/DataProvider/SearchEngineChartDataProvider.php
+++ src/Service/Charts/DataProvider/SearchEngineChartDataProvider.php
@@ -6,10 +6,13 @@ use WP_Statistics\Components\DateRange;
 use WP_STATISTICS\Helper;
 use WP_Statistics\Models\VisitorsModel;
 use WP_Statistics\Service\Charts\AbstractChartDataProvider;
+use WP_Statistics\Service\Charts\Traits\LineChartResponseTrait;
 use WP_STATISTICS\TimeZone;
 
 class SearchEngineChartDataProvider extends AbstractChartDataProvider
 {
+    use LineChartResponseTrait;
+
     public $args;
     protected $visitorsModel;
 
@@ -18,6 +21,8 @@ class SearchEngineChartDataProvider extends AbstractChartDataProvider
         $this->args = $args;
 
         $this->visitorsModel = new VisitorsModel();
+
+        $this->initChartData(true);
     }
 
     public function getData()
@@ -25,60 +30,45 @@ class SearchEngineChartDataProvider extends AbstractChartDataProvider
         $thisPeriod = isset($this->args['date']) ? $this->args['date'] : DateRange::get();
         $prevPeriod = DateRange::getPrevPeriod($thisPeriod);
 
+        $data       = $this->visitorsModel->getSearchEngineReferrals($this->args);
+        $prevData   = $this->visitorsModel->getSearchEngineReferrals(array_merge($this->args, ['date' => $prevPeriod]));
+
+        $result     = $this->prepareResult($data, $prevData);
+
+        return $result;
+    }
+
+    public function prepareResult($data, $prevData)
+    {
+        $parsedData = [
+            'data'          => [],
+            'previousData'  => []
+        ];
+
+        $thisPeriod = isset($this->args['date']) ? $this->args['date'] : DateRange::get();
+        $prevPeriod = DateRange::getPrevPeriod($thisPeriod);
+
         $thisPeriodDates = array_keys(TimeZone::getListDays($thisPeriod));
         $prevPeriodDates = array_keys(TimeZone::getListDays($prevPeriod));
 
-        $result = [
-            'data' => [
-                'labels'   => array_map(
-                    function ($date) {
-                        return [
-                            'date'  => date_i18n(Helper::getDefaultDateFormat(false, true, true), strtotime($date)),
-                            'day'   => date_i18n('l', strtotime($date)),
-                        ];
-                    },
-                    $thisPeriodDates
-                ),
-                'datasets' => []
-            ],
-            'previousData' => [
-                'labels'   => array_map(
-                    function ($date) {
-                        return [
-                            'date'  => date_i18n(Helper::getDefaultDateFormat(false, true, true), strtotime($date)),
-                            'day'   => date_i18n('l', strtotime($date)),
-                        ];
-                    },
-                    $prevPeriodDates
-                ),
-                'datasets' => []
-            ],
-        ];
-
         // This period data
         $thisParsedData     = [];
-        $thisPeriodData     = $this->visitorsModel->getSearchEngineReferrals($this->args);
         $thisPeriodTotal    = array_fill_keys($thisPeriodDates, 0);
 
-        foreach ($thisPeriodData as $item) {
+        // Set chart labels
+        $this->setChartLabels($this->generateChartLabels($thisPeriodDates));
+        $this->setChartPreviousLabels($this->generateChartLabels($prevPeriodDates));
+
+        foreach ($data as $item) {
             $visitors = intval($item->visitors);
             $thisParsedData[$item->engine][$item->date] = $visitors;
             $thisPeriodTotal[$item->date]               += $visitors;
         }
 
-        // Create an array of top search engines
-        $topEngines = array_map(function($item) {
-            return array_sum($item);
-        }, $thisParsedData);
-
-        // Sort top search engines in descending order
-        arsort($topEngines);
-
-        // Get the top 3 items
-        $topEngines = array_slice($topEngines, 0, 3, true);
+        $topSearchEngines = $this->getTopSearchEngines($thisParsedData);
 
         foreach ($thisParsedData as $searchEngine => &$data) {
-            if (!in_array($searchEngine, array_keys($topEngines))) continue;
+            if (!in_array($searchEngine, $topSearchEngines)) continue;
 
             // Fill out missing visitors with 0
             $data = array_merge(array_fill_keys($thisPeriodDates, 0), $data);
@@ -86,39 +76,68 @@ class SearchEngineChartDataProvider extends AbstractChartDataProvider
             // Sort data by date
             ksort($data);
 
-            // Generate dataset
-            $result['data']['datasets'][] = [
-                'label' => ucfirst($searchEngine),
-                'data'  => array_values($data)
-            ];
+            // Add search engine data as dataset
+            $this->addChartDataset(
+                ucfirst($searchEngine),
+                array_values($data)
+            );
         }
 
-        usort($result['data']['datasets'], function($a, $b) {
+        usort($parsedData['data'], function($a, $b) {
             return array_sum($b['data']) - array_sum($a['data']);
         });
 
         if (!empty($thisPeriodTotal)) {
-            $result['data']['datasets'][] = [
-                'label' => esc_html__('Total', 'wp-statistics'),
-                'data'  => array_values($thisPeriodTotal)
-            ];
+            $this->addChartDataset(
+                esc_html__('Total', 'wp-statistics'),
+                array_values($thisPeriodTotal)
+            );
         }
 
         // Previous period data
-        $prevPeriodData     = $this->visitorsModel->getSearchEngineReferrals(array_merge($this->args, ['date' => $prevPeriod]));
-        $prevPeriodTotal    = array_fill_keys($prevPeriodDates, 0);
+        $prevPeriodTotal = array_fill_keys($prevPeriodDates, 0);
 
-        foreach ($prevPeriodData as $item) {
+        foreach ($prevData as $item) {
             $prevPeriodTotal[$item->date] += intval($item->visitors);
         }
 
         if (!empty($prevPeriodTotal)) {
-            $result['previousData']['datasets'][] = [
-                'label' => esc_html__('Total', 'wp-statistics'),
-                'data'  => array_values($prevPeriodTotal)
-            ];
+            $this->addChartPreviousDataset(
+                esc_html__('Total', 'wp-statistics'),
+                array_values($prevPeriodTotal)
+            );
         }
 
-        return $result;
+        return $this->getChartData();
+    }
+
+    public function generateChartLabels($dateRange)
+    {
+        $labels = array_map(
+            function ($date) {
+                return [
+                    'date'  => date_i18n(Helper::getDefaultDateFormat(false, true, true), strtotime($date)),
+                    'day'   => date_i18n('l', strtotime($date)),
+                ];
+            },
+            $dateRange
+        );
+
+        return $labels;
+    }
+
+    public function getTopSearchEngines($data)
+    {
+        // Create an array of top search engines
+        $topSearchEngines = array_map(function($item) {
+            return array_sum($item);
+        }, $data);
+
+        // Get the top 3 items
+        arsort($topSearchEngines);
+        $topSearchEngines = array_slice($topSearchEngines, 0, 3, true);
+        $topSearchEngines = array_keys($topSearchEngines);
+
+        return $topSearchEngines;
     }
 }
