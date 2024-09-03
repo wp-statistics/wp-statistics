996840312bdeaa0b66b8d34fa87130d37f882488
Foad <foad.adeli@gmail.com>
2024-09-03 12:34:11 +0330

    refactor: use line chart trait in traffic chart

--- src/Service/Charts/DataProvider/TrafficChartDataProvider.php
+++ src/Service/Charts/DataProvider/TrafficChartDataProvider.php
@@ -7,10 +7,13 @@ use WP_STATISTICS\Helper;
 use WP_Statistics\Models\ViewsModel;
 use WP_Statistics\Models\VisitorsModel;
 use WP_Statistics\Service\Charts\AbstractChartDataProvider;
+use WP_Statistics\Service\Charts\Traits\LineChartResponseTrait;
 use WP_STATISTICS\TimeZone;
 
 class TrafficChartDataProvider extends AbstractChartDataProvider
 {
+    use LineChartResponseTrait;
+
     public $args;
     protected $visitorsModel;
     protected $viewsModel;
@@ -21,34 +24,42 @@ class TrafficChartDataProvider extends AbstractChartDataProvider
 
         $this->visitorsModel    = new VisitorsModel();
         $this->viewsModel       = new ViewsModel();
+
+        $this->initChartData(true);
     }
 
     public function getData()
     {
-        $result = [
-            'data'          => ['labels' => [], 'datasets' => []],
-            'previousData'  => ['labels' => [], 'datasets' => []]
-        ];
-
-        // If range is set, use it, otherwise, get from usermeta
-        $thisPeriod = isset($this->args['date']) ? $this->args['date'] : DateRange::get();
-        $prevPeriod = DateRange::getPrevPeriod($thisPeriod);
-
+        $thisPeriod     = isset($this->args['date']) ? $this->args['date'] : DateRange::get();
+        $prevPeriod     = DateRange::getPrevPeriod($thisPeriod);
         $currentDates   = array_keys(TimeZone::getListDays($thisPeriod));
         $prevDates      = array_keys(TimeZone::getListDays($prevPeriod));
 
-        $currentVisitors = $this->visitorsModel->countDailyVisitors($this->args);
-        $currentVisitors = wp_list_pluck($currentVisitors, 'visitors', 'date');
-        $currentViews    = $this->viewsModel->countDailyViews(array_merge($this->args, ['ignore_post_type' => true]));
-        $currentViews    = wp_list_pluck($currentViews, 'views', 'date');
+        // Get current data from database
+        $visitors       = $this->visitorsModel->countDailyVisitors($this->args);
+        $views          = $this->viewsModel->countDailyViews(array_merge($this->args, ['ignore_post_type' => true]));
 
+        // Get previous data from database
         $prevVisitors   = $this->visitorsModel->countDailyVisitors(array_merge($this->args, ['date' => $prevPeriod]));
-        $prevVisitors   = wp_list_pluck($prevVisitors, 'visitors', 'date');
         $prevViews      = $this->viewsModel->countDailyViews(array_merge($this->args, ['date' => $prevPeriod]));
-        $prevViews      = wp_list_pluck($prevViews, 'views', 'date');
+
+        // Parse data
+        $parsedData     = $this->parseData($currentDates, ['visitors' => $visitors, 'views' => $views]);
+        $prevParsedData = $this->parseData($prevDates, ['visitors' => $prevVisitors, 'views' => $prevViews]);
+
+        // Prepare data
+        $result = $this->prepareResult($parsedData, $prevParsedData);
+
+        return $result;
+    }
+
+    public function parseData($dates, $data)
+    {
+        $currentVisitors = wp_list_pluck($data['visitors'], 'visitors', 'date');
+        $currentViews    = wp_list_pluck($data['views'], 'views', 'date');
 
         $parsedData = [];
-        foreach ($currentDates as $date) {
+        foreach ($dates as $date) {
             $parsedData['labels'][]   = [
                 'date' => date_i18n(Helper::getDefaultDateFormat(false, true, true), strtotime($date)),
                 'day'  => date_i18n('l', strtotime($date))
@@ -57,43 +68,21 @@ class TrafficChartDataProvider extends AbstractChartDataProvider
             $parsedData['views'][]    = isset($currentViews[$date]) ? intval($currentViews[$date]) : 0;
         }
 
-        $prevParsedData = [];
-        foreach ($prevDates as $date) {
-            $prevParsedData['labels'][]   = [
-                'date' => date_i18n(Helper::getDefaultDateFormat(false, true, true), strtotime($date)),
-                'day'  => date_i18n('l', strtotime($date))
-            ];
-            $prevParsedData['visitors'][] = isset($prevVisitors[$date]) ? intval($prevVisitors[$date]) : 0;
-            $prevParsedData['views'][]    = isset($prevViews[$date]) ? intval($prevViews[$date]) : 0;
-        }
+        return $parsedData;
+    }
 
-        // Add parsed data to dataset
-        $result['data']['labels']       = $parsedData['labels'];
+    public function prepareResult($data, $prevData)
+    {
+        // Current Data
+        $this->setChartLabels($data['labels']);
+        $this->addChartDataset(esc_html__('Visitors', 'wp-statistics'), $data['visitors']);
+        $this->addChartDataset(esc_html__('Views', 'wp-statistics'), $data['views']);
 
-        $result['data']['datasets'][]   = [
-            'label' => __('Visitors', 'wp-statistics'),
-            'data'  => $parsedData['visitors'],
-        ];
+        // Previous Data
+        $this->setChartPreviousLabels($prevData['labels']);
+        $this->addChartPreviousDataset(esc_html__('Visitors', 'wp-statistics'), $prevData['visitors']);
+        $this->addChartPreviousDataset(esc_html__('Views', 'wp-statistics'), $prevData['views']);
 
-        $result['data']['datasets'][]   = [
-            'label' => __('Views', 'wp-statistics'),
-            'data'  => $parsedData['views'],
-        ];
-
-
-        // Add previous parsed data to dataset
-        $result['previousData']['labels']       = $prevParsedData['labels'];
-
-        $result['previousData']['datasets'][]   = [
-            'label' => __('Visitors', 'wp-statistics'),
-            'data'  => $prevParsedData['visitors']
-        ];
-
-        $result['previousData']['datasets'][]   = [
-            'label' => __('Views', 'wp-statistics'),
-            'data'  => $prevParsedData['views']
-        ];
-
-        return $result;
+        return $this->getChartData();
     }
 }
