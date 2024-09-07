a04d4b422de0286b7aee246c4fbb0038161b30db
Foad <foad.adeli@gmail.com>
2024-09-07 15:52:37 +0330

    Pass default date format to JS

--- includes/admin/class-wp-statistics-admin-assets.php
+++ includes/admin/class-wp-statistics-admin-assets.php
@@ -327,7 +327,8 @@ class Admin_Assets
             'geo_city'      => 1,
             'overview_page' => (Menus::in_page('overview') ? 1 : 0),
             'gutenberg'     => (Helper::is_gutenberg() ? 1 : 0),
-            'more_btn'      => (apply_filters('wp_statistics_meta_box_more_button', true) ? 1 : 0)
+            'more_btn'      => (apply_filters('wp_statistics_meta_box_more_button', true) ? 1 : 0),
+            'wp_date_format'=> Helper::getDefaultDateFormat()
         );
 
         // WordPress Current Page
