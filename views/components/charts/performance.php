8b86a25b5bc8345d464ada60034f931d81751091
Foad <foad.adeli@gmail.com>
2024-09-07 12:17:00 +0330

    fix: minor issues

--- views/components/charts/performance.php
+++ views/components/charts/performance.php
@@ -1,4 +1,5 @@
 <?php
+
 use WP_STATISTICS\Helper;
 use WP_Statistics\Utils\Request;
 
@@ -20,14 +21,14 @@ $postType = Request::get('tab', 'post');
     <div class="wps-postbox-chart--container c-wps-performance-chart__container">
         <?php
         $canvasIds = [
-            'category' => 'performance-category-chart',
-            'categorySingle' => 'performance-category-chart-single',
-            'post-type' => 'performance-chart',
-            'single' => 'performance-chart-single',
+            'category'          => 'performance-category-chart',
+            'categorySingle'    => 'performance-category-chart-single',
+            'post-type'         => 'performance-chart',
+            'single'            => 'performance-chart-single',
         ];
 
         if (isset($canvasIds[$type])) {
-            echo '<canvas id="' . $canvasIds[$type] . '" height="299"></canvas>';
+            echo '<canvas id="' . esc_attr($canvasIds[$type]) . '" height="299"></canvas>';
         }
         ?>
     </div>
