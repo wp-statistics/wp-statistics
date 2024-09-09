ef9e4b3dffebd954dcf3f9b2bcf11ede435531e3
Foad <foad.adeli@gmail.com>
2024-09-09 16:26:20 +0330

    Add source channels class

---
+++ src/Service/Analytics/Referrals/SourceChannels.php
@@ -0,0 +1,12 @@
+<?php
+namespace WP_Statistics\Service\Analytics\Referrals;
+
+class SourceChannels
+{
+    const SEARCH = 'search';
+    const PAID_SEARCH = 'paid_search';
+    const SOCIAL_MEDIA = 'social_media';
+    const PAID_SOCIAL_MEDIA = 'paid_social_media';
+    const REFERRAL_SITES = 'referral_sites';
+    const DIRECT = 'direct';
+}
