d4024a76aa197df3a255f4ed6ac202d60356ca4a
Foad <foad.adeli@gmail.com>
2024-09-10 10:32:24 +0330

    feat: add referrals parser methods

--- src/Service/Analytics/Referrals/ReferralsParser.php
+++ src/Service/Analytics/Referrals/ReferralsParser.php
@@ -2,7 +2,33 @@
 
 namespace WP_Statistics\Service\Analytics\Referrals;
 
-class ReferralsParser
-{
+use WP_Statistics\Utils\Url;
 
+class ReferralsParser {
+    private $referralsList;
+
+    public function __construct() {
+        $this->referralsList = ReferralsDatabase::get();
+    }
+
+    public function parse($url) {
+        $domain = Url::getDomain($url);
+
+        foreach ($this->referralsList['source_channels'] as $channelType => $channelData) {
+            foreach ($channelData['channels'] as $channel) {
+                foreach ($channel['domains'] as $channelDomain) {
+                    if ($channelDomain === $domain) {
+                        return [
+                            'name'          => $channel['name'],
+                            'identifier'    => $channel['identifier'],
+                            'domain'        => $channelDomain,
+                            'channel'       => $channelType
+                        ];
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
 }
