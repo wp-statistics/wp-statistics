3ee03c07ac873055591915a5724f53a586e672e4
Foad <foad.adeli@gmail.com>
2024-09-10 12:08:50 +0330

    format: format file

--- src/Service/Analytics/Referrals/ReferralsParser.php
+++ src/Service/Analytics/Referrals/ReferralsParser.php
@@ -4,20 +4,24 @@ namespace WP_Statistics\Service\Analytics\Referrals;
 
 use WP_Statistics\Utils\Url;
 
-class ReferralsParser {
+class ReferralsParser
+{
     private $referralsList;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->referralsList = ReferralsDatabase::get();
     }
 
-    public function parse($url) {
+    public function parse($url)
+    {
         $domain = Url::getDomain($url);
 
         foreach ($this->referralsList['source_channels'] as $channelType => $channelData) {
             foreach ($channelData['channels'] as $channel) {
                 foreach ($channel['domains'] as $channelDomain) {
                     if ($channelDomain === $domain) {
+
                         return [
                             'name'          => $channel['name'],
                             'identifier'    => $channel['identifier'],
