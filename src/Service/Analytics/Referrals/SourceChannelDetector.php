d3efeafbbe37116522c7ad5726de1bcc4980aace
Foad <foad.adeli@gmail.com>
2024-09-10 10:32:12 +0330

    feat: add source channel methods

--- src/Service/Analytics/Referrals/SourceChannelDetector.php
+++ src/Service/Analytics/Referrals/SourceChannelDetector.php
@@ -4,58 +4,31 @@ namespace WP_Statistics\Service\Analytics\Referrals;
 
 class SourceChannelDetector
 {
-    public $currentChannel;
+    public $referral;
 
-    public function __construct($referrer)
+    public function __construct($url)
     {
+        $parser         = new ReferralsParser();
+        $this->referral = $parser->parse($url);
     }
 
-    /**
-     * Check if the current traffic source is organic.
-     */
-    public function isOrganic()
+    public function getName()
     {
-        // TODO: regex check UTM parameters
-        return $this->currentChannel['category'] === SourceChannels::SEARCH;
+        return $this->referral['name'];
     }
 
-    /**
-     * Check if the current traffic source is from paid search.
-     */
-    public function isPaidSearch()
+    public function getIdentifier()
     {
-        return $this->currentChannel['category'] === SourceChannels::PAID_SEARCH;
+        return $this->referral['identifier'];
     }
 
-    /**
-     * Check if the current traffic source is from social media.
-     */
-    public function isSocialMedia()
+    public function getChannel()
     {
-        return $this->currentChannel['category'] === SourceChannels::SOCIAL_MEDIA;
+        return $this->referral['channel'];
     }
 
-    /**
-     * Check if the current traffic source is from paid social media.
-     */
-    public function isPaidSocialMedia()
+    public function getDomain()
     {
-        return $this->currentChannel['category'] === SourceChannels::PAID_SOCIAL_MEDIA;
-    }
-
-    /**
-     * Check if the current traffic source is from referral sites.
-     */
-    public function isReferral()
-    {
-        return $this->currentChannel['category'] === SourceChannels::REFERRAL_SITES;
-    }
-
-    /**
-     * Check if the current traffic source is direct.
-     */
-    public function isDirect()
-    {
-        return $this->currentChannel['category'] === SourceChannels::DIRECT;
+        return $this->referral['domain'];
     }
 }
