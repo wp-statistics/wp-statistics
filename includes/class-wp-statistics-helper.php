6fb61ad2eaddf6f17d547c289126f45309dbd5f4
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-09-11 14:11:04 +0330

    Remove new Helper class

--- includes/class-wp-statistics-helper.php
+++ includes/class-wp-statistics-helper.php
@@ -2062,4 +2062,18 @@ class Helper
 
         return [];
     }
+
+    /**
+     * Is the given string a JSON object?
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+    public static function isJson($string)
+    {
+        json_decode($string);
+
+        return json_last_error() === JSON_ERROR_NONE;
+    }
 }
