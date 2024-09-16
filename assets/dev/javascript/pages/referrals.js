79350278ffaa1ee8588334c63c078a4a2d290415
Nora <mwdrad@gmail.com>
2024-09-16 12:52:05 +0330

    fix: Fix referrals chart

--- assets/dev/javascript/pages/referrals.js
+++ assets/dev/javascript/pages/referrals.js
@@ -16,23 +16,7 @@ if (wps_js.isset(wps_js.global, 'request_params', 'page') && wps_js.global.reque
             jQuery('.wps-ph-item').remove();
             jQuery('.wps-postbox-chart--data').removeClass('c-chart__wps-skeleton--legend');
             parentElement.removeClass('c-chart__wps-skeleton');
-            const data = {
-                data: {
-                    labels: searchData.data.labels,
-                    ...searchData.data.datasets.reduce((acc, item) => {
-                        acc[item.label] = item.data;
-                        return acc;
-                    }, {})
-                },
-                previousData: {
-                    labels: searchData.previousData.labels,
-                    ...searchData.previousData.datasets.reduce((acc, item) => {
-                        acc[item.label] = item.data;
-                        return acc;
-                    }, {})
-                }
-            };
-            wps_js.new_line_chart(data, 'incomeVisitorChart', null)
+            wps_js.new_line_chart(searchData, 'incomeVisitorChart', null)
         }
     }
 }
