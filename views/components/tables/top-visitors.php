a494f629a3cd9398b3681b542b890b0d9e58797b
Foad <foad.adeli@gmail.com>
2024-09-17 15:33:24 +0330

    refactor: use decorator in top visitors page

--- views/components/tables/top-visitors.php
+++ views/components/tables/top-visitors.php
@@ -6,6 +6,8 @@ use WP_STATISTICS\Admin_Template;
 use WP_STATISTICS\Visitor;
 use WP_STATISTICS\Country;
 use WP_Statistics\Components\View;
+use WP_Statistics\Service\Analytics\VisitorDecorator;
+
 ?>
 
 <div class="inside">
@@ -33,12 +35,11 @@ use WP_Statistics\Components\View;
                 </thead>
 
                 <tbody>
-                <?php foreach ($data as $visitor) :
-                    $page = Visitor::get_page_by_id($visitor->page_id);
-                ?>
+                <?php foreach ($data as $visitor) : ?>
+                    <?php /** @var VisitorDecorator $visitor */ ?>
                     <tr>
                         <td class="wps-pd-l">
-                            <a href="<?php echo esc_url(Menus::admin_url('visitors', ['type' => 'single-visitor', 'visitor_id' => $visitor->ID])); ?>"><?php echo esc_html(number_format_i18n($visitor->hits)); ?></a>
+                            <a href="<?php echo esc_url(Menus::admin_url('visitors', ['type' => 'single-visitor', 'visitor_id' => $visitor->getId()])); ?>"><?php echo esc_html($visitor->getHits()); ?></a>
                         </td>
 
                         <td class="wps-pd-l">
@@ -49,19 +50,24 @@ use WP_Statistics\Components\View;
 
                         <td class="wps-pd-l">
                             <div class="wps-country-flag wps-ellipsis-parent">
-                                <a href="<?php echo esc_url(Menus::admin_url('geographic', ['type' => 'single-country', 'country' => $visitor->location])) ?>" class="wps-tooltip" title="<?php echo esc_attr(Country::getName($visitor->location)) ?>">
-                                    <img src="<?php echo esc_url(Country::flag($visitor->location)) ?>" alt="<?php echo esc_attr(Country::getName($visitor->location)) ?>" width="15" height="15">
+                                <a href="<?php echo esc_url(Menus::admin_url('geographic', ['type' => 'single-country', 'country' => $visitor->getCountryCode()])) ?>" class="wps-tooltip" title="<?php echo esc_attr($visitor->getCountryName()) ?>">
+                                    <img src="<?php echo esc_url($visitor->getCountryFlag()) ?>" alt="<?php echo esc_attr($visitor->getCountryName()) ?>" width="15" height="15">
                                 </a>
-                                <?php $location = Admin_Template::locationColumn($visitor->location, $visitor->region, $visitor->city); ?>
+                                <?php $location = Admin_Template::locationColumn($visitor->getCountryCode(), $visitor->getRegion(), $visitor->getCity()); ?>
                                 <span class="wps-ellipsis-text" title="<?php echo esc_attr($location) ?>"><?php echo esc_html($location) ?></span>
                             </div>
                         </td>
 
                         <td class="wps-pd-l">
-                            <?php echo Referred::get_referrer_link($visitor->referred, '', true); ?>
+                            <?php if ($visitor->getReferral()->getReferrer()) : ?>
+                                <a href="<?php echo esc_url($visitor->getReferral()->getReferrer()) ?>"><?php echo esc_html($visitor->getReferral()->getReferrer()) ?></a>
+                            <?php else : ?>
+                                <?php echo Admin_Template::UnknownColumn() ?>
+                            <?php endif; ?>
                         </td>
 
                         <td class="wps-pd-l">
+                            <?php $page = $visitor->getLastPage(); ?>
                             <?php if (!empty($page)) : ?>
                                 <a target="_blank" href="<?php echo esc_url($page['link']) ?>" title="<?php echo esc_attr($page['title']) ?>" class="wps-link-arrow">
                                     <span><?php echo esc_html($page['title']) ?></span>
