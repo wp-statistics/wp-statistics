3fe5ab6dcd6b7fb09b12a003c8c909118a8bff4a
Foad <foad.adeli@gmail.com>
2024-09-21 13:00:33 +0330

    fix: geolocation error exception when invalid IP provided

--- includes/class-wp-statistics-ip.php
+++ includes/class-wp-statistics-ip.php
@@ -21,7 +21,7 @@ class IP
      *
      * @var array
      */
-    public static $private_SubNets = array('10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16', '127.0.0.1/24', 'fc00::/7');
+    public static $private_SubNets = array('10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16', '127.0.0.1/24', 'fc00::/7', '::1');
 
     /**
      * List Of Common $_SERVER for get Users IP
