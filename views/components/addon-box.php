d7ce8a7bff13a963e0275af0a9cd94c194abf636
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-09-22 15:52:06 +0330

    Add semicolons after PHP codes

--- views/components/addon-box.php
+++ views/components/addon-box.php
@@ -29,27 +29,27 @@ if (!defined('ABSPATH') || empty($addOn)) {
             <span class="wps-postbox-addon__status wps-postbox-addon__status--<?php echo esc_attr($addOn->getStatusClass()); ?> "><?php echo esc_html($addOn->getStatusLabel()); ?></span>
 
             <?php if (isset($has_license_btn)) : ?>
-                <a class="wps-postbox-addon__button js-wps-addon-license-button"><?php echo esc_html__('License', 'wp-statistics') ?></a>
+                <a class="wps-postbox-addon__button js-wps-addon-license-button"><?php echo esc_html__('License', 'wp-statistics'); ?></a>
             <?php endif; ?>
 
             <?php if (isset($active_link)) : ?>
-                <a class="wps-postbox-addon__button" href="<?php echo esc_url($active_link) ?>" title="<?php echo esc_html__('Active', 'wp-statistics') ?>"><?php echo esc_html__('Active', 'wp-statistics') ?></a>
+                <a class="wps-postbox-addon__button" href="<?php echo esc_url($active_link); ?>" title="<?php echo esc_html__('Active', 'wp-statistics'); ?>"><?php echo esc_html__('Active', 'wp-statistics'); ?></a>
             <?php endif; ?>
 
             <div class="wps-addon--actions">
                 <span class="wps-addon--actions--show-more js-addon-show-more"></span>
                 <ul class="wps-addon--submenus">
                     <?php if ($addOn->isActivated()) : ?>
-                        <li><a href="<?php echo esc_url($addOn->getSettingsUrl()) ?>" class="wps-addon--submenu wps-addon--submenu__settings"><?php esc_html_e('Settings', 'wp-statistics'); ?></a></li>
+                        <li><a href="<?php echo esc_url($addOn->getSettingsUrl()); ?>" class="wps-addon--submenu wps-addon--submenu__settings"><?php esc_html_e('Settings', 'wp-statistics'); ?></a></li>
                     <?php endif; ?>
                     <?php if (isset($detail_link)) : ?>
-                        <li><a href="<?php echo esc_url($detail_link) ?>" class="wps-addon--submenu" target="_blank"><?php esc_html_e('Add-On Detail', 'wp-statistics'); ?></a></li>
+                        <li><a href="<?php echo esc_url($detail_link); ?>" class="wps-addon--submenu" target="_blank"><?php esc_html_e('Add-On Detail', 'wp-statistics'); ?></a></li>
                     <?php endif; ?>
                     <?php if (isset($change_log_link)) : ?>
-                        <li><a href="<?php echo esc_url($change_log_link) ?>" class="wps-addon--submenu" target="_blank"><?php esc_html_e('Changelog', 'wp-statistics'); ?></a></li>
+                        <li><a href="<?php echo esc_url($change_log_link); ?>" class="wps-addon--submenu" target="_blank"><?php esc_html_e('Changelog', 'wp-statistics'); ?></a></li>
                     <?php endif; ?>
                     <?php if (isset($documentation_link)) : ?>
-                        <li><a href="<?php echo esc_url($documentation_link) ?>" class="wps-addon--submenu" target="_blank"><?php esc_html_e('Documentation', 'wp-statistics'); ?></a></li>
+                        <li><a href="<?php echo esc_url($documentation_link); ?>" class="wps-addon--submenu" target="_blank"><?php esc_html_e('Documentation', 'wp-statistics'); ?></a></li>
                     <?php endif; ?>
                 </ul>
             </div>
@@ -58,7 +58,7 @@ if (!defined('ABSPATH') || empty($addOn)) {
     <div class="wps-addon__item__license js-wps-addon-license">
         <div class="wps-addon__item__update_license">
             <input type="text" placeholder="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx">
-            <button><?php echo esc_html__('Update License', 'wp-statistics') ?></button>
+            <button><?php echo esc_html__('Update License', 'wp-statistics'); ?></button>
         </div>
         <?php if (isset($alert_text)) : ?>
         <div class="wps-alert wps-alert--<?php echo $alert_class;?>">
