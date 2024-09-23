436a02f0816a3db88526ddc63f1a0f308d5ca972
Koorosh14 <97566920+Koorosh14@users.noreply.github.com>
2024-09-23 16:29:19 +0330

    Remove retry_link and active_link from addon-active-card view

--- views/components/addon-active-card.php
+++ views/components/addon-active-card.php
@@ -23,11 +23,11 @@ if (!defined('ABSPATH') || empty($addOn)) {
     <div class="wps-addon__download__item--actions">
         <?php if (!$addOn->isInstalled() || $addOn->isUpdateAvailable()) : ?>
             <span class="wps-postbox-addon__status wps-postbox-addon__status--danger "><?php esc_html_e('Failed', 'wp-statistics'); ?></span>
-            <a class="wps-postbox-addon__button" href="<?php echo esc_url($retry_link); ?>" title="<?php esc_html_e('Retry', 'wp-statistics'); ?>"><?php esc_html_e('Retry', 'wp-statistics'); ?></a>
+            <a class="wps-postbox-addon__button button-retry-addon-download" href="#" data-slug="<?php echo esc_attr($addOn->getSlug()); ?>" title="<?php esc_html_e('Retry', 'wp-statistics'); ?>"><?php esc_html_e('Retry', 'wp-statistics'); ?></a>
         <?php elseif ($addOn->isActivated()) : ?>
             <span class="wps-postbox-addon__status wps-postbox-addon__status--success "><?php esc_html_e('Activated', 'wp-statistics'); ?></span>
         <?php elseif ($addOn->isInstalled()) : ?>
-            <a class="wps-postbox-addon__button" href="<?php echo esc_url($active_link); ?>" title="<?php esc_html_e('Active', 'wp-statistics'); ?>"><?php esc_html_e('Active', 'wp-statistics'); ?></a>
+            <a class="wps-postbox-addon__button button-activate-addon" href="#" data-slug="<?php echo esc_attr($addOn->getSlug()); ?>" title="<?php esc_html_e('Active', 'wp-statistics'); ?>"><?php esc_html_e('Active', 'wp-statistics'); ?></a>
         <?php endif; ?>
 
         <?php if ($addOn->isActivated() || !empty($addOn->getProductUrl()) || !empty($addOn->getChangelogUrl()) || !empty($addOn->getDocumentationUrl())) : ?>
