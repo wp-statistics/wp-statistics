e416bee2cb9c3e6674cf5a8b6a710bf397d237be
Foad <foad.adeli@gmail.com>
2024-10-06 15:01:02 +0330

    feat: add helper methods to datetime class

--- src/Components/DateTime.php
+++ src/Components/DateTime.php
@@ -2,6 +2,8 @@
 
 namespace WP_Statistics\Components;
 
+use ErrorException;
+
 class DateTime
 {
     public static $defaultDateFormat = 'Y-m-d';
@@ -36,5 +38,109 @@ class DateTime
             default:
                 return 'monday';
         }
+    }
+
+    /**
+     * Gets the date format string from WordPress settings.
+     *
+     * @return string
+     */
+    public static function getDateFormat()
+    {
+        return get_option('date_format', self::$defaultDateFormat);
+    }
+
+    /**
+     * Gets the time format string from WordPress settings.
+     *
+     * @return string
+     */
+    public static function getTimeFormat()
+    {
+        return get_option('time_format', self::$defaultTimeFormat);
+    }
+
+    /**
+     * Gets the date and time format string from WordPress settings.
+     *
+     * @param string $separator (optional) The separator to use between date and time.
+     * @return string
+     */
+    public static function getDateTimeFormat($separator = ' ')
+    {
+        return self::getDateFormat() . $separator . self::getTimeFormat();
+    }
+
+
+    /**
+     * Formats a given datetime string according to WordPress settings and provided arguments.
+     *
+     * @param string|int $datetime The datetime string to format. If numeric, it is treated as a Unix timestamp.
+     * @param array $args {
+     *     Arguments to customize the formatting.
+     *
+     *     @type bool $include_time Whether to include the time in the formatted string. Default false.
+     *     @type bool $exclude_year Whether to exclude the year from the formatted string. Default false.
+     *     @type bool $short_month Whether to use a short month name (e.g. 'Jan' instead of 'January'). Default false.
+     *     @type string $separator The separator to use between date and time. Default ' '.
+     *     @type string $date_format The format string to use for the date. Default is the WordPress option 'date_format'.
+     *     @type string $time_format The format string to use for the time. Default is the WordPress option 'time_format'.
+     * }
+     *
+     * @return string The formatted datetime string.
+     *
+     * @throws ErrorException If the provided datetime string is invalid.
+     */
+    public static function format($datetime, $args = [])
+    {
+        $args = wp_parse_args($args, [
+            'include_time'  => false,
+            'exclude_year'  => false,
+            'short_month'   => false,
+            'separator'     => ' ',
+            'date_format'   => self::getDateFormat(),
+            'time_format'   => self::getTimeFormat()
+        ]);
+
+        $timestamp  = is_numeric($datetime) ? $datetime : strtotime($datetime);
+
+        if ($timestamp === false) {
+            throw new ErrorException(esc_html__('Invalid date passed as argument.', 'wp-statistics'));
+        }
+
+        if ($args['include_time'] === true) {
+            $format = $args['date_format'] . $args['separator'] . $args['time_format'];
+        } else {
+            $format = $args['date_format'];
+        }
+
+        if ($args['exclude_year']) {
+            $format = preg_replace('/(,\s?Y|Y\s?,|Y[, \/-]?|[, \/-]?Y)/i', '', $format);
+        }
+
+        if ($args['short_month']) {
+            $format = str_replace('F', 'M', $format);
+        }
+
+        return date($format, $timestamp);
+    }
+
+    /**
+     * Check is Valid date
+     *
+     * @param $date
+     * @return bool
+     */
+    public static function isValidDate($date)
+    {
+        if (empty($date)) {
+            return false;
+        }
+
+        if (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $date) && strtotime($date) !== false) {
+            return true;
+        }
+
+        return false;
     }
 }
