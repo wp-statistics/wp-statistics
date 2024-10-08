68c4784fea31e9bf4174cf9500556aa51c4e49a4
Nora <mwdrad@gmail.com>
2024-10-08 14:33:30 +0330

    pref : Improving the overview tables.

--- assets/dev/javascript/meta-box/recent.js
+++ assets/dev/javascript/meta-box/recent.js
@@ -15,35 +15,35 @@ wps_js.recent_meta_box = {
 
         args.forEach(function (value, index) {
             t += `<tr>
-            <td class="wps-pd-l">${value['date']}</td>
+            <td class="wps-pd-l">${value['last_view']}</td>
             <td class="wps-pd-l">
                 ${wps_js.visitor_info(value)}
             </td>
              </td>`
                 + (wps_js.is_active('geo_ip') ? `<td class="wps-pd-l">
                 <div class="wps-country-flag wps-ellipsis-parent">
-                    <a href="" class="wps-tooltip" title="${value['country']['name']}">
-                        <img src="${value['country']['flag']}" alt="" width="15" height="15">
+                    <a href="" class="wps-tooltip" title="${value['location']['country']}">
+                        <img src="${value['location']['flag']}" alt="" width="15" height="15">
                     </a>
-                     <span class="wps-ellipsis-text" title="${value['country']['name']}">${value['country']['name']}</span>
+                     <span class="wps-ellipsis-text" title="${value['location']['country']}">${value['location']['country']}</span>
                 </div>
              </td>` : `-`) + `
              <td class="wps-pd-l">`
-                + (value['refer'] && value['refer'] !== '' ?
-                    `<a target="_blank" href="" title="${value['refer']}" class="wps-link-arrow">
-                        <span>${value['refer']}</span>
+                + (value['referrer'] && value['referrer']['name'] !== '' ?
+                    `<a target="_blank" href="${value['referrer']['link']}" title="${value['referrer']['name']}" class="wps-link-arrow">
+                        <span>${value['referrer']['name']}</span>
                     </a>`
                     : `<span aria-hidden="true">—</span>`) + `
             </td>
             <td class="wps-pd-l">`
-                + (value['page'] && value['page']['title'] !== '' ?
-                    `<a target="_blank" href="${value['page']['link']}" title="${value['page']['title']}" class="wps-link-arrow">
-                        <span>${value['page']['title']}</span>
+                + (value['last_page'] && value['last_page']['title'] !== '' ?
+                    `<a target="_blank" href="${value['last_page']['link']}" title="${value['last_page']['title']}" class="wps-link-arrow">
+                        <span>${value['last_page']['title']}</span>
                     </a>`
                     : `<span aria-hidden="true">—</span><span class="screen-reader-text">-</span>`) + `
             </td>
             <td class="wps-pd-l">
-                <a href="">${value['hits']}</a>
+                <a href="${value['single_url']}">${value['hits']}</a>
             </td>
 			</tr>`;
         });
--- assets/dev/javascript/meta-box/useronline.js
+++ assets/dev/javascript/meta-box/useronline.js
@@ -17,33 +17,35 @@ wps_js.useronline_meta_box = {
 
         args.forEach(function (value) {
             t += `<tbody><tr>
-            <td class="wps-pd-l">10 Mar, 4:15 pm</td>
+            <td class="wps-pd-l">${value['last_view']}</td>
             <td class="wps-pd-l">
                 ${wps_js.visitor_info(value)}
             </td>`
                 + (wps_js.is_active('geo_ip') ? `<td class="wps-pd-l">
                 <div class="wps-country-flag wps-ellipsis-parent">
-                    <a href="" class="wps-tooltip" title="${value['country']['name']}">
-                        <img src="${value['country']['flag']}" alt="" width="15" height="15">
+                    <a href="" class="wps-tooltip" title="${value['location']['country']}">
+                        <img src="${value['location']['flag']}" alt="" width="15" height="15">
                     </a>
-                     <span class="wps-ellipsis-text" title="${value['country']['name']}">${value['country']['name']}</span>
+                     <span class="wps-ellipsis-text" title="${value['location']['country']}">${value['location']['country']}</span>
                 </div>
              </td>` : `-`) + `
             <td class="wps-pd-l">`
-                + (value['refer'] && value['refer'] !== '' ?
-                    `<a target="_blank" href="" title="" class="wps-link-arrow">
-                        <span></span>
+                + (value['referrer'] && value['referrer']['name'] !== '' ?
+                    `<a target="_blank" href="${value['referrer']['link']}" title="${value['referrer']['name']}" class="wps-link-arrow">
+                        <span>${value['referrer']['name']}</span>
                     </a>`
-                    : `<span aria-hidden="true">—</span><span class="screen-reader-text">Unknown</span>`) + `
+                    : `<span aria-hidden="true">—</span>`) + `
             </td>
-            <td class="wps-pd-l">
-                <a target="_blank" href="" title="" class="wps-link-arrow">
-                    <span>Home Page: Sample Page</span>
-                </a>
+            <td class="wps-pd-l">`
+                + (value['last_page'] && value['last_page']['title'] !== '' ?
+                    `<a target="_blank" href="${value['last_page']['link']}" title="${value['last_page']['title']}" class="wps-link-arrow">
+                        <span>${value['last_page']['title']}</span>
+                    </a>`
+                    : `<span aria-hidden="true">—</span><span class="screen-reader-text">-</span>`) + `
             </td>
-            <td class="wps-pd-l">${value['online_for']}</td>
+            <td class="wps-pd-l">${value['online_time']}</td>
             <td class="wps-pd-l">
-                <a href="${value['single_url']}">12</a>
+                 <a href="${value['single_url']}">${value['hits']}</a>
             </td>
              </tr></tbody>`;
         });
@@ -54,87 +56,88 @@ wps_js.useronline_meta_box = {
 
 };
 wps_js.visitor_info = function (value) {
-
+    const track_users = wps_js.isset(wps_js.global, 'options', 'track_users') ? wps_js.global['options']['track_users'] : null;
     let user_info = '<ul class="wps-visitor__information--container">';
     if (Object.keys(value['browser']).length > 0) {
         user_info += `<li class="wps-visitor__information">
             <div class="wps-tooltip" title="${value['browser']['name']}">
-                <a href="${value['browser']['link']}">
-                    <img src="${value['browser']['logo']}" width="15" height="15">
-                </a>
+                <img src="${value['browser']['logo']}" width="15" height="15">
             </div>
         </li>`
     }
 
-    if (value['platform']) {
+    if (Object.keys(value['os']).length > 0) {
         user_info += `<li class="wps-visitor__information">
-            <div class="wps-tooltip" title="${value['platform']}">
-                <a href="">
-                    <img src="" width="15" height="15">
-                </a>
+            <div class="wps-tooltip" title="${value['os']['name']}">
+                <img src="${value['os']['logo']}" width="15" height="15">
             </div>
         </li>`
     }
-
-    if (typeof value['user'] != 'undefined') {
-        user_info += `
+    user_info += `
     <li class="wps-visitor__information">
-        <div>
-             <a href="">
+        <div>`;
+
+    if (value['user'] && Object.keys(value['user']).length > 0) {
+        if (track_users) {
+            user_info += `<a href="${value['single_url']}">
                 <span class="wps-visitor__information__user-img"></span>
             </a>
-            <a class="wps-visitor__information__user-text" href="">
-                <span>getDisplayName</span>
-                <span>#ID</span>
-            </a>
-             <div class="wps-tooltip" data-tooltip-content="#tooltip_user_id">
-                <a href=""><span class="wps-visitor__information__user-img"></span></a>
+            <a class="wps-visitor__information__user-text" href="${value['single_url']}">
+                <span>${value['user']['name']}</span>
+                <span>#${value['ID']}</span>
+            </a>`
+        } else {
+            user_info += `<div class="wps-tooltip" data-tooltip-content="#tooltip_user_id">
+                <a href="${value['single_url']}"><span class="wps-visitor__information__user-img"></span></a>
             </div>
             <div class="wps-tooltip_templates">
                 <div id="tooltip_user_id">
                     <div>
-                    ${wps_js._('id')}: ID
+                    ${wps_js._('id')}: ${value['ID']}
                     </div>
                     <div>
-                        ${wps_js._('name')}: name
+                        ${wps_js._('name')}: ${value['user']['name']}
                     </div>
 
                     <div>
-                        ${wps_js._('email')}: email
+                        ${wps_js._('email')}: ${value['user']['email']}
                      </div>
 
                     <div>
-                        ${wps_js._('ip')}: IP
+                        ${wps_js._('ip')}: ${value['IP']}
                     </div>
                 </div>
-            </div>
-         </div>
-    </li>
-     <li class="wps-visitor__information">
-        <div>
-             <a href="">
-                <span class="wps-visitor__information__incognito-img"></span>
-            </a>
-            <span class="wps-visitor__information__incognito-text">
-                ip
-            </span>
-             <div class="wps-tooltip" title="">
-                <a href=""><span class="wps-visitor__information__incognito-img"></span></a>
-            </div>
-         </div>
-    </li>
-    `;
+            </div>`
+        }
+    } else {
+        if (track_users) {
+            user_info += `
+                <a href="${value['single_url']}">
+                    <span class="wps-visitor__information__incognito-img"></span>
+                </a>
+                <span class="wps-visitor__information__incognito-text">
+                ${value['IP']}
+                </span>`;
+        } else {
+            user_info += `
+                <div class="wps-tooltip" title="  ${wps_js._('ip')}: ${value['IP']}">
+                    <a href="${value['single_url']}"><span class="wps-visitor__information__incognito-img"></span></a>
+                </div>`;
+        }
     }
-    user_info += `</ul>`;
-    user_info += ` <div class="wps-visitor__information__user-more-info">
-        <div>
-             ${wps_js._('email')}: email
-        </div>
-    
-        <div>
-            ${wps_js._('role')}: role   
-        </div>
-    </div>`;
+    user_info += `</div></li></ul>`;
+    if (track_users && Object.keys(value['user']).length > 0) {
+        user_info += ` <div class="wps-visitor__information__user-more-info">
+            <div>
+                 ${wps_js._('email')}:  ${value['user']['email']}
+            </div>
+        
+            <div>
+                ${wps_js._('role')}:  ${value['user']['role']}   
+            </div>
+        </div>`;
+    }
+
 
     return user_info;
 }
