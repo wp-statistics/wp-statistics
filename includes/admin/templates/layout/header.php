8bfacf5ca5ddf94d7cf6b21aa71b438d5578c1cc
Foad <foad.adeli@gmail.com>
2024-10-19 14:21:36 +0330

    fix: minor issues

--- includes/admin/templates/layout/header.php
+++ includes/admin/templates/layout/header.php
@@ -1,7 +1,10 @@
-<div class="wrap wps-wrap<?php echo(isset($class) ? ' ' . esc_attr($class) : ''); ?>">
 <?php
 use WP_Statistics\Components\View;
+?>
 
+<div class="wrap wps-wrap<?php echo(isset($class) ? ' ' . esc_attr($class) : ''); ?>">
+
+<?php
 require_once WP_STATISTICS_DIR . "/includes/admin/templates/header.php";
 View::load("components/premium-pop-up/welcome-modal");
 ?>
