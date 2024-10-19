59eb0f98ace30c5b6d58034a5a2c75011efc2850
Nora <mwdrad@gmail.com>
2024-10-19 13:44:53 +0330

    pref : Improvement on premium pop-up

--- views/components/premium-pop-up/welcome-modal.php
+++ views/components/premium-pop-up/welcome-modal.php
@@ -8,11 +8,11 @@ use WP_Statistics\Components\View;
     <div class="wps-modal__content wps-modal__content--welcome js-wps-premiumModalWelcomeContent">
         <span class="wps-modal__welcome-image"></span>
         <div class="wps-modal__welcome-description">
-            <h2><?php esc_html_e('Welcome to the All-New WP Statistics!', 'wp-statistics'); ?></h2>
-            <p><?php esc_html_e('Meet the new WP Statistics! Faster, smarter insights await. Try Premium for even more!', 'wp-statistics'); ?></p>
+            <h2><?php esc_html_e('Discover the Full Power of WP Statistics', 'wp-statistics'); ?></h2>
+            <p><?php esc_html_e('Explore the powerful features and smart insights that help you track and understand your site’s performance like never before. Want to dive deeper? Unlock the full potential of WP Statistics by exploring Premium features.', 'wp-statistics'); ?></p>
         </div>
         <div class="wps-modal__actions">
-            <a class="wps-modal__action-btn js-wps-premiumModalExploreBtn wps-modal__action-btn--premium"><?php esc_html_e('Explore Premium', 'wp-statistics'); ?></a>
+            <a class="wps-modal__action-btn js-wps-premiumModalExploreBtn wps-modal__action-btn--premium"><?php esc_html_e('Discover Premium Features', 'wp-statistics'); ?></a>
             <a class="wps-modal__action-btn wps-modal__action-btn--skip js-wps-premiumModalClose"><?php esc_html_e('Skip', 'wp-statistics'); ?></a>
         </div>
     </div>
