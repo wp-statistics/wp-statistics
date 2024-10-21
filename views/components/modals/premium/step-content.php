2631757fbcf32084106a440673fe0c5fdc8f6875
mostafa <mst404@gmail.com>
2024-10-21 19:45:09 +0300

    Minor improvement

--- views/components/modals/premium/step-content.php
+++ views/components/modals/premium/step-content.php
@@ -1,4 +1,4 @@
-<div class="wps-modal__premium-step js-wps-premiumModalStep wps-modal__premium-step--<?php echo $step_name ?>" data-href="<?php echo $step_href ?>">
+<div class="wps-modal__premium-step js-wps-premiumModalStep wps-modal__premium-step--<?php echo esc_attr($step_name) ?>" data-href="<?php echo esc_url($step_href); ?>">
     <?php echo $description; ?>
     <?php if ( $step_name !== 'first-step') : ?>
         <img class="wps-premium-step__image" src="<?php echo WP_STATISTICS_URL . 'assets/images/premium-modal/' . esc_attr($step_name) . '.png'; ?>" alt="<?php echo esc_attr($step_name); ?>">
