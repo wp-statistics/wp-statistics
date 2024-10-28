5168865657b5024d5d107d8b15099fec14a498dc
Nora <mwdrad@gmail.com>
2024-10-28 18:24:12 +0330

    pref : Minor improvement

--- views/pages/lock-page.php
+++ views/pages/lock-page.php
@@ -8,7 +8,7 @@
             <?php echo $description?>
         </div>
         <div class="wps-lock-page__actions">
-            <a targe="_blank" class="wps-lock-page__action wps-lock-page__action--premium" href="<?php echo esc_url(WP_STATISTICS_SITE_URL . '/product/'.$addon_slug.'?utm_source=wp-statistics&utm_medium=link&utm_campaign=dp-'.$campaign); ?>">
+            <a target="_blank" class="wps-lock-page__action wps-lock-page__action--premium" href="<?php echo esc_url(WP_STATISTICS_SITE_URL . '/product/'.$addon_slug.'?utm_source=wp-statistics&utm_medium=link&utm_campaign=dp-'.$campaign); ?>">
                 <?php echo sprintf(esc_html__('Go Premium for Complete %s Reports', 'wp-statistics'), $addon_name); ?>
             </a>
              <a data-target="<?php echo $addon_slug?>" class="wps-lock-page__action wps-lock-page__action--learn-more js-wps-openPremiumModal"><?php esc_html_e('Learn More', 'wp-statistics'); ?></a>
