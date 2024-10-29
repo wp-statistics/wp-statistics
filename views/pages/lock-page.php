2b6a67d0266e7ad6835bb1ed12e16be82b31060e
Nora <mwdrad@gmail.com>
2024-10-29 11:33:16 +0330

    feat : Add premium locked page different scenarios

--- views/pages/lock-page.php
+++ views/pages/lock-page.php
@@ -1,27 +1,38 @@
 <?php
 use WP_Statistics\Components\View;
-?>
+use WP_Statistics\Service\Admin\LicenseManagement\LicenseHelper;
+use WP_Statistics\Service\Admin\LicenseManagement\Plugin\PluginHandler;
 
+$pluginHandler  = new PluginHandler();
+$isLicenseValid         = LicenseHelper::isPluginLicenseValid($addon_slug);
+$isAddonActive  = WP_STATISTICS\Helper::isAddOnActive( str_replace("wp-statistics-", "", $addon_slug));
+$hasLicense     = LicenseHelper::isPluginLicenseValid($addon_slug) ? true : false;
+$isActive       = $pluginHandler->isPluginActive($addon_slug);
+?>
 
 <div class="wps-wrap__main">
     <div class="wp-header-end"></div>
     <div class="wps-lock-page wps-lock-page--container">
         <?php
-        View::load("components/lock-sections/notice-inactive-license-addon");
-         ?>
-
-       <div class="wps-premium-feature wps-premium-feature--premium-user">
-           <?php
-            View::load("components/lock-sections/setting-active-licensed-addon", ['addon_title' => $addon_name]);
-           ?>
-       </div>
+            if ($isAddonActive && !$isLicenseValid) {
+                View::load("components/lock-sections/notice-inactive-license-addon");
+            }
+        ?>
+        <?php  if (!$isActive && $hasLicense) : ?>
+           <div class="wps-premium-feature wps-premium-feature--premium-user">
+               <?php
+                View::load("components/lock-sections/setting-active-licensed-addon", ['addon_title' => $addon_name]);
+               ?>
+           </div>
+        <?php endif ?>
         <div class="wps-lock-page__head">
              <?php echo $page_title;?>
          </div>
         <div class="wps-lock-page__description">
             <?php echo $description?>
         </div>
-        <div class="wps-lock-page__actions">
+
+         <div class="wps-lock-page__actions">
             <a target="_blank" class="wps-lock-page__action wps-lock-page__action--premium" href="<?php echo esc_url(WP_STATISTICS_SITE_URL . '/add-ons/'.$addon_slug.'?utm_source=wp-statistics&utm_medium=link&utm_campaign=dp-'.$campaign); ?>">
                 <?php echo sprintf(esc_html__('Go Premium for Complete %s Reports', 'wp-statistics'), $addon_name); ?>
             </a>
