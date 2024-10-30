151c4899e2259a77eb272c17dbfb56b2ee325e74
mostafa <mst404@gmail.com>
2024-10-30 11:19:12 +0200

    fix: update the plugin version

--- wp-statistics.php
+++ wp-statistics.php
@@ -22,7 +22,7 @@ if (!defined('ABSPATH')) exit;
 require_once __DIR__ . '/includes/defines.php';
 
 # Set another useful plugin define.
-define('WP_STATISTICS_VERSION', '14.10.3');
+define('WP_STATISTICS_VERSION', '14.11');
 
 # Load Plugin
 if (!class_exists('WP_Statistics')) {
