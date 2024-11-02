0766941619b8bf13e1f3d44ce177e2047869055b
Foad <foad.adeli@gmail.com>
2024-11-02 15:01:33 +0330

    fix: hitsmap widget visibility issue

--- includes/class-wp-statistics-meta-box.php
+++ includes/class-wp-statistics-meta-box.php
@@ -283,7 +283,6 @@ class Meta_Box
             'hitsmap'         => array(
                 'name'              => __('Global Visitor Distribution', 'wp-statistics'),
                 'description'       => __('Geographical representation of where your site\'s visitors come from.', 'wp-statistics'),
-                'require'           => array('geoip' => true),
                 'hidden'            => true,
                 'show_on_dashboard' => true,
                 'place'             => 'normal',
