8abc8465717e2e6510c9387c1fe3d7d11bdafeec
Nora <mwdrad@gmail.com>
2024-11-03 14:38:07 +0330

    pref : Improvement Modal premium

--- views/components/modals/introduce-premium/step-details.php
+++ views/components/modals/introduce-premium/step-details.php
@@ -149,6 +149,8 @@ $isPremium        = LicenseHelper::isPremiumLicenseAvailable();
 
                         if ($hasLicense && $isActive) {
                             $class = 'activated';
+                        }elseif ($hasLicense && !$isActive) {
+                            $class = 'not-active';
                         } elseif (!$hasLicense && $isInstalled) {
                             $class = 'no-license';
                         }
