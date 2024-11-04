d4ecae1c52ee797e507a26b00e649ad552af8344
Nora <mwdrad@gmail.com>
2024-11-04 14:50:53 +0330

    fix : Fix Optimization Plugin Maintenance issue

--- includes/admin/templates/optimization/updates.php
+++ includes/admin/templates/optimization/updates.php
@@ -16,8 +16,8 @@
                     </th>
 
                     <td>
-                        <input type="hidden" name="submit" value="1"/>
-                        <button id="populate-submit" class="button button-primary" type="submit" value="1" name="populate-submit"><?php esc_html_e('Refresh Country Data', 'wp-statistics'); ?></button>
+                        <input type="hidden" id="populate-submit"  name="submit" value="1"/>
+                        <button class="button button-primary" type="submit" value="1" name="populate-submit"><?php esc_html_e('Refresh Country Data', 'wp-statistics'); ?></button>
                         <p class="description"><?php esc_html_e('This action updates and corrects any unidentified or missing country data in the database. Please be patient, as the process might take some time, depending on the amount of data.', 'wp-statistics'); ?></p>
                     </td>
                 </tr>
@@ -46,8 +46,8 @@
                     </th>
 
                     <td>
-                        <input type="hidden" name="submit" value="1"/>
-                        <button id="populate-source-channel-submit" class="button button-primary" type="submit" value="1" name="populate-source-channel-submit"><?php esc_html_e('Update Source Channel', 'wp-statistics'); ?></button>
+                        <input id="populate-source-channel-submit" type="hidden" name="submit" value="1"/>
+                        <button class="button button-primary" type="submit" value="1" name="populate-source-channel-submit"><?php esc_html_e('Update Source Channel', 'wp-statistics'); ?></button>
                         <p class="description"><?php _e('This action updates and corrects any unidentified source channels in the database. Please be patient, as this process might take some time depending on the amount of data. <br> <i>Note: The accuracy of the results may be affected as we only retain whitelisted query parameters.</i>', 'wp-statistics'); ?></p>
                     </td>
                 </tr>
@@ -75,8 +75,8 @@
                     </th>
 
                     <td>
-                        <input type="hidden" name="submit" value="1"/>
-                        <button id="hash-ips-submit" class="button button-primary" type="submit" value="1" name="hash-ips-submit" onclick="return confirm('<?php esc_html_e('This will replace all IP addresses in the database with hash values and cannot be undo, are you sure?', 'wp-statistics'); ?>')"><?php esc_html_e('Initiate Conversion', 'wp-statistics'); ?></button>
+                        <input id="hash-ips-submit" type="hidden" name="submit" value="1"/>
+                        <button class="button button-primary" type="submit" value="1" name="hash-ips-submit" onclick="return confirm('<?php esc_html_e('This will replace all IP addresses in the database with hash values and cannot be undo, are you sure?', 'wp-statistics'); ?>')"><?php esc_html_e('Initiate Conversion', 'wp-statistics'); ?></button>
                         <p class="description"><?php esc_html_e('This function will transform all stored IP addresses into hashed values. Please note that after hashing, original IP addresses cannot be retrieved. The hashing process ensures user privacy while still allowing the system to generate accurate location-based insights. This operation might be time-consuming, so we recommend performing it during off-peak hours.', 'wp-statistics'); ?></p>
                     </td>
                 </tr>
