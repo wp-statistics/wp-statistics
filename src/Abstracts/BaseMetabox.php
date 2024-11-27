c971d8e3981858116847219d2b06bba2e4071a13
Foad <foad.adeli@gmail.com>
2024-11-27 13:06:51 +0330

    feat: add common abstract methods to metabox class

--- src/Abstracts/BaseMetabox.php
+++ src/Abstracts/BaseMetabox.php
@@ -3,5 +3,19 @@ namespace WP_Statistics\Abstracts;
 
 abstract class BaseMetabox
 {
+    abstract public function getKey();
 
+    abstract public function getName();
+
+    abstract public function isActive();
+
+    abstract public function getPriority();
+
+    abstract public function showOnDashboard();
+
+    abstract public function showOnOverview();
+
+    abstract public function getOptions();
+
+    abstract public function render();
 }
