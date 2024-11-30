aa1d6735d605197474a68c91695a5334a6429ee3
Foad <foad.adeli@gmail.com>
2024-11-30 15:29:04 +0330

    feat: register metaboxes in manager class

--- src/Service/Admin/Metabox/MetaboxManager.php
+++ src/Service/Admin/Metabox/MetaboxManager.php
@@ -1,21 +1,46 @@
 <?php
 namespace WP_Statistics\Service\Admin\Metabox;
 
+use WP_Statistics\Abstracts\BaseMetabox;
+use WP_Statistics\Service\Admin\Metabox\Metaboxes\TrafficSummary;
+
 class MetaboxManager
 {
     public function __construct()
     {
-        $this->registerMetaboxes();
+        add_action('admin_init', [$this, 'registerMetaboxes']);
     }
 
+    /**
+     * Retrieves a list of all metaboxes.
+     *
+     * @return BaseMetabox[]
+     */
     public function getMetaboxes()
     {
-        $metaboxes = [];
+        $metaboxes = [
+            TrafficSummary::class
+        ];
+
         return apply_filters('wp_statistics_metabox_list', $metaboxes);
     }
 
+    /**
+     * Registers active metaboxes.
+     *
+     * @return void
+     */
     public function registerMetaboxes()
     {
+        $metaboxes = $this->getMetaboxes();
 
+        foreach ($metaboxes as $metabox) {
+            $metabox = new $metabox();
+
+            // Skip inactive metaboxes
+            if (!$metabox->isActive()) continue;
+
+            add_meta_box($metabox->getKey(), $metabox->getName(), [$metabox, 'render'], $metabox->getScreen(), $metabox->getPriority());
+        }
     }
 }
