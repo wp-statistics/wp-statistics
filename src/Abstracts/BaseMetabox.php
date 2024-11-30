898d915c74f88ef0e58163073a3071e7239c291c
Foad <foad.adeli@gmail.com>
2024-11-30 15:28:16 +0330

    feat: improve BaseMetabox class

--- src/Abstracts/BaseMetabox.php
+++ src/Abstracts/BaseMetabox.php
@@ -1,21 +1,55 @@
 <?php
 namespace WP_Statistics\Abstracts;
 
+use WP_Statistics\Service\Admin\Overview\OverviewPage;
+
 abstract class BaseMetabox
 {
+    /**
+     * Returns the key for the metabox (should be unique)
+     * @return string
+     */
     abstract public function getKey();
 
+    /**
+     * Returns the name of the metabox
+     * @return string
+     */
     abstract public function getName();
 
-    abstract public function isActive();
-
+    /**
+     * Returns the priority of the metabox (side, normal, advanced)
+     * @return string
+     */
     abstract public function getPriority();
 
-    abstract public function showOnDashboard();
+    /**
+     * Returns the data for the metabox
+     * @return array
+     */
+    abstract public function getData();
 
-    abstract public function showOnOverview();
+    /**
+     * Renders the metabox output
+     * @return string
+     */
+    abstract public function render($data);
 
-    abstract public function getOptions();
+    /**
+     * Determines if the metabox is active and should be displayed
+     * @return bool
+     */
+    public function isActive()
+    {
+        return true;
+    }
 
-    abstract public function render();
+    /**
+     * Returns the screens the metabox is active on
+     * @return array
+     */
+    public function getScreen()
+    {
+        return ['statistics_page_wps_overview-new_page', 'dashboard'];
+    }
 }
