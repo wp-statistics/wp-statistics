a5c1a687ea42c064582cbb23f4f17fbbc368567c
Nora <mwdrad@gmail.com>
2024-12-08 13:52:54 +0330

    pref : Refactor Currently Online and Latest Visitors widget

--- assets/dev/javascript/meta-box/table.js
+++ assets/dev/javascript/meta-box/table.js
@@ -12,3 +12,5 @@ wps_js.render_traffic_summary = wps_js.render_table_content;
 wps_js.render_top_referring = wps_js.render_table_content;
 wps_js.render_most_visited_pages = wps_js.render_table_content;
 wps_js.render_most_active_visitors = wps_js.render_table_content;
+wps_js.render_latest_visitor_breakdown = wps_js.render_table_content;
+wps_js.render_currently_online = wps_js.render_table_content;
