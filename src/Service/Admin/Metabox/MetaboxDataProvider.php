e7d1d0bb2b21e0878a4f678964f196862f85a2fd
Foad <foad.adeli@gmail.com>
2024-12-10 15:48:15 +0330

    fix: don't show unknown countries in countries metabox

--- src/Service/Admin/Metabox/MetaboxDataProvider.php
+++ src/Service/Admin/Metabox/MetaboxDataProvider.php
@@ -65,7 +65,7 @@ class MetaboxDataProvider
 
     public function getTopCountiesData($args = [])
     {
-        return $this->visitorsModel->getVisitorsGeoData(array_merge($args, ['per_page' => 10]));
+        return $this->visitorsModel->getVisitorsGeoData(array_merge($args, ['per_page' => 10, 'not_null' => 'location']));
     }
 
     public function getTrafficChartData($args = [])
