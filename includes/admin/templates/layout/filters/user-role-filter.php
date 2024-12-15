3d45c22fd44dad93211ef67ed2f73b324846e396
Foad <foad.adeli@gmail.com>
2024-12-15 15:18:52 +0330

    fix: user role filter issues

--- includes/admin/templates/layout/filters/user-role-filter.php
+++ includes/admin/templates/layout/filters/user-role-filter.php
@@ -2,19 +2,25 @@
 
 use WP_Statistics\Utils\Request;
 
-$queryKey       = 'pid';
-$postId         = Request::get($queryKey, false, 'number');
-$defaultTitle   = $postId ? get_the_title($postId) : esc_html__('All', 'wp-statistics');
-$defaultUrl     = ''; // remove post id from query
+$queryKey       = 'role';
+$role           = Request::get($queryKey, '');
+$roles          = wp_roles()->role_names;
+$defaultUrl     = remove_query_arg([$queryKey]);
 ?>
 
 <div class="wps-filter-user-role wps-head-filters__item loading">
     <div class="wps-dropdown">
         <label for="wps-user-role" class="selectedItemLabel"><?php esc_html_e('User Role', 'wp-statistics'); ?>:</label>
         <select id="wps-user-role" class="wps-select2" data-type-show="select2">
-            <option value="<?php echo esc_url($defaultUrl) ?>" selected>
-                <?php echo esc_html($defaultTitle); ?>
+            <option value="<?php echo esc_url($defaultUrl) ?>" <?php selected($role, '') ?>>
+              <?php esc_html_e('All', 'wp-statistics'); ?>
             </option>
+
+            <?php foreach ($roles as $key => $value) : ?>
+                <option <?php selected($role, $key) ?> value="<?php echo esc_url(add_query_arg([$queryKey => $key])) ?>">
+                    <?php echo esc_html($value); ?>
+                </option>
+            <?php endforeach; ?>
         </select>
     </div>
 </div>
