3312c7b3c3d5fa256180a542b9b4700db63a8a3a
Foad <foad.adeli@gmail.com>
2024-12-21 16:18:34 +0330

    feat: add exclusions page data

--- src/Service/Admin/Exclusions/ExclusionsPage.php
+++ src/Service/Admin/Exclusions/ExclusionsPage.php
@@ -5,14 +5,32 @@ namespace WP_Statistics\Service\Admin\Exclusions;
 use WP_STATISTICS\Menus;
 use WP_STATISTICS\Admin_Template;
 use WP_Statistics\Abstracts\BasePage;
+use WP_STATISTICS\Admin_Assets;
 
 class ExclusionsPage extends BasePage
 {
     protected $pageSlug = 'exclusions';
+    protected $dataProvider;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->dataProvider = new ExclusionsDataProvider();
+    }
 
     public function init()
     {
         $this->disableScreenOption();
+    }
+
+    public function getData()
+    {
+        wp_localize_script(Admin_Assets::$prefix, 'Wp_Statistics_Exclusions_Object', [
+            'exclusions_chart_data' => $this->dataProvider->getChartData()
+        ]);
+
+        return $this->dataProvider->getExclusionsData();
     }
 
     public function view()
@@ -21,7 +39,8 @@ class ExclusionsPage extends BasePage
             'title'         => esc_html__('Excluded Data Overview', 'wp-statistics'),
             'pageName'      => Menus::get_page_slug('exclusions'),
             'DateRang'      => Admin_Template::DateRange(),
-            'hasDateRang'   => true
+            'hasDateRang'   => true,
+            'data'          => $this->getData()
         ];
 
         Admin_Template::get_template(['layout/header', 'layout/title', 'pages/exclusions', 'layout/footer'], $args);
