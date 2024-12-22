e89e47a601538e5f857e85d8db65ac81ba4804ae
Foad <foad.adeli@gmail.com>
2024-12-22 10:00:31 +0330

    feat: add exclusions chart data provider

--- src/Service/Admin/Exclusions/ExclusionsDataProvider.php
+++ src/Service/Admin/Exclusions/ExclusionsDataProvider.php
@@ -2,26 +2,30 @@
 namespace WP_Statistics\Service\Admin\Exclusions;
 
 use WP_Statistics\Models\ExclusionsModel;
+use WP_Statistics\Service\Charts\ChartDataProviderFactory;
 
 class ExclusionsDataProvider
 {
+    private $args;
     private $exclusionsModel;
 
-    public function __construct()
+    public function __construct($args = [])
     {
-        $this->exclusionsModel = new ExclusionsModel();
+        $this->args = $args;
+
+        $this->exclusionsModel  = new ExclusionsModel();
     }
 
     public function getChartData()
     {
-        return $this->exclusionsModel->getExclusions([
-            'group_by'  => ['date', 'reason']
-        ]);
+        return  [
+            'exclusions_chart_data' => ChartDataProviderFactory::exclusionsChart($this->args)->getData()
+        ];
     }
 
     public function getExclusionsData()
     {
-        $data   = $this->exclusionsModel->getExclusions();
+        $data   = $this->exclusionsModel->getExclusions($this->args);
         $total  = array_sum(array_column($data, 'count'));
 
         return [
