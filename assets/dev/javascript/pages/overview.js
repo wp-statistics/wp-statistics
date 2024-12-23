19eaa326206c7cfa27471ba1f76a5ae8f6567fe2
Nora <mwdrad@gmail.com>
2024-12-23 08:38:02 +0330

    fix : Fix number of online users

--- assets/dev/javascript/pages/overview.js
+++ assets/dev/javascript/pages/overview.js
@@ -419,13 +419,23 @@ if (wps_js.global.page.file === "index.php" || wps_js.is_active('overview_page')
         html += `</div></div>`;
         let selector = jQuery("#" + key + " h2.hndle");
 
-        if (key==='currently_online'){
-            const current_online =  jQuery(".wps-currently-online") ;
-            if(current_online.length && current_online.text() > 0 && !selector.find('.wps-wps-currently-online__container').length){
-                const online = `<span class="wps-wps-currently-online__container"><span class="wps-wps-currently-online__dot"></span><span class="wps-wps-currently-online__text">${current_online.text()}</span></span>`;
-                selector.append(online);
+        if (key === 'currently_online') {
+            const current_online = jQuery(".wps-currently-online");
+            if (current_online.length && current_online.text() > 0) {
+                const container = selector.find('.wps-wps-currently-online__container');
+
+                if (container.length) {
+                    container.find('.wps-wps-currently-online__text').text(current_online.text());
+                } else {
+                    const online = `<span class="wps-wps-currently-online__container">
+                                <span class="wps-wps-currently-online__dot"></span>
+                                <span class="wps-wps-currently-online__text">${current_online.text()}</span>
+                            </span>`;
+                    selector.append(online);
+                }
             }
         }
+
 
 
         if(response.meta && response.meta.description){
