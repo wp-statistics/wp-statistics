494732d03fb0a73330ba801737d9f2b7c39c20cf
Nora <mwdrad@gmail.com>
2025-01-05 15:27:27 +0330

    feat : Add ability to change chart interval by user

--- views/components/charts/exclusions.php
+++ views/components/charts/exclusions.php
@@ -1,11 +1,20 @@
-<div class="wps-postbox-chart">
-    <div class="wps-postbox-chart--data c-chart__wps-skeleton--legend">
-        <div class="wps-postbox-chart--items" ></div>
-        <div class="wps-postbox-chart--previousPeriod">
-            <?php esc_html_e('Previous period', 'wp-statistics') ?>
+<?php
+use WP_Statistics\Components\View;
+?>
+
+<div class="o-wrap wps-p-0">
+    <div class="wps-postbox-chart">
+        <div class="wps-postbox-chart--data c-chart__wps-skeleton--legend">
+            <div class="wps-postbox-chart--items"></div>
+            <div class="wps-postbox-chart--info">
+                <div class="wps-postbox-chart--previousPeriod">
+                    <?php esc_html_e('Previous period', 'wp-statistics') ?>
+                </div>
+                <?php View::load("components/objects/chart-time-range"); ?>
+            </div>
         </div>
-    </div>
-    <div class="wps-postbox-chart--container c-chart__wps-skeleton">
-        <canvas id="exclusionsChart">
+        <div class="wps-postbox-chart--container c-chart__wps-skeleton">
+            <canvas id="exclusionsChart">
+        </div>
     </div>
 </div>
---
+++ views/components/objects/chart-time-range.php
@@ -0,0 +1,10 @@
+<div class="wps-unit-time-chart">
+    <div class="wps-custom-select js-unitTimeSelect">
+        <span class="wps-unit-time-chart__selected-item"></span>
+        <div class="wps-unit-time-chart__dropdown">
+            <div class="wps-unit-time-chart__option" data-value="day"><?php echo esc_html__('Day', 'wp-statistics') ?></div>
+            <div class="wps-unit-time-chart__option" data-value="week"><?php echo esc_html__('Week', 'wp-statistics') ?></div>
+            <div class="wps-unit-time-chart__option" data-value="month"><?php echo esc_html__('Month', 'wp-statistics') ?></div>
+        </div>
+    </div>
+</div>
