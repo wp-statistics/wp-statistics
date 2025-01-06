c8c9aa208856a21dc4491f553969f50905ce478d
Nora <mwdrad@gmail.com>
2025-01-06 12:04:33 +0330

    pref : Header Alignment for Single Author and Category Analytics

---
+++ views/components/headers/author-analytics.php
@@ -0,0 +1,17 @@
+<div class="wps-author-analytics--header">
+    <div class="wps-author-analytics--header__img">
+        <img src="https://secure.gravatar.com/avatar/f22b8cddf3548bdf7bf3610985b12e46?s=96&amp;d=mm&amp;r=g" alt="">
+    </div>
+    <div>
+        <div class="wps-author-analytics--header__title">
+            <h2 class="wps_title">Oliver</h2>
+            <span>Administrator</span>
+        </div>
+        <div class="wps-author-analytics--header__info">
+            <span class="wps-author-analytics--header__joined"><?php echo esc_html__('Joined on', 'wp-statistics') ?> March 15, 2024 03:01 PM</span>
+            <a href=""><?php echo esc_html__('Visit Profile', 'wp-statistics') ?></a>
+            <a href=""><?php echo esc_html__('View Author Posts', 'wp-statistics') ?></a>
+            <a href=""><?php echo esc_html__('Email to Author', 'wp-statistics') ?></a>
+        </div>
+    </div>
+</div>
---
+++ views/components/headers/category-analytics.php
@@ -0,0 +1,9 @@
+<div class="wps-category-analytics--header">
+    <div class="wps-category-analytics--header__title">
+        <h2 class="wps_title"><span><?php echo esc_html__('Category:', 'wp-statistics') ?></span>"Content Marketing"</h2>
+        <a href="" target="_blank" title="Content Marketing"></a>
+    </div>
+    <div class="wps-category-analytics--header__tags">
+        <a href="">Category</a>
+    </div>
+</div>
