e8edf5120daa48ac7e584a7a4b78af35c9da3dce
Foad <foad.adeli@gmail.com>
2025-01-13 10:27:58 +0330

    fix: minor issues

--- includes/admin/templates/layout/filters/referrer-filter.php
+++ includes/admin/templates/layout/filters/referrer-filter.php
@@ -3,7 +3,9 @@
 use WP_Statistics\Components\View;
 use WP_Statistics\Utils\Request;
 
-$classes[] = Request::has('referrer') ? 'wp-referral-filter--active' : '';
+$activeFilters = Request::has('referrer');
+
+$classes[] = $activeFilters ? 'wp-referral-filter--active' : '';
 $classes[] = is_rtl() ? 'wps-pull-left' : 'wps-pull-right';
 ?>
 
