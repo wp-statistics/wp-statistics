74615b051de22648dbc369ab2610b64cd7171d6e
Nora <mwdrad@gmail.com>
2025-01-14 13:23:23 +0330

    pref : Minor improvement

--- views/components/headers/author-analytics.php
+++ views/components/headers/author-analytics.php
@@ -6,7 +6,7 @@ use WP_Statistics\Utils\Request;
 $authorId = Request::get('author_id');
 $userData = get_userdata($authorId);
 
-$hasUserData = ! empty($userData);
+$hasUserData = !empty($userData);
 
 $roles    = $hasUserData ? $userData->roles : [];
 $userRole = !empty($roles) ? ucfirst($roles[0]) : esc_html__('No role', 'wp-statistics');
@@ -27,7 +27,7 @@ $email       = $hasUserData ? $userData->user_email : '';
             <span><?php echo esc_html($userRole); ?></span>
         </div>
         <div class="wps-author-analytics--header__info">
-            <span class="wps-author-analytics--header__joined"><?php echo esc_html__('Joined on', 'wp-statistics') ?><?php echo esc_html($formattedDate); ?></span>
+            <span class="wps-author-analytics--header__joined"><?php echo esc_html__('Joined on', 'wp-statistics') ?> <?php echo esc_html($formattedDate); ?></span>
             <a href="<?php echo esc_url(get_edit_user_link($authorId)); ?>"><?php echo esc_html__('Visit Profile', 'wp-statistics') ?></a>
             <a href="<?php echo Menus::admin_url('pages', ['tab' => 'contents', 'author_id' => $authorId, 'pt' => 'post']); ?>"><?php echo esc_html__('View Author Posts', 'wp-statistics') ?></a>
             <a href="mailto:<?php echo esc_attr($email); ?>"><?php echo esc_html__('Email to Author', 'wp-statistics') ?></a>
