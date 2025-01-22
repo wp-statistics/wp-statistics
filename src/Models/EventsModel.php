ce181d7a317ee6504b513181edd0182b9566401f
Foad <foad.adeli@gmail.com>
2025-01-22 15:09:11 +0330

    feat: add method to count daily events

--- src/Models/EventsModel.php
+++ src/Models/EventsModel.php
@@ -75,6 +75,40 @@ class EventsModel extends BaseModel
         return $query->getAll();
     }
 
+    public function countDailyEvents($args = [])
+    {
+        $args = $this->parseArgs($args, [
+            'event_name'    => '',
+            'event_target'  => '',
+            'author_id'     => '',
+            'post_type'     => '',
+            'post_id'       => '',
+            'date'          => '',
+            'decorator'     => '',
+            'order'         => 'date',
+            'order_by'      => 'DESC',
+        ]);
+
+        $query = Query::select('COUNT(events.ID) as count, DATE(events.date) as date')
+            ->from('events')
+            ->where('event_name', 'IN', $args['event_name'])
+            ->where('events.page_id', '=', $args['post_id'])
+            ->whereJson('event_data', 'target_url', '=', $args['event_target'])
+            ->whereDate('events.date', $args['date'])
+            ->orderBy($args['order'], $args['order_by'])
+            ->groupBy('Date(events.date)')
+            ->decorate($args['decorator']);
+
+        if (!empty($args['author_id']) || !empty($args['post_type']) || !empty($args['post_id'])) {
+            $query
+                ->join('posts', ['events.page_id', 'posts.ID'])
+                ->where('posts.post_type', '=', $args['post_type'])
+                ->where('posts.post_author', '=', $args['author_id']);
+        }
+
+        return $query->getAll();
+    }
+
     public function getTopEvents($args = [])
     {
         $args = $this->parseArgs($args, [
