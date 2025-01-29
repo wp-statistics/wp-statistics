1566fc7bcc5a2e770baa86dfbcd1626cd5944d39
Foad <foad.adeli@gmail.com>
2025-01-29 10:48:43 +0330

    fix: visitors by country query issue

--- src/Models/VisitorsModel.php
+++ src/Models/VisitorsModel.php
@@ -948,7 +948,7 @@ class VisitorsModel extends BaseModel
                 'visitor.location as country',
                 'visitor.region as region',
                 'visitor.continent as continent',
-                'COUNT(visitor.ID) as visitors',
+                'COUNT(DISTINCT visitor.ID) as visitors',
                 'SUM(visitor.hits) as views', // All views are counted and results can't be filtered by author, post type, etc...
             ],
             'date'          => '',
