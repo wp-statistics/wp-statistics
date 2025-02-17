65226c72b9291c023625d3fe7bb1335163955021
Nora <mwdrad@gmail.com>
2025-02-17 18:46:15 +0330

    feat : Add marketing setting page

--- includes/admin/templates/settings/add-ons/marketing.php
+++ includes/admin/templates/settings/add-ons/marketing.php
@@ -34,16 +34,133 @@ if ($isMarketingActive && !$isLicenseValid) {
     View::load("components/lock-sections/notice-inactive-license-addon");
 }
 ?>
-<div class="postbox">
+<div class="postbox wps-addon-settings--marketing">
     <table class="form-table <?php echo !$isMarketingActive ? esc_attr('form-table--preview') : '' ?>">
         <tbody>
+        <tr valign="top">
+            <th scope="row" colspan="2">
+                <h3><?php esc_html_e('Search Console', 'wp-statistics'); ?></h3>
+            </th>
+        </tr>
+        <tr>
+            <td  scope="row" colspan="2">
+                <div class="wps-alert wps-alert--setting wps-alert--success">
+                    <a href="" class="button button-primary"><?php esc_html_e('Connect to Google Search Console', 'wp-statistics'); ?></a>
+                    <div class="wps-alert--setting--title">
+                        <h1>
+                            <?php esc_html_e('Benefits of Connecting', 'wp-statistics'); ?>
+                            <span><?php esc_html_e('Google Search Console', 'wp-statistics'); ?></span>
+                        </h1>
+                        <p><?php esc_html_e('The settings on this page are part of the REST API add-on, which enables the following endpoints in the WordPress REST API:', 'wp-statistics'); ?></p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><?php esc_html_e('Verify site ownership on Google Search Console in a single click', 'wp-statistics'); ?></li>
+                            <li><?php esc_html_e('Track page and keyword rankings with the Advanced Analytics module', 'wp-statistics'); ?></li>
+                            <li><?php esc_html_e('Easily set up Google Analytics without using another 3rd party plugin', 'wp-statistics'); ?></li>
+                            <li><?php esc_html_e('Automatically submit sitemaps to the Google Search Console', 'wp-statistics'); ?></li>
+                            <li><?php esc_html_e('Free keyword suggestions when entering a focus keyword', 'wp-statistics'); ?></li>
+                            <li><?php esc_html_e('Use our revolutionary SEO Analyzer to scan your website for SEO errors', 'wp-statistics'); ?></li>
+                        </ul>
+                        <a href="" class="wps-link-underline"
+                           target="_blank"><?php esc_html_e('Learn more about the benefits of connecting your account here.', 'wp-statistics'); ?>
+                        </a>
+                    </div>
+                </div>
+            </td>
 
+        </tr>
+        <tr valign="top">
+            <th scope="row">
+                <label for="wps_addon_settings[marketing][database]"><?php esc_html_e('Analytics Database', 'wp-statistics'); ?></label>
+            </th>
+
+            <td>
+                <input dir="ltr" type="text" id="wps_addon_settings[marketing][database]" name="wps_addon_settings[marketing][database]" size="30" >
+                <div class="wps-addon-settings--action">
+                    <a href="" class="wps-button wps-button--outline-danger"><?php esc_html_e('Delete data', 'wp-statistics'); ?></a>
+                    <a href="" class="wps-button wps-button--default"><?php esc_html_e('Update data manually', 'wp-statistics'); ?></a>
+                </div>
+                <p class="description">
+                    <?php esc_html_e('Enter the number of days to keep Analytics data in your database. The maximum allowed days are 180. Though, 2x data will be stored in the DB for calculating the difference properly.', 'wp-statistics'); ?>
+                </p>
+                <?php View::load("components/objects/google-data-policy-alert"); ?>
+            </td>
+        </tr>
         </tbody>
     </table>
+</div>
+<div class="postbox wps-addon-settings--marketing">
+    <table class="form-table <?php echo !$isMarketingActive ? esc_attr('form-table--preview') : '' ?>">
+        <tbody>
+        <tr valign="top">
+            <th scope="row" colspan="2">
+                <h3><?php esc_html_e('Search Console', 'wp-statistics'); ?></h3>
+            </th>
+        </tr>
+        <tr valign="top">
+            <th scope="row">
+                <label for="wps_addon_settings[marketing][site]"><?php esc_html_e('Site', 'wp-statistics'); ?></label>
+            </th>
+
+            <td>
+                <input dir="ltr" type="text" id="wps_addon_settings[marketing][site]" name="wps_addon_settings[marketing][site]" size="30" >
+                <p class="description">
+                    <?php esc_html_e('Set a threshold for daily robot visits. Robots exceeding this number daily will be identified as bots.', 'wp-statistics'); ?>
+                </p>
+            </td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">
+                <label for="wps_addon_settings[marketing][status]"><?php esc_html_e('Enable the Index status tab', 'wp-statistics'); ?></label>
+            </th>
+
+            <td>
+                <input id="wps_addon_settings[marketing][status]" name="wps_addon_settings[marketing][status]" type="checkbox">
+                <label for="wps_addon_settings[marketing][status]"><?php esc_html_e('Enable', 'wp-statistics'); ?></label>
+                 <p class="description">
+                    <?php esc_html_e('Enable this option to show the index status tab in the Analytics module.', 'wp-statistics'); ?>
+                </p>
+                <?php View::load("components/objects/google-data-policy-alert"); ?>
+            </td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">
+                <label for="wps_addon_settings[marketing][database]"><?php esc_html_e('Analytics Database', 'wp-statistics'); ?></label>
+            </th>
+
+            <td>
+                <input dir="ltr" type="text" id="wps_addon_settings[marketing][database]" name="wps_addon_settings[marketing][database]" size="30" >
+                <p class="description">
+                    <?php esc_html_e('Enter the number of days to keep Analytics data in your database. The maximum allowed days are 180. Though, 2x data will be stored in the DB for calculating the difference properly.', 'wp-statistics'); ?>
+                </p>
+                <div class="wps-addon-settings-marketing-analytics">
+                    <div class="wps-addon-settings-marketing-analytics--options">
+                        <div class="wps-addon-settings-marketing-analytics--option">
+                            <?php esc_html_e('Storage Days:', 'wp-statistics'); ?> <span>177</span>
+                        </div>
+                        <div class="wps-addon-settings-marketing-analytics--option">
+                            <?php esc_html_e('Data rows:', 'wp-statistics'); ?> <span>192.9K</span>
+                        </div>
+                        <div class="wps-addon-settings-marketing-analytics--option">
+                            <?php esc_html_e('Size:', 'wp-statistics'); ?> <span>59 MB</span>
+                        </div>
+                    </div>
+                    <div class="wps-addon-settings--action">
+                        <a href="" class="wps-button wps-button--outline-danger"><?php esc_html_e('Delete data', 'wp-statistics'); ?></a>
+                        <a href="" class="wps-button wps-button--default"><?php esc_html_e('Update data manually', 'wp-statistics'); ?></a>
+                    </div>
+                </div>
+                <?php View::load("components/objects/google-data-policy-alert"); ?>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+
 </div>
 
 <?php
 if ($isMarketingActive) {
-    submit_button(__('Update', 'wp-statistics'), 'primary', 'submit', '', array('OnClick' => "var wpsCurrentTab = getElementById('wps_current_tab'); wpsCurrentTab.value='marketing-settings'"));
+    submit_button(__('Save Settings', 'wp-statistics'), 'primary', 'submit', '', array('OnClick' => "var wpsCurrentTab = getElementById('wps_current_tab'); wpsCurrentTab.value='marketing-settings'"));
 }
 ?>
---
+++ views/components/objects/google-data-policy-alert.php
@@ -0,0 +1,3 @@
+<div class="wps-addon-settings-marketing--alert">
+      <p><?php esc_html_e('We do not store any of the data from your Google account on our servers, everything is processed & stored on your server. We take your privacy extremely seriously and ensure it is never misused.', 'wp-statistics'); ?> <a href="" target="_blank"><?php esc_html_e('Learn more', 'wp-statistics'); ?></a>.</p>
+ </div>
