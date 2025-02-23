265b5595e950a93bab206a6a5004e95c01ced010
Nora <mwdrad@gmail.com>
2025-02-23 10:34:09 +0330

    pref : Minor improvement

--- includes/admin/templates/settings/privacy.php
+++ includes/admin/templates/settings/privacy.php
@@ -84,13 +84,28 @@
                 </select>
                 <p class="description"><?php esc_html_e("Enable WP Consent Level API integration to ensure WP Statistics complies with user-selected privacy preferences. When enabled, WP Statistics will only trigger tracking based on the user's chosen consent category.", 'wp-statistics'); ?></p>
                 <?php if ($isWpConsentApiActive) : ?>
-                    <p class="description"><?php _e('<b>Note</b>: This integration requires a compatible consent management provider. If no compatible plugin is installed or active, all categories default to <b>allowed</b> (as though every visitor has opted in).', 'wp-statistics'); ?></p>
+                    <p class="description">
+                        <?php
+                        printf(
+                            __('%1$s: This integration requires a compatible consent management provider. If no compatible plugin is installed or active, all categories default to %2$s (as though every visitor has opted in).', 'wp-statistics'),
+                            '<b>' . __('Note', 'wp-statistics') . '</b>',
+                            '<b>' . __('allowed', 'wp-statistics') . '</b>'
+                        );
+                        ?>
+                    </p>
 
                     <?php
                         $compatiblePlugins = \WP_Statistics\Service\Integrations\WpConsentApi::getCompatiblePlugins();
 
                         if (empty($compatiblePlugins)) {
-                            ?><p class="description"><?php _e('<b>No Plugin Found:</b> No recognized consent management plugin detected. Please install or activate a compatible plugin to ensure compliance with user consent.', 'wp-statistics'); ?></p><?php
+                            ?><p class="description">
+                            <?php
+                            printf(
+                                __('%1$s: No recognized consent management plugin detected. Please install or activate a compatible plugin to ensure compliance with user consent.', 'wp-statistics'),
+                                '<b>' . __('No Plugin Found', 'wp-statistics') . '</b>'
+                            );
+                            ?>
+                            </p><?php
                         } else {
                             ?>
                             <p class="description">
