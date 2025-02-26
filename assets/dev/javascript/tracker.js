337f131187b6609d022473b924c2d4a4b2bcd221
Mohammad Yari <mhd.yari021@gmail.com>
2025-02-26 19:58:32 +0330

    Fix: Check debug mode before showing log about dnt.

--- assets/dev/javascript/tracker.js
+++ assets/dev/javascript/tracker.js
@@ -56,7 +56,7 @@ let wpStatisticsUserOnline = {
             if (WP_Statistics_Dnd_Active !== 1) {
                 this.sendHitRequest();
             } else {
-                if (wps_js.global.wp_debug) {
+                if (WP_Statistics_Tracker_Object.wpDebug) {
                     console.log('WP Statistics: Do Not Track (DNT) is enabled. Hit request not sent.');
                 }
             }
