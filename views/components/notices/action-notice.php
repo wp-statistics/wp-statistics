465c7c2488b3c62fa3a097d5abb3e71fec727147
Nora <mwdrad@gmail.com>
2025-02-27 18:44:14 +0330

    pref : Enhance usage tracking notification styles and add close button

--- views/components/notices/action-notice.php
+++ views/components/notices/action-notice.php
@@ -1,9 +1,14 @@
 <div class="notice notice-<?php echo esc_attr($notice['class'] ?? 'default'); ?> wp-statistics-notice">
-    <h2 class="notice-title"><?php echo esc_html($notice['message']['title'] ?? 'Default Title'); ?></h2>
+    <h2 class="notice-title">
+        <?php echo esc_html($notice['message']['title'] ?? 'Default Title'); ?>
+        <?php if ($notice['is_dismissible']) : ?>
+            <?php if ($dismissUrl) : ?>
+                <a href="<?php echo esc_url($dismissUrl); ?>" class="notice--dismiss"></a>
+            <?php endif; ?>
+        <?php endif; ?>
+    </h2>
     <p><?php echo wp_kses_post($notice['message']['content']); ?>
-        <a href="<?php echo esc_url($notice['message']['links']['learn_more']['url']); ?>" target="_blank">
-            <?php echo esc_html($notice['message']['links']['learn_more']['text']); ?>
-        </a>.
+        <a href="<?php echo esc_url($notice['message']['links']['learn_more']['url']); ?>" target="_blank"><?php echo esc_html($notice['message']['links']['learn_more']['text']);?></a>.
     </p>
     <div class="notice-footer">
         <a href="<?php echo esc_url($notice['message']['links']['enable_tracking']['url']); ?>" target="_blank"
