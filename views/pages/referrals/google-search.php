7b8a346f978c79be1eab10985462dacc4b24afc3
Foad <foad.adeli@gmail.com>
2025-03-05 14:28:31 +0330

    feat: add locked google search view

---
+++ views/pages/referrals/google-search.php
@@ -0,0 +1,12 @@
+<?php
+use WP_Statistics\Components\View;
+
+$isLocked = apply_filters('wp_statistics_referrals_google-search_locked', true);
+
+if ($isLocked) {
+    // TODO: Replace with "components/locked-marketing-page"
+    View::load("components/locked-page", [
+        'campaign'  => '',
+        'src'       => '',
+    ]);
+}
