75b9fa2b81c397cfc819e0c8dc9b2f1ee3d10b90
Mohammad Yari <mhd.yari021@gmail.com>
2025-03-11 11:22:55 +0330

    refactor: reorganize event listener and functions

--- assets/dev/javascript/tracker.js
+++ assets/dev/javascript/tracker.js
@@ -1,23 +1,26 @@
-// Initialize tracking on page load
-document.addEventListener('DOMContentLoaded', function (e) {
-    WpStatisticsUserTracker.wpStatisticsInitialize(e);
+document.addEventListener('DOMContentLoaded', function () {
+    if (WP_Statistics_Tracker_Object.option.consentLevel == 'disabled' || WP_Statistics_Tracker_Object.option.trackAnonymously || !WP_Statistics_Tracker_Object.option.isWpConsentApiActive || wp_has_consent(WP_Statistics_Tracker_Object.option.consentLevel)) {
+        WpStatisticsUserTracker.init();
+        WpStatisticsEventTracker.init();
+    }
+
+    document.addEventListener("wp_listen_for_consent_change", function (e) {
+        const changedConsentCategory = e.detail;
+
+        for (let key in changedConsentCategory) {
+            if (changedConsentCategory.hasOwnProperty(key)) {
+                if (key === WP_Statistics_Tracker_Object.option.consentLevel && changedConsentCategory[key] === 'allow') {
+                    WpStatisticsUserTracker.init();
+                    WpStatisticsEventTracker.init();
+
+                    // When trackAnonymously is enabled, the init() call above will get ignored (since it's already initialized before)
+                    // So, in this specific case, we can call checkHitRequestConditions() manually
+                    // This will insert a new record for the user (who just gave consent to us) and prevent other scripts (e.g. event.js) from malfunctioning
+                    if (WP_Statistics_Tracker_Object.option.trackAnonymously) {
+                        WpStatisticsUserTracker.checkHitRequestConditions();
+                    }
+                }
+            }
+        }
+    });
 });
-
-// Listen for consent changes
-document.addEventListener("wp_listen_for_consent_change", function (e) {
-    WpStatisticsUserTracker.wpStatisticsHandleConsentChange(e);
-});
-
-// Listen for popstate event (browser back/forward navigation)
-window.addEventListener("popstate", WpStatisticsUserTracker.wpStatisticsHandleConsentChange);
-
-// Listen for pushState/replaceState (URL changes in SPA)
-history.pushState = function (state, title, url) {
-    WpStatisticsUserTracker.originalPushState.apply(history, arguments);
-    WpStatisticsUserTracker.wpStatisticsTrackUrlChange();
-};
-
-history.replaceState = function (state, title, url) {
-    WpStatisticsUserTracker.originalReplaceState.apply(history, arguments);
-    WpStatisticsUserTracker.wpStatisticsTrackUrlChange();
-};
