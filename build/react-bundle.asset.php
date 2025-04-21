be370b7a45d7106d243b863470246b74060b2928
AlirezaVero <alireza.veronalab@gmail.com>
2025-04-21 11:22:46 +0330

    fix: fix render bugs of react pages

--- build/react-bundle.asset.php
+++ build/react-bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '299b97ea9d281a1568d5');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b5a59f22ca5bc158bd6c');
