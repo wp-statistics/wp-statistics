dcc930bb40d0269c485b65e43ef2573583a06056
Foad <foad.adeli@gmail.com>
2025-05-07 14:53:41 +0330

    fix: single resource header title issue

--- views/components/headers/single-resource.php
+++ views/components/headers/single-resource.php
@@ -1,15 +1,13 @@
 <?php
-
 use WP_Statistics\Utils\Request;
-
-$postId           = Request::get('post_id');
- ?>
+$uri = Request::get('uri');
+?>
 
 <div class="wps-content-analytics-header wps-content-analytics-header__single-resource">
     <div>
         <div class="wps-content-analytics-header__title">
-            <h2 class="wps_title"><?php echo esc_html(get_the_title($postId)); ?></h2>
-            <a href="<?php echo esc_url(get_the_permalink($postId)); ?>" target="_blank" title="<?php echo esc_attr(get_the_title($postId)); ?>"></a>
+            <h2 class="wps_title"><?php echo esc_html($uri); ?></h2>
+            <a href="<?php echo esc_url(home_url($uri)); ?>" target="_blank" title="<?php echo esc_attr($uri); ?>"></a>
         </div>
     </div>
 </div>
