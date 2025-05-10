bdb7f59064092bf3f195e603992a35ab6dd5cc31
Foad <foad.adeli@gmail.com>
2025-05-10 14:25:36 +0330

    refactor: top source categories data provider

--- src/Service/Charts/ChartDataProviderFactory.php
+++ src/Service/Charts/ChartDataProviderFactory.php
@@ -8,6 +8,7 @@ use WP_Statistics\Service\Charts\DataProvider\ModelChartDataProvider;
 use WP_Statistics\Service\Charts\DataProvider\DeviceChartDataProvider;
 use WP_Statistics\Service\Charts\DataProvider\BrowserChartDataProvider;
 use WP_Statistics\Service\Charts\DataProvider\CountryChartDataProvider;
+use WP_Statistics\Service\Charts\DataProvider\TopSourceCategoriesDataProvider;
 use WP_Statistics\Service\Charts\DataProvider\TrafficChartDataProvider;
 use WP_Statistics\Service\Charts\DataProvider\PlatformChartDataProvider;
 use WP_Statistics\Service\Charts\DataProvider\ExclusionsChartDataProvider;
@@ -195,5 +196,16 @@ class ChartDataProviderFactory
     public static function countryChart($args)
     {
         return new CountryChartDataProvider($args);
+    }
+
+    /**
+     * Returns a new instance of TopSourceCategoriesDataProvider.
+     *
+     * @param array $args
+     * @return TopSourceCategoriesDataProvider
+     */
+    public static function topSourceCategories($args)
+    {
+        return new TopSourceCategoriesDataProvider($args);
     }
 }
