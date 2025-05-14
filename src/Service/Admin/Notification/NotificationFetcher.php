1118ee9e5a05c252056db096cc22f37febc47978
Mohammad Yari <mhd.yari021@gmail.com>
2025-05-14 13:18:30 +0330

    fix: apply requested review changes

--- src/Service/Admin/Notification/NotificationFetcher.php
+++ src/Service/Admin/Notification/NotificationFetcher.php
@@ -12,7 +12,7 @@ class NotificationFetcher
      *
      * @var string
      */
-    private $apiUrl = 'http://127.0.0.1:8000';
+    private $apiUrl = 'https://connect.wp-statistics.com';
 
     /**
      * Fetches notifications from the remote API and stores them in the WordPress database.
