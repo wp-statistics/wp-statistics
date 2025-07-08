b2bd7580d0ee8bc25a5ea54e2d5b1f3e4a158066
Mohammad Yari <mhd.yari021@gmail.com>
2025-07-08 12:46:43 +0330

    fix: apply requested review changes

--- src/Service/Geolocation/Provider/DbIpProvider.php
+++ src/Service/Geolocation/Provider/DbIpProvider.php
@@ -9,6 +9,7 @@ use WP_Statistics\Components\RemoteRequest;
 use WP_STATISTICS\Option;
 use WP_Statistics\Service\Geolocation\AbstractGeoIPProvider;
 use WP_Statistics\Dependencies\GeoIp2\Database\Reader;
+use WP_Statistics\Utils\Env;
 
 class DbIpProvider extends AbstractGeoIPProvider
 {
@@ -45,7 +46,7 @@ class DbIpProvider extends AbstractGeoIPProvider
 
         try {
             // Check if the GeoIP database exists and download it immediately.
-            if (!$this->isDatabaseExist() && !$this->isLocal()) {
+            if (!$this->isDatabaseExist() && !Env::isLocal()) {
                 $this->downloadDatabase();
             }
 
