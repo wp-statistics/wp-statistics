06a189757690b231255d8c5bada63ea40bed6122
Foad <foad.adeli@gmail.com>
2025-07-22 15:30:51 +0330

    fix: minor improvements

--- src/Models/VisitorsModel.php
+++ src/Models/VisitorsModel.php
@@ -1555,26 +1555,29 @@ class VisitorsModel extends BaseModel
             ->groupBy('pages.id')
             ->getQuery();
 
-        $result = Query::select([
+        $query = Query::select([
             'page_visitors.visitors as visitors',
             'COUNT(visitor.ID) as exits',
             'pages.id as post_id, pages.page_id',
-            "COALESCE(COUNT(visitor.ID) / $totalExits, 0) * 100 AS exit_rate",
-            'posts.post_title',
-            'posts.post_date'
+            "COALESCE(COUNT(visitor.ID) / $totalExits, 0) * 100 AS exit_rate"
         ])
             ->from('visitor')
             ->join('pages', ['visitor.last_page', 'pages.page_id'])
-            ->join('posts', ['posts.ID', 'pages.id'], [], 'LEFT')
             ->joinQuery($subQuery, ['pages.id', 'page_visitors.id'], 'page_visitors')
             ->where('pages.type', 'IN', $args['resource_type'])
             ->where('pages.uri', '=', $args['uri'])
-            ->where('posts.post_author', '=', $args['author_id'])
             ->whereDate('last_counter', $args['date'])
             ->groupBy('pages.id')
             ->orderBy($args['order_by'], $args['order'])
-            ->perPage($args['page'], $args['per_page'])
-            ->getAll();
+            ->perPage($args['page'], $args['per_page']);
+
+        if (!empty($args['author_id'])) {
+            $query
+                ->join('posts', ['posts.ID', 'pages.id'], [], 'LEFT')
+                ->where('posts.post_author', '=', $args['author_id']);
+        }
+
+        $result = $query->getAll();
 
         return $result;
     }
