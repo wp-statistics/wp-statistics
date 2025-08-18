a9d49eb75f21cbfa2c066e1cefdb9a08c1ead2dc
Foad <foad.adeli@gmail.com>
2025-08-18 13:24:17 +0330

    fix: minor issues

--- src/Service/ThirdParty/ThirdPartyServices.php
+++ src/Service/ThirdParty/ThirdPartyServices.php
@@ -4,7 +4,7 @@ namespace WP_Statistics\Service\ThirdParty;
 use WP_Statistics\Service\ThirdParty\RankMath\RankMath;
 
 /**
- * The ThirdPartyFactory is a factory class that creates instances of third-party services
+ * ThirdPartyServices is a factory class that creates instances of third-party services.
 */
 class ThirdPartyServices
 {
