6e437d389230afa37ee9e649f52cd4caa1b19198
Nora <mwdrad@gmail.com>
2025-08-27 09:46:07 +0330

    pref: Minor improvement config js

--- assets/dev/javascript/config.js
+++ assets/dev/javascript/config.js
@@ -1,8 +1,10 @@
 /* Start Wp-statistics Admin Js */
-var wps_js = {};
+var wps_js = window.wps_js || {};
 
 /* Get WP Statistics global Data From Frontend */
-wps_js.global = (typeof wps_global != 'undefined') ? wps_global : [];
+wps_js.global = (typeof wps_global != 'undefined') ? wps_global : (wps_js.global || {});
+wps_js.global.options = wps_js.global.options || {};
+wps_js.global.page = wps_js.global.page || {};
 
 /* WordPress Global Lang */
 wps_js._ = function (key) {
