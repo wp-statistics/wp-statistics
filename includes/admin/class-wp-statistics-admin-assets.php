6ba4384ff256fc5474060ecfab36fee02f5a369d
Mohammad Yari <mhd.yari021@gmail.com>
2025-09-03 16:00:28 +0330

    refactor: restructure optimization.js for modular maintenance actions

--- includes/admin/class-wp-statistics-admin-assets.php
+++ includes/admin/class-wp-statistics-admin-assets.php
@@ -456,6 +456,7 @@ class Admin_Assets
             'confirm_update_channel'       => __('Are you sure you want to update and correct any unidentified source channels in the database?', 'wp-statistics'),
             'confirm_hash_ips'             => __('This will replace all IP addresses in the database with hash values and cannot be undone, are you sure?', 'wp-statistics'),
             'confirm_repair_schema'        => __('Are you sure you want to repair the schema issues?', 'wp-statistics'),
+            'operation_completed'          => __('Operation completed.', 'wp-statistics'),
         );
 
         $list['active_post_type'] = Helper::getPostTypeName(Request::get('pt', 'post'));
