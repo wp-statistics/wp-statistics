fc3ed097d6245dee82fdc923c77c6921f9708a5f
Foad <foad.adeli@gmail.com>
2025-10-07 15:46:43 +0330

    feat: add device export type

--- src/Service/Admin/Devices/Views/SingleBrowserView.php
+++ src/Service/Admin/Devices/Views/SingleBrowserView.php
@@ -7,6 +7,7 @@ use WP_STATISTICS\Menus;
 use WP_Statistics\Abstracts\BaseView;
 use WP_Statistics\Exception\SystemErrorException;
 use WP_Statistics\Service\Admin\Devices\DevicesDataProvider;
+use WP_Statistics\Service\Admin\ExportImport\ExportTypes;
 use WP_Statistics\Utils\Request;
 
 class SingleBrowserView extends BaseView
@@ -44,6 +45,7 @@ class SingleBrowserView extends BaseView
             'paged'         => Admin_Template::getCurrentPaged(),
             'DateRang'      => Admin_Template::DateRange(),
             'hasDateRang'   => true,
+            'export'        => [ExportTypes::CSV_TABLE, ExportTypes::PDF_PAGE],
             'custom_get'    => ['type' => 'single-browser', 'browser' => $this->browser],
             'data'          => $this->getData()
         ];
