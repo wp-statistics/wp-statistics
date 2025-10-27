0374553dc62c80da92512d3fcc5de6fdafb3ded0
Ehsan Moodi <ehsanmoodi90@gmail.com>
2025-10-27 15:35:01 +0330

    chore: modify root layout

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,24 +1,51 @@
-import { createRootRouteWithContext, Link, Outlet } from '@tanstack/react-router'
-import { Loader2 } from 'lucide-react'
+import { AppSidebar } from '@/components/app-sidebar'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { Separator } from '@radix-ui/react-separator'
+import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
 
 const RootLayout = () => (
-  <div>
-    <div className="p-2">
-      <div className="flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
+  <SidebarProvider>
+    <AppSidebar />
+    <SidebarInset>
+      <header className="flex h-16 shrink-0 items-center gap-2">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </header>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+          <div className="bg-muted/50 aspect-video rounded-xl" />
+          <div className="bg-muted/50 aspect-video rounded-xl" />
+          <div className="bg-muted/50 aspect-video rounded-xl" />
+        </div>
+        <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min">
+          <Outlet />
+        </div>
       </div>
-    </div>
-    <Outlet />
-  </div>
+    </SidebarInset>
+  </SidebarProvider>
 )
 
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: RootLayout,
-  errorComponent: ({ error }) => <div>{error.message}</div>,
-  pendingComponent: () => <Loader2 className="animate-spin" />,
 })
