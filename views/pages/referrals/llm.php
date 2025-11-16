465bcf076939ba85434412ddafc0e0add5111937
Nora <mwdrad@gmail.com>
2025-11-16 11:14:10 +0330

    fix: fix minor issue

--- views/pages/referrals/llm.php
+++ views/pages/referrals/llm.php
@@ -15,7 +15,7 @@ if ($isLocked) {
         'premium_btn_title'  => esc_html__('Upgrade Now to Unlock All Premium Features', 'wp-statistics'),
         'images'             => ['ai-lock.png'],
         'description'        => __('The <b>AI Insights Add-on</b> is a premium feature for WP Statistics that brings data-driven insights directly into your dashboard. It helps you understand your visitors, discover growth opportunities, and make smarter decisions.', 'wp-statistics'),
-        'second_description' => __('When you upgrade to <b>WP Statistics Premium</b>, you get access to the AI Insights add - on and all other premium add - ons . WP Statistics Premium delivers advanced analytics, detailed reports, and powerful insights for every part of your site.', 'wp - statistics'),
+        'second_description' => __('When you upgrade to <b>WP Statistics Premium</b>, you get access to the AI Insights add-on and all other premium add-ons. WP Statistics Premium delivers advanced analytics, detailed reports, and powerful insights for every part of your site.', 'wp-statistics'),
      ];
     View::load("pages/lock-page", $args);
 }
