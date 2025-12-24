60d6b0ef20cf8fff86be4e3a09d0c6adbbdd3c1e
Foad <foad.adeli@gmail.com>
2025-12-24 11:58:25 +0330

    fix: update gsc visibility option title and description

--- includes/admin/templates/settings/add-ons/marketing.php
+++ includes/admin/templates/settings/add-ons/marketing.php
@@ -101,7 +101,7 @@ $isAuthenticated = apply_filters('wp_statistics_oath_authentication_status', fal
             </tr>
             <tr data-id="wps_addon_settings-marketing-search-console">
                 <th scope="row">
-                    <span class="wps-setting-label"><?php esc_html_e('Show Google Search tab', 'wp-statistics'); ?></span>
+                    <span class="wps-setting-label"><?php esc_html_e('Show SEO Performance page', 'wp-statistics'); ?></span>
                 </th>
                 <td>
                     <?php
@@ -113,7 +113,7 @@ $isAuthenticated = apply_filters('wp_statistics_oath_authentication_status', fal
                            name="wps_addon_settings[marketing][gsc_report]" type="checkbox"
                            value="1" <?php disabled(!empty($site)); ?> <?php checked($gscReport || $site); ?>>
                     <label for="wps_addon_settings[marketing][gsc_report]"><?php esc_html_e('Enable', 'wp-statistics'); ?></label>
-                    <p class="description"><?php esc_html_e('Display the Google Search Console report tab when no Google property is connected.', 'wp-statistics'); ?></p>
+                    <p class="description"><?php esc_html_e('Display the Google Search Console reports when no Google property is connected.', 'wp-statistics'); ?></p>
                 </td>
             </tr>
 
