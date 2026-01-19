75a874955ce54c77409007126307564f34c10c47
mostafa <mst404@gmail.com>
2026-01-19 21:30:05 +0330

    refactor: use __DIR__ consistently in main plugin file

--- wp-statistics.php
+++ wp-statistics.php
@@ -48,14 +48,14 @@ require_once __DIR__ . '/src/constants.php';
 | Load Composer Autoloader
 |--------------------------------------------------------------------------
 */
-require_once WP_STATISTICS_DIR . 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
 | Load Global Functions
 |--------------------------------------------------------------------------
 */
-require_once WP_STATISTICS_DIR . 'src/functions.php';
+require_once __DIR__ . '/src/functions.php';
 
 /*
 |--------------------------------------------------------------------------
