510b9d4934d55fd5ab54bc5299093988c9d844a4
mostafa <mst404@gmail.com>
2026-05-17 12:48:50 +0330

    Catch a missed date() site in visitor tracker

--- includes/class-wp-statistics-visitor.php
+++ includes/class-wp-statistics-visitor.php
@@ -269,7 +269,7 @@ class Visitor
         if ($args['day'] == 'today') {
             $sql_time = TimeZone::getCurrentDate('Y-m-d');
         } else {
-            $sql_time = date('Y-m-d', strtotime($args['day']));
+            $sql_time = gmdate('Y-m-d', strtotime($args['day']));
         }
 
         // Prepare Query
