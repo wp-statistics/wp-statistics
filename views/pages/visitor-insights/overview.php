80888c1729c9179dc7722c67d5804033ab9d8c1f
mostafa <mst404@gmail.com>
2026-09-18 19:00:52 +0400

    Escape admin report links built from request and stored values
    Read author_id as an integer in the Author Analytics header and wrap every
    echoed Menus::admin_url() in esc_url() so query values cannot break out of
    the href attribute.

--- views/pages/visitor-insights/overview.php
+++ views/pages/visitor-insights/overview.php
@@ -74,7 +74,7 @@ $isTrackLoggedInUsersEnabled = Option::get('visitors_log');
             <?php View::load("components/tables/top-referrers", ['data' => $data['referrers']]); ?>
             <div class="wps-card__footer">
                 <div class="wps-card__footer__more">
-                    <a class="wps-card__footer__more__link" href="<?php echo Menus::admin_url('referrals', ['tab' => 'referrers']) ?>">
+                    <a class="wps-card__footer__more__link" href="<?php echo esc_url(Menus::admin_url('referrals', ['tab' => 'referrers'])) ?>">
                         <?php esc_html_e('View Referrers', 'wp-statistics') ?>
                     </a>
                 </div>
